708def70d5c3f6398dccdbd9386f396367410d35
Walid <walid.kayhan@hotmail.com>
2021-01-07 15:26:18 -0500

    Added icons as components for dynamic color change

---
+++ src/app/widgets/Location/icons/Chart.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const Chart = ({ fill }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 512.001 512.001" enableBackground="new 0 0 512 512" xmlSpace="preserve">
+            <g>
+                <g xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path d="M506.35,80.699c-7.57-7.589-19.834-7.609-27.43-0.052L331.662,227.31l-42.557-42.557c-7.577-7.57-19.846-7.577-27.423,0    L89.076,357.36c-7.577,7.57-7.577,19.853,0,27.423c3.782,3.788,8.747,5.682,13.712,5.682c4.958,0,9.93-1.894,13.711-5.682    l158.895-158.888l42.531,42.524c7.57,7.57,19.808,7.577,27.397,0.032l160.97-160.323    C513.881,100.571,513.907,88.288,506.35,80.699z" fill={fill} data-original="#000000" />
+                    </g>
+                </g>
+                <g xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path d="M491.96,449.94H38.788V42.667c0-10.712-8.682-19.394-19.394-19.394S0,31.955,0,42.667v426.667    c0,10.712,8.682,19.394,19.394,19.394H491.96c10.712,0,19.394-8.682,19.394-19.394C511.354,458.622,502.672,449.94,491.96,449.94z    " fill={fill} data-original="#000000" />
+                    </g>
+                </g>
+                <g xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path d="M492.606,74.344H347.152c-10.712,0-19.394,8.682-19.394,19.394s8.682,19.394,19.394,19.394h126.061v126.067    c0,10.705,8.682,19.394,19.394,19.394S512,249.904,512,239.192V93.738C512,83.026,503.318,74.344,492.606,74.344z" fill={fill} data-original="#000000" />
+                    </g>
+                </g>
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+                <g xmlns="http://www.w3.org/2000/svg" />
+            </g>
+        </svg>
+    );
+};
+
+Chart.propTypes = {
+    fill: PropTypes.string,
+};
+
+Chart.defaultProps = {
+    fill: '#00000'
+};
+
+export default Chart;
