d3b335972c1a79783e514ad35b3577d435fed403
Walid <walid.kayhan@hotmail.com>
2021-01-07 15:27:00 -0500

    Added control button component for action handling

---
+++ src/app/widgets/Location/components/ControlButton.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const PRIMARY_COLOR = '#3E85C7'; // Light Blue
+const SECONDARY_COLOR = '#6F7376'; // Grey (for disabled look)
+
+const Button = styled.button`
+    border: 3px solid ${PRIMARY_COLOR};
+    color: ${PRIMARY_COLOR};
+    background-color: #fff;
+    border-radius: 5px;
+    width: 100%;
+    max-width: 10rem;
+    padding: 5px;
+    margin: 0;
+    transition: 200ms ease-in-out;
+
+    &:hover:not([disabled]) {
+        color: #000;
+        border: 3px solid ${PRIMARY_COLOR};
+        transition: 200ms ease-in-out;
+    }
+
+    &:disabled {
+        color: ${SECONDARY_COLOR};
+        background-color: #D9DCE1;
+        border: 3px solid ${SECONDARY_COLOR};
+    }
+
+    svg {
+        width: 17px;
+        height: 17px;
+    }
+`;
+
+const ControlButton = ({ label, icon: Icon, onClick, disabled }) => {
+    return (
+        <Button
+            type="button"
+            onClick={onClick}
+            style={{ fontSize: '14px' }}
+            disabled={disabled}
+        >
+            {Icon && <Icon /> } {label}
+        </Button>
+    );
+};
+
+ControlButton.propTypes = {
+    label: PropTypes.string,
+    icon: PropTypes.func,
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool,
+};
+
+
+export default ControlButton;
