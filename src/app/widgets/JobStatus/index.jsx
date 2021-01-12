f6768bbf3d9f3b1ca2734c86e472273f15a39a10
Walid <walid.kayhan@hotmail.com>
2021-01-12 13:06:26 -0500

    Added main index file for new widget

---
+++ src/app/widgets/JobStatus/index.jsx
@@ -0,0 +1,380 @@
+import classNames from 'classnames';
+import mapValues from 'lodash/mapValues';
+import pubsub from 'pubsub-js';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import Space from 'app/components/Space';
+import Widget from 'app/components/Widget';
+import controller from 'app/lib/controller';
+import i18n from 'app/lib/i18n';
+import { mapPositionToUnits } from 'app/lib/units';
+import WidgetConfig from '../WidgetConfig';
+import JobStatus from './JobStatus';
+import {
+    GRBL,
+    // Units
+    IMPERIAL_UNITS,
+    METRIC_UNITS,
+    WORKFLOW_STATE_IDLE
+} from '../../constants';
+import styles from './index.styl';
+
+import { MAX_SPINDLE_SPEED } from './constants';
+
+class JobStatusWidget extends PureComponent {
+    static propTypes = {
+        widgetId: PropTypes.string.isRequired,
+        onFork: PropTypes.func.isRequired,
+        onRemove: PropTypes.func.isRequired,
+        sortable: PropTypes.object
+    };
+
+    // Public methods
+    collapse = () => {
+        this.setState({ minimized: true });
+    };
+
+    expand = () => {
+        this.setState({ minimized: false });
+    };
+
+    config = new WidgetConfig(this.props.widgetId);
+
+    state = this.getInitialState();
+
+    actions = {
+        toggleFullscreen: () => {
+            const { minimized, isFullscreen } = this.state;
+            this.setState({
+                minimized: isFullscreen ? minimized : false,
+                isFullscreen: !isFullscreen
+            });
+        },
+        toggleMinimized: () => {
+            const { minimized } = this.state;
+            this.setState({ minimized: !minimized });
+        },
+        handleSpindleSpeedChange: (event) => {
+            const spindleSpeed = this.state.spindleSpeed + Number(event.target.value) || 0;
+
+            if (spindleSpeed < 0) {
+                this.setState({ spindleSpeed: 0 });
+                return;
+            }
+
+            if (spindleSpeed > MAX_SPINDLE_SPEED) {
+                this.setState({ spindleSpeed: MAX_SPINDLE_SPEED });
+                return;
+            }
+
+            this.setState({ spindleSpeed: spindleSpeed });
+        }
+    };
+
+    controllerEvents = {
+        'serialport:open': (options) => {
+            const { port } = options;
+            this.setState({ port: port });
+        },
+        'serialport:close': (options) => {
+            const initialState = this.getInitialState();
+            this.setState({ ...initialState });
+        },
+        'gcode:unload': () => {
+            this.setState({
+                bbox: {
+                    min: {
+                        x: 0,
+                        y: 0,
+                        z: 0
+                    },
+                    max: {
+                        x: 0,
+                        y: 0,
+                        z: 0
+                    },
+                    delta: {
+                        x: 0,
+                        y: 0,
+                        z: 0
+                    }
+                }
+            });
+        },
+        'sender:status': (data) => {
+            const { total, sent, received, startTime, finishTime, elapsedTime, remainingTime } = data;
+
+            this.setState({
+                total,
+                sent,
+                received,
+                startTime,
+                finishTime,
+                elapsedTime,
+                remainingTime,
+                workflow: {
+                    state: controller.workflow.state
+                },
+            });
+        },
+        'controller:state': (type, state) => {
+            // Grbl
+            if (type === GRBL) {
+                const { parserstate } = { ...state };
+                const { modal = {} } = { ...parserstate };
+                const units = {
+                    'G20': IMPERIAL_UNITS,
+                    'G21': METRIC_UNITS
+                }[modal.units] || this.state.units;
+
+                let unitsState = {};
+                if (this.state.units !== units) {
+                    unitsState = { units: units };
+                }
+
+                this.setState(prevState => ({
+                    controller: {
+                        ...prevState.controller,
+                        type: type,
+                        state: { ...state, ...unitsState }
+                    }
+                }));
+            }
+        }
+    };
+
+    pubsubTokens = [];
+
+    componentDidMount() {
+        this.subscribe();
+        this.addControllerEvents();
+    }
+
+    componentWillUnmount() {
+        this.removeControllerEvents();
+        this.unsubscribe();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const {
+            minimized,
+            spindleSpeed,
+            probeFeedrate
+        } = this.state;
+
+        this.config.set('minimized', minimized);
+        this.config.set('speed', spindleSpeed);
+        this.config.set('probeFeedrate', Number(probeFeedrate));
+    }
+
+    getInitialState() {
+        return {
+            minimized: this.config.get('minimized', false),
+            spindleSpeed: this.config.get('speed', 1000),
+            probeFeedrate: Number(this.config.get('probeFeedrate') || 0).toFixed(3) * 1,
+            isFullscreen: false,
+            workflow: {
+                state: controller.workflow.state
+            },
+
+            port: controller.port,
+            units: METRIC_UNITS,
+
+            controller: {
+                type: controller.type,
+                settings: controller.settings,
+                state: controller.state
+            },
+
+            // G-code Status (from server)
+            total: 0,
+            sent: 0,
+            received: 0,
+            startTime: 0,
+            finishTime: 0,
+            elapsedTime: 0,
+            remainingTime: 0,
+
+            // Bounding box
+            bbox: {
+                min: {
+                    x: 0,
+                    y: 0,
+                    z: 0
+                },
+                max: {
+                    x: 0,
+                    y: 0,
+                    z: 0
+                },
+                delta: {
+                    x: 0,
+                    y: 0,
+                    z: 0
+                }
+            }
+        };
+    }
+
+    subscribe() {
+        const tokens = [
+            pubsub.subscribe('gcode:bbox', (msg, bbox) => {
+                const dX = bbox.max.x - bbox.min.x;
+                const dY = bbox.max.y - bbox.min.y;
+                const dZ = bbox.max.z - bbox.min.z;
+
+                this.setState({
+                    bbox: {
+                        min: {
+                            x: bbox.min.x,
+                            y: bbox.min.y,
+                            z: bbox.min.z
+                        },
+                        max: {
+                            x: bbox.max.x,
+                            y: bbox.max.y,
+                            z: bbox.max.z
+                        },
+                        delta: {
+                            x: dX,
+                            y: dY,
+                            z: dZ
+                        }
+                    }
+                });
+            })
+        ];
+        this.pubsubTokens = this.pubsubTokens.concat(tokens);
+    }
+
+    unsubscribe() {
+        this.pubsubTokens.forEach((token) => {
+            pubsub.unsubscribe(token);
+        });
+        this.pubsubTokens = [];
+    }
+
+    addControllerEvents() {
+        Object.keys(this.controllerEvents).forEach(eventName => {
+            const callback = this.controllerEvents[eventName];
+            controller.addListener(eventName, callback);
+        });
+    }
+
+    removeControllerEvents() {
+        Object.keys(this.controllerEvents).forEach(eventName => {
+            const callback = this.controllerEvents[eventName];
+            controller.removeListener(eventName, callback);
+        });
+    }
+
+    isRunningJob() {
+        const { workflow } = this.state;
+
+        if (workflow.state !== WORKFLOW_STATE_IDLE) {
+            return true;
+        }
+
+        return false;
+    }
+
+    render() {
+        const { widgetId } = this.props;
+        const { minimized, isFullscreen } = this.state;
+        const { units, bbox } = this.state;
+        const isForkedWidget = widgetId.match(/\w+:[\w\-]+/);
+        const state = {
+            ...this.state,
+            isRunningJob: this.isRunningJob(),
+            bbox: mapValues(bbox, (position) => {
+                return mapValues(position, (pos, axis) => {
+                    return mapPositionToUnits(pos, units);
+                });
+            })
+        };
+        const actions = {
+            ...this.actions
+        };
+
+        return (
+            <Widget fullscreen={isFullscreen}>
+                <Widget.Header>
+                    <Widget.Title>
+                        <Widget.Sortable className={this.props.sortable.handleClassName}>
+                            <i className="fa fa-bars" />
+                            <Space width="8" />
+                        </Widget.Sortable>
+                        {isForkedWidget &&
+                        <i className="fa fa-code-fork" style={{ marginRight: 5 }} />
+                        }
+                        {i18n._('Job Status')}
+                    </Widget.Title>
+                    <Widget.Controls className={this.props.sortable.filterClassName}>
+                        <Widget.Button
+                            disabled={isFullscreen}
+                            title={minimized ? i18n._('Expand') : i18n._('Collapse')}
+                            onClick={actions.toggleMinimized}
+                        >
+                            <i
+                                className={classNames(
+                                    'fa',
+                                    { 'fa-chevron-up': !minimized },
+                                    { 'fa-chevron-down': minimized }
+                                )}
+                            />
+                        </Widget.Button>
+                        <Widget.DropdownButton
+                            title={i18n._('More')}
+                            toggle={<i className="fa fa-ellipsis-v" />}
+                            onSelect={(eventKey) => {
+                                if (eventKey === 'fullscreen') {
+                                    actions.toggleFullscreen();
+                                } else if (eventKey === 'fork') {
+                                    this.props.onFork();
+                                } else if (eventKey === 'remove') {
+                                    this.props.onRemove();
+                                }
+                            }}
+                        >
+                            <Widget.DropdownMenuItem eventKey="fullscreen">
+                                <i
+                                    className={classNames(
+                                        'fa',
+                                        'fa-fw',
+                                        { 'fa-expand': !isFullscreen },
+                                        { 'fa-compress': isFullscreen }
+                                    )}
+                                />
+                                <Space width="4" />
+                                {!isFullscreen ? i18n._('Enter Full Screen') : i18n._('Exit Full Screen')}
+                            </Widget.DropdownMenuItem>
+                            <Widget.DropdownMenuItem eventKey="fork">
+                                <i className="fa fa-fw fa-code-fork" />
+                                <Space width="4" />
+                                {i18n._('Fork Widget')}
+                            </Widget.DropdownMenuItem>
+                            <Widget.DropdownMenuItem eventKey="remove">
+                                <i className="fa fa-fw fa-times" />
+                                <Space width="4" />
+                                {i18n._('Remove Widget')}
+                            </Widget.DropdownMenuItem>
+                        </Widget.DropdownButton>
+                    </Widget.Controls>
+                </Widget.Header>
+                <Widget.Content
+                    className={classNames(
+                        styles['widget-content'],
+                        { [styles.hidden]: minimized }
+                    )}
+                >
+                    <JobStatus
+                        state={state}
+                        actions={actions}
+                    />
+                </Widget.Content>
+            </Widget>
+        );
+    }
+}
+
+export default JobStatusWidget;
