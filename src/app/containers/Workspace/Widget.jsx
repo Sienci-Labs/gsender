d39a4f5c964a614e292dd3bd9a84d1af92345308
Walid <walid.kayhan@hotmail.com>
2021-01-12 13:04:32 -0500

    Added new job status widget to list

--- src/app/containers/Workspace/Widget.jsx
+++ src/app/containers/Workspace/Widget.jsx
@@ -5,6 +5,7 @@ import ConnectionWidget from 'app/widgets/Connection';
 import ConsoleWidget from 'app/widgets/Console';
 import GCodeWidget from 'app/widgets/GCode';
 import GrblWidget from 'app/widgets/Grbl';
+import JobStatusWidget from 'app/widgets/JobStatus';
 import LaserWidget from 'app/widgets/Laser';
 import MacroWidget from 'app/widgets/Macro';
 import MarlinWidget from 'app/widgets/Marlin';
@@ -23,6 +24,7 @@ const getWidgetByName = (name) => {
         'console': ConsoleWidget,
         'gcode': GCodeWidget,
         'grbl': GrblWidget,
+        'job_status': JobStatusWidget,
         'laser': LaserWidget,
         'macro': MacroWidget,
         'marlin': MarlinWidget,
