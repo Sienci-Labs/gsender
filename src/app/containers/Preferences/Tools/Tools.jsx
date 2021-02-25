8b44119620cdd232b49e19f93479e407014b7d31
Walid <walid.kayhan@hotmail.com>
2021-02-25 10:52:48 -0500

    Adjusted to use new styling classes

--- src/app/containers/Preferences/Tools/Tools.jsx
+++ src/app/containers/Preferences/Tools/Tools.jsx
@@ -11,16 +11,16 @@ const ToolSettings = ({ active, state, actions }) => {
     return (
         <div className={classNames(
             styles.hidden,
-            styles.settingsContainer,
+            styles['settings-wrapper'],
             { [styles.visible]: active }
         )}
         >
-            <h3>
+            <h3 className={styles['settings-title']}>
                 Tools
             </h3>
             <div className={styles.toolMain}>
                 <div className={styles.toolListings}>
-                    <h4>Available Tools</h4>
+                    <h4 className={styles['settings-subtitle']}>Available Tools</h4>
                     <div className={styles.tools}>
                         {
                             tools.map((tool, index) => (
@@ -34,7 +34,7 @@ const ToolSettings = ({ active, state, actions }) => {
                     </div>
                 </div>
                 <div className={styles.addToolForm}>
-                    <h4>Add New Tool</h4>
+                    <h4 className={styles['settings-subtitle']}>Add New Tool</h4>
                     <AddTool actions={actions} state={state} />
                 </div>
             </div>
