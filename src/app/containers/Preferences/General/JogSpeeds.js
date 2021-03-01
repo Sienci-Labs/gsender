20abdcea5d8ceb30e8df4bfe918264411a13fce1
Walid <walid.kayhan@hotmail.com>
2021-03-01 09:20:55 -0500

    Added pubsub and uniform Input components

--- src/app/containers/Preferences/General/JogSpeeds.js
+++ src/app/containers/Preferences/General/JogSpeeds.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import pubsub from 'pubsub-js';
+import Input from '../Input';
 
 import styles from '../index.styl';
 
+// pubsub.publish('jogSpeeds', { xyStep, zStep, feedrate });
 export default class JogSpeeds extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
-
     state = {
         units: 'mm',
         active: '',
+        jogspeeds: {},
     }
+
+    pubsubTokens = [];
 
     handleJogClick = (active) => {
         this.setState({ active });
+    }
+
+    subscribe() {
+        const tokens = [
+            pubsub.subscribe('jogSpeeds', (msg, speeds) => {
+                this.setState({ speeds });
+            }),
+        ];
+
+        this.pubsubTokens = this.pubsubTokens.concat(tokens);
+    }
+
+    unsubscribe() {
+        this.pubsubTokens.forEach((token) => {
+            pubsub.unsubscribe(token);
+        });
+        this.pubsubTokens = [];
+    }
+
+    componentDidMount() {
+        this.subscribe();
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
     }
 
     render() {
@@ -29,44 +55,26 @@ export default class JogSpeeds extends Component {
                     <button type="button" onClick={() => this.handleJogClick('rapid')} className={styles[active === 'rapid' ? 'jog-speed-active' : 'jog-speed-inactive']}>Rapid</button>
                 </div>
                 <div className={styles['jog-spead-wrapper']}>
-                    <div className="form-group" style={{ margin: 0 }}>
-                        <label htmlFor="">XY Move ({units})</label>
-                        <div className="input-group" style={{ display: 'block' }}>
-                            <input
-                                type="text"
-                                name="depth"
-                                className="form-control"
-                                style={{ zIndex: '0', fontSize: '1.5rem', textAlign: 'center', color: '#3e85c7' }}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                    </div>
 
-                    <div className="form-group" style={{ margin: 0 }}>
-                        <label htmlFor="">Z Move ({units})</label>
-                        <div className="input-group" style={{ display: 'block' }}>
-                            <input
-                                type="text"
-                                name="depth"
-                                className="form-control"
-                                style={{ zIndex: '0', fontSize: '1.5rem', textAlign: 'center', color: '#3e85c7' }}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                    </div>
+                    <Input
+                        label="XY Move"
+                        units={units}
+                        onChange={this.handleChange}
+                        additionalProps={{ type: 'number' }}
+                    />
 
-                    <div className="form-group" style={{ margin: 0 }}>
-                        <label htmlFor="">Speed ({`${units}/min`})</label>
-                        <div className="input-group" style={{ display: 'block' }}>
-                            <input
-                                type="text"
-                                name="depth"
-                                className="form-control"
-                                style={{ zIndex: '0', fontSize: '1.5rem', textAlign: 'center', color: '#3e85c7' }}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                    </div>
+                    <Input
+                        label="Z Move"
+                        units={units}
+                        onChange={this.handleChange}
+                        additionalProps={{ type: 'number' }}
+                    />
+                    <Input
+                        label="Speed"
+                        units={`${units}/min`}
+                        onChange={this.handleChange}
+                        additionalProps={{ type: 'number' }}
+                    />
                 </div>
             </div>
         );
