30dd0c8396b2b3df9eb39b986a9907fc41920a53
Walid <walid.kayhan@hotmail.com>
2021-03-10 13:36:10 -0500

    Added modal for invalid gcode lines

---
+++ src/app/widgets/Visualizer/ValidationModal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'app/components/Modal';
+import { Button } from 'app/components/Buttons';
+
+import { modalStyle, modalHeaderStyle, modalTitleStyle, modalBodyStyle, modalFooterStyle } from './modalStyle';
+
+const ValidationModal = ({ onClose, invalidGcode }) => {
+    const { list } = invalidGcode;
+
+    return (
+        <Modal
+            size="xs"
+            onClose={onClose}
+            style={modalStyle}
+        >
+            <Modal.Header style={modalHeaderStyle}>
+                <Modal.Title style={modalTitleStyle}>
+                    Invalid G-Code Warning
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={modalBodyStyle}>
+
+                {
+                    list?.size === 1
+                        ? <p>There is <span style={{ fontWeight: 'bold' }}>1</span> invalid G-Code line in the file, your job may not run properly.</p>
+                        : <p>There are <span style={{ fontWeight: 'bold' }}>{list?.size}</span> invalid G-Code lines in the file, your job may not run properly.</p>
+                }
+
+            </Modal.Body>
+            <Modal.Footer style={modalFooterStyle}>
+                <Button
+                    style={{ backgroundColor: '#3e85c7', color: 'white', backgroundImage: 'none' }}
+                    onClick={onClose}
+                >
+                    Proceed
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+ValidationModal.propTypes = {
+    onClose: PropTypes.func,
+    invalidGcode: PropTypes.object,
+};
+
+export default ValidationModal;
---
+++ src/app/widgets/Visualizer/modalStyle.js
@@ -0,0 +1,22 @@
+export const modalStyle = {
+    border: 'none',
+    backgroundColor: '#d1d5db'
+};
+
+export const modalHeaderStyle = {
+    backgroundColor: '#d1d5db',
+    border: 'none'
+};
+
+export const modalTitleStyle = {
+    color: '#dc2626',
+    height: '22px',
+};
+
+export const modalBodyStyle = {
+    fontSize: '1.25rem'
+};
+
+export const modalFooterStyle = {
+    backgroundColor: '#d1d5db'
+};
