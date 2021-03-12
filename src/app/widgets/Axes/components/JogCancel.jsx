4cce9360b5d1d36be0560b1dec941135e1d40e67
KGN <kevin.glovernetherton@gmail.com>
2021-03-12 09:21:01 -0500

    Styling changes to state of jog cancel button to show it's active when moving

--- src/app/widgets/Axes/components/JogCancel.jsx
+++ src/app/widgets/Axes/components/JogCancel.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import cx from 'classnames';
+import { GRBL_ACTIVE_STATE_JOG, GRBL_ACTIVE_STATE_RUN } from 'app/constants';
 import styles from '../index.styl';
 
-const JogCancel = (props) => {
+const JogCancel = ({ activeState, ...props }) => {
     return (
         <button
             {...props}
-            className={styles.jogCancelButton}
+            className={cx(styles.jogCancelButton, { [styles.jogActive]: (activeState === GRBL_ACTIVE_STATE_RUN || activeState === GRBL_ACTIVE_STATE_JOG) })}
         >
             <i className="fas fa-ban" />
         </button>
