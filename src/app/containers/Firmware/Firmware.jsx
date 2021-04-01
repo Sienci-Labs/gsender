543bda2c42cc21da8b09fbe4fa9a6bdd3669de04
KGN <kevin.glovernetherton@gmail.com>
2021-04-01 16:20:03 -0400

    Removed call to backend that was causing firmware wizard to crash

--- src/app/containers/Firmware/Firmware.jsx
+++ src/app/containers/Firmware/Firmware.jsx
@@ -48,7 +48,7 @@ class Firmware extends PureComponent {
 
     componentDidMount() {
         this.addControllerEvents();
-        controller.command('firmware:grabMachineProfile');
+        //controller.command('firmware:grabMachineProfile');
     }
 
     componentWillUnmount() {
