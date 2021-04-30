63cdcae909ecfa1397e96d0b670c1349e1b09bb7
KGN <kevin.glovernetherton@gmail.com>
2021-04-30 15:12:04 -0400

    Updated copyright, changed homing notice logic

--- src/app/widgets/Visualizer/MachineStatusArea.js
+++ src/app/widgets/Visualizer/MachineStatusArea.js
@@ -1,4 +1,27 @@
 /*
+ * Copyright (C) 2021 Sienci Labs Inc.
+ *
+ * This file is part of gSender.
+ *
+ * gSender is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, under version 3 of the License.
+ *
+ * gSender is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gSender.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact for information regarding this program and its license
+ * can be sent through gSender@sienci.com or mailed to the main office
+ * of Sienci Labs Inc. in Waterloo, Ontario, Canada.
+ *
+ */
+
+/*
  *     This file is part of gSender.
  *
  *     gSender is free software: you can redistribute it and/or modify
@@ -20,6 +43,7 @@ import PropTypes from 'prop-types';
 import controller from 'app/lib/controller';
 import styles from './machine-status-area.styl';
 import UnlockAlarmButton from './UnlockAlarmButton';
+import { GRBL_ACTIVE_STATE_HOME } from '../../constants';
 
 /**
  * Control Area component displaying machine status
@@ -41,6 +65,7 @@ export default class ControlArea extends Component {
     }
 
     handleHomeMachine = () => {
+        controller.command('unlock');
         controller.command('homing');
     }
 
@@ -66,20 +91,20 @@ export default class ControlArea extends Component {
          */
         const machineStateRender = () => {
             if (port) {
-                if (this.props.state.controller.state !== undefined) {
-                    if (this.props.state.controller.state.status !== undefined) {
-                        let alarmCode = this.props.state.controller.state.status.alarmCode;
-                        let softLimitsEnabled = this.props.state.controller.settings.settings.$20;
-                        if (alarmCode === 2 && softLimitsEnabled === '1') {
-                            return (
-                                <div className={styles['machine-status-wrapper']}>
-                                    <div className={styles['machine-Alarm']}>
-                                        {state.status.activeState} ({state.status.alarmCode}){' '}
-                                    </div>
-                                    <UnlockAlarmButton newMessage="Click To Home Machine" onClick={this.handleHomeMachine} />
+                if (controller?.state?.status) {
+                    let alarmCode = controller.state.status.alarmCode;
+                    let homingEnabled = controller.settings.settings.$22;
+                    if (alarmCode === 'Homing' && homingEnabled === '1') {
+                        return (
+                            <div className={styles['machine-status-wrapper']}>
+                                <div className={styles[`machine${state.status.activeState}`]}>
+                                    {
+                                        state.status.activeState === GRBL_ACTIVE_STATE_HOME ? 'Homing...' : 'Run Homing'
+                                    }
                                 </div>
-                            );
-                        }
+                                <UnlockAlarmButton newMessage="Click To Home Machine" onClick={this.handleHomeMachine} />
+                            </div>
+                        );
                     }
                 }
                 if (state.status?.activeState === 'Alarm') {
