e4bd0d63b685b116cabbb0e9e0bab4983d43cc4d
Walid Kayhan <walid.kayhan@hotmail.com>
2021-04-30 10:04:29 -0400

    Added additional keymaps

--- src/app/containers/Preferences/Keybindings/EditArea.js
+++ src/app/containers/Preferences/Keybindings/EditArea.js
@@ -72,7 +72,10 @@ export default class EditArea extends Component {
             ArrowRight: 'right',
             ArrowUp: 'up',
             ArrowDown: 'down',
-            Delete: 'delete',
+            Delete: 'del',
+            Insert: 'ins',
+            End: 'end',
+            Home: 'home',
         }[e.key];
 
         const key = keyMap || e.key.toLowerCase();
