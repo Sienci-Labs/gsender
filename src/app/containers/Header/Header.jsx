81d2398dc3489613cc86effa808d2ccaa4b38dc1
KGN <kevin.glovernetherton@gmail.com>
2021-05-06 14:03:18 -0400

    removed further test code and logs

--- src/app/containers/Header/Header.jsx
+++ src/app/containers/Header/Header.jsx
@@ -20,24 +20,6 @@
  * of Sienci Labs Inc. in Waterloo, Ontario, Canada.
  *
  */
-
-/*
- *     This file is part of gSender.
- *
- *     gSender is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     gSender is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with gSender.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
 import without from 'lodash/without';
@@ -53,7 +35,6 @@ import NavbarConnection from 'app/widgets/NavbarConnection';
 import styles from './index.styl';
 import NavLogo from '../../components/NavLogo';
 import NavSidebar from '../NavSidebar';
-import FunctionButton from '../../components/FunctionButton/FunctionButton';
 //const releases = 'https://github.com/cncjs/cncjs/releases';
 
 
