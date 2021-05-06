a73a811bed8c0d23c892052b70e4b79a5f88190c
KGN <kevin.glovernetherton@gmail.com>
2021-05-06 10:50:53 -0400

    Moved to customizable controller

--- src/app/lib/controller.js
+++ src/app/lib/controller.js
@@ -20,26 +20,8 @@
  * of Sienci Labs Inc. in Waterloo, Ontario, Canada.
  *
  */
-
-/*
- *     This file is part of gSender.
- *
- *     gSender is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     gSender is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with gSender.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-import Controller from 'cncjs-controller';
 import io from 'socket.io-client';
+import Controller from './controller-base';
 
 const controller = new Controller(io);
 
