a8060633cf3e9172eb2e5812d201b3ca64dff213
KGN <kevin.glovernetherton@gmail.com>
2021-05-18 09:39:44 -0400

    Added redux dev tools if in dev environment

--- src/app/store/redux/index.js
+++ src/app/store/redux/index.js
@@ -21,15 +21,29 @@
  *
  */
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { END } from 'redux-saga';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import mainReducer from 'app/reducers';
 import sagaMiddleware from './saga';
 
-const enhancer = applyMiddleware(thunk, sagaMiddleware);
+const env = process.env.NODE_ENV;
+
+let enhancer;
 
 const configureStore = (preloadedState) => {
+    if (env === 'production') {
+        console.log('p');
+        enhancer = applyMiddleware(thunk, sagaMiddleware);
+    } else {
+        console.log('d');
+        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        enhancer = composeEnhancers(
+            applyMiddleware(thunk, sagaMiddleware, createLogger({ collapsed: true })),
+        );
+    }
+
     const store = createStore(mainReducer, preloadedState, enhancer);
     store.close = () => store.dispatch(END);
     store.runSaga = sagaMiddleware.run;
