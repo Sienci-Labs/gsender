d4feae2b6f50a21d29c3bc801914779c6f5c2db4
KGN <kevin.glovernetherton@gmail.com>
2021-05-27 15:48:53 -0400

    Added menu for console testing

--- src/electron-app/WindowManager.js
+++ src/electron-app/WindowManager.js
@@ -94,7 +94,7 @@ class WindowManager {
             }
         });
         const webContents = window.webContents;
-        window.removeMenu();
+        //window.removeMenu();
         window.webContents.on('did-finish-load', () => {
             window.setTitle(options.title);
         });
