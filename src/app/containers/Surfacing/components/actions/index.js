7953bddf7153241a2d049e8db6c208752adee7b6
Walid Kayhan <walid.kayhan@hotmail.com>
2021-05-27 09:57:43 -0400

    Removed cancel and clear buttons

--- src/app/containers/Surfacing/components/actions/index.js
+++ src/app/containers/Surfacing/components/actions/index.js
@@ -4,13 +4,11 @@ import FunctionButton from 'app/components/FunctionButton/FunctionButton';
 
 import styles from './index.styl';
 
-const Actions = ({ handleCancel, handleClear, handleGenerateGcode, handleLoadGcode, surfacing, canLoad }) => {
+const Actions = ({ handleCancel, handleGenerateGcode, handleLoadGcode, surfacing, canLoad }) => {
     const canGenerateGcode = Object.keys(surfacing).every(key => surfacing[key]);
 
     return (
         <div className={styles.container}>
-            <FunctionButton onClick={handleCancel}>Cancel</FunctionButton>
-            <FunctionButton onClick={handleClear}>Clear G-code</FunctionButton>
             <FunctionButton primary onClick={handleGenerateGcode} disabled={!canGenerateGcode}>Generate G-code</FunctionButton>
             <FunctionButton primary disabled={!canLoad} onClick={handleLoadGcode}>Run on Main Visualizer</FunctionButton>
         </div>
@@ -19,7 +17,6 @@ const Actions = ({ handleCancel, handleClear, handleGenerateGcode, handleLoadGco
 
 Actions.propTypes = {
     handleCancel: PropTypes.func,
-    handleClear: PropTypes.func,
     handleGenerateGcode: PropTypes.func,
     handleLoadGcode: PropTypes.func,
     surfacing: PropTypes.object,
