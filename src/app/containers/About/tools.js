3edd96f7f5d7acd929d16f654fc6f86c5a47cd08
Walid Kayhan <walid.kayhan@hotmail.com>
2021-05-28 15:03:29 -0400

    Added data for about modal

---
+++ src/app/containers/About/tools.js
@@ -0,0 +1,52 @@
+export const team = [
+    {
+        id: 0,
+        name: 'Walid Kayhan',
+        title: 'Development',
+    },
+    {
+        id: 1,
+        name: 'Kevin Glover-Netherton',
+        title: 'Development',
+    },
+    {
+        id: 2,
+        name: 'Justin Wozniak',
+        title: 'Development',
+    },
+    {
+        id: 3,
+        name: 'Chris Thorogood',
+        title: 'Administration',
+    },
+    {
+        id: 4,
+        name: 'Kelly Zhu',
+        title: 'Graphic Design',
+        isLastInList: true
+    },
+];
+
+export const timeline = [
+    {
+        id: 0,
+        date: 'January 29th',
+        text: 'Closed Alpha Release'
+    },
+    {
+        id: 1,
+        date: 'March 5th',
+        text: 'Closed Beta Release'
+    },
+    {
+        id: 2,
+        date: 'May 14th',
+        text: 'Closed Beta Release'
+    },
+    {
+        id: 3,
+        date: 'TBA',
+        text: 'Full Version Release',
+        isLastInList: true
+    },
+];
