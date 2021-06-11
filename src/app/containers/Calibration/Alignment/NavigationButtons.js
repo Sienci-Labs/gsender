ba2001a45f211bbe06b2017719b0dd5fdb759b15
Walid Kayhan <walid.kayhan@hotmail.com>
2021-06-11 14:18:07 -0400

    Added button for showing jog keypad

--- src/app/containers/Calibration/Alignment/NavigationButtons.js
+++ src/app/containers/Calibration/Alignment/NavigationButtons.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import FunctionButton from 'app/components/FunctionButton/FunctionButton';
 
-const NavigationButtons = ({ onNext, onPrevious, nextDisabled, prevDisabled }) => {
+const NavigationButtons = ({ onNext, onPrevious, nextDisabled, prevDisabled, onShowJogControls }) => {
     return (
-        <div style={{ display: 'flex', gap: '1rem' }}>
-            <FunctionButton primary onClick={onPrevious} disabled={!prevDisabled}>Previous</FunctionButton>
-            <FunctionButton primary onClick={onNext} disabled={!nextDisabled}>Next</FunctionButton>
+        <div style={{ display: 'grid', gridTemplateRows: '1fr 1fr' }}>
+            <div style={{ display: 'flex', gap: '1rem' }}>
+                <FunctionButton primary onClick={onPrevious} disabled={!prevDisabled}>Previous</FunctionButton>
+                <FunctionButton primary onClick={onNext} disabled={!nextDisabled}>Next</FunctionButton>
+            </div>
+            <FunctionButton onClick={onShowJogControls}>Show Jog Controls</FunctionButton>
         </div>
     );
 };
