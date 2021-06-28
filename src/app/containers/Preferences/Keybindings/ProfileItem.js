8156d9ce1326f902f61aca595a70b9baf2c8dbcf
Walid Kayhan <walid.kayhan@hotmail.com>
2021-06-28 12:37:36 -0400

    Added profile item component

---
+++ src/app/containers/Preferences/Keybindings/ProfileItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import styles from './index.styl';
+
+const ProfileItem = ({ title, icon, id, onClick, onDelete }) => {
+    return (
+        <div
+            tabIndex={-1}
+            role="button"
+            onClick={() => onClick(id)}
+            onKeyDown={null}
+            className={styles.profileItem}
+        >
+            <i className={classnames(icon, styles.profileItemIcon)} />
+            <div className={styles.profileItemTitle}>{title}</div>
+
+            <i className={classnames('fas fa-circle', styles.profileItemStatus)} />
+
+            <i
+                tabIndex={-1}
+                role="button"
+                onClick={(e) => {
+                    e.stopPropagation(); //Prevents bubbling that will fire the parent div's onclick first
+                    onDelete(id);
+                }}
+                onKeyDown={null}
+                className={classnames('fas fa-times', styles.profileItemDelete)}
+            />
+        </div>
+    );
+};
+ProfileItem.propTypes = {
+    title: PropTypes.string,
+    icon: PropTypes.string,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    active: PropTypes.bool,
+    onClick: PropTypes.func,
+    onDelete: PropTypes.func,
+    showActiveStatus: PropTypes.bool,
+    showDeleteButton: PropTypes.bool,
+};
+
+export default ProfileItem;
