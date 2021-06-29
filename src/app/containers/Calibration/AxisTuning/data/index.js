e68e8784eaec947abffa8b5066958b062145b21f
KGN <kevin.glovernetherton@gmail.com>
2021-06-29 14:29:35 -0400

    Updated copy to mark on gantry instead of wasteboard

--- src/app/containers/Calibration/AxisTuning/data/index.js
+++ src/app/containers/Calibration/AxisTuning/data/index.js
@@ -35,35 +35,35 @@ export const yAxisStep = [
         checked: false,
         hasBeenChanged: false,
         label: () => {
-            return <span>Mark Location 1</span>;
+            return <span>Mark first location</span>;
         },
         shapeActions: [
             {
                 shapeType: 'circlePoints',
-                shapeID: 0,
+                shapeID: 1,
                 isActive: true,
                 show: true,
                 clearPrevious: true,
-                label: '1'
+                label: '2',
             },
         ],
-        description: 'This is the initial mark you will make. Like mentioned before, marking is simply drawing an X on a piece of tape and placing it on your wasteboard. In this instance, you would put it directly under where your cutting bit is where the end of the bit is pointing directly of the middle of the X you drew.'
+        description: 'Mark your machine here directly on the gantry at the current position using the edge of a piece of tape or pencil.'
     },
     {
         id: 1,
         checked: false,
         hasBeenChanged: false,
-        label: ({ isCurrentAction }) => {
+        hideCompleteButton: true,
+        label: ({ isCurrentAction, onChange, setRequestedDistance }) => {
             const [val, setVal] = useState(100);
             const [didClick, setDidClick] = useState(false);
+            const AXIS = 'Y';
 
             const handleClick = () => {
-                const { wpos } = controller?.state?.status;
-
-                const value = (Number(wpos.y) - Number(val)).toFixed(3);
-
+                setRequestedDistance(val);
                 setDidClick(true); //Disable button
-                controller.command('gcode', `G0 Y${value}`);
+                jogMachine({ axis: AXIS, value: val });
+                onChange({ axis: AXIS.toLowerCase(), value: val });
             };
 
             return (
@@ -91,33 +91,85 @@ export const yAxisStep = [
         shapeActions: [
             {
                 shapeType: 'arrows',
-                shapeID: 0,
+                shapeID: 1,
                 isActive: true,
                 show: true,
                 clearPrevious: true,
                 label: 'Y-Axis Movement'
             },
         ],
-        description: 'Here you will jog your machine in the Y-axis in the distance given which is customizable. Once you are ready, click the "Move Y-Axis" button.'
+        description: 'Here you will jog your machine in the Y-axis by a customizable distance.' +
+            'Larger distances are better but make sure you don\'t hit your machine limits. Once you are ready, click the "Move Y-Axis" button.'
     },
     {
         id: 2,
         checked: false,
         hasBeenChanged: false,
         label: () => {
-            return <span>Mark Location 2</span>;
+            return <span>Mark second location</span>;
         },
         shapeActions: [
             {
                 shapeType: 'circlePoints',
-                shapeID: 1,
+                shapeID: 2,
                 isActive: true,
                 show: true,
                 clearPrevious: true,
-                label: '2',
+                label: '3',
             },
         ],
-        description: 'You will mark your machine here once again where you must place the piece tape directly under the cutting bit and make sure the end of the bit is pointing directly of the middle of the X.'
+        description: 'Here you will make one last mark on the gantry using the tape or pencil. Once you\'ve done so, you may check off this step off and proceed to the next page'
+    },
+    {
+        id: 3,
+        checked: false,
+        hasBeenChanged: false,
+        hideCompleteButton: true,
+        label: ({ isCurrentAction, onChange, options, setActualDistance }) => {
+            const [val, setVal] = useState(100);
+            const AXIS = 'Y';
+
+            const handleClick = () => {
+                setActualDistance(val);
+                onChange({ axis: AXIS.toLowerCase(), value: val });
+            };
+
+            return (
+                <div style={{ display: 'flex', gap: '0.5rem' }}>
+                    <Button
+                        primary
+                        disabled={!isCurrentAction}
+                        style={buttonStyle}
+                        onClick={handleClick}
+                    >
+                        Set travel
+                    </Button>
+                    <Input
+                        className={styles.actionInput}
+                        style={inputStyle}
+                        disabled={!isCurrentAction}
+                        units="mm"
+                        value={val}
+                        onChange={(e) => {
+                            setVal(e.target.value);
+                        }}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </div>
+
+            );
+        },
+        shapeActions: [
+            {
+                shapeType: 'circlePoints',
+                shapeID: 2,
+                isActive: true,
+                show: true,
+                clearPrevious: true,
+                label: '3',
+            },
+        ],
+        description: 'Measure the distance travelled between point A and point B and enter it in the provided input.  The more accurate the measurement, the more accurate the motor calibration will be.'
     },
 ];
 
@@ -139,7 +191,7 @@ export const xAxisStep = [
                 label: '2',
             },
         ],
-        description: 'Mark your machine here by placing the piece of tape directly under the cutting bit and make sure the end of the bit is pointing directly to the middle of the X.'
+        description: 'Mark your machine here directly on the gantry at the current position using the edge of a piece of tape or pencil.'
     },
     {
         id: 1,
@@ -210,7 +262,7 @@ export const xAxisStep = [
                 label: '3',
             },
         ],
-        description: 'Here you will make one last mark using the tape. Once you\'ve done so, you may check off this step off and proceed to the next page'
+        description: 'Here you will make one last mark on the gantry using the tape or pencil. Once you\'ve done so, you may check off this step off and proceed to the next page'
     },
     {
         id: 3,
@@ -265,7 +317,152 @@ export const xAxisStep = [
     },
 ];
 
+export const zAxisStep = [
+    {
+        id: 0,
+        checked: false,
+        hasBeenChanged: false,
+        label: () => {
+            return <span>Mark first location</span>;
+        },
+        shapeActions: [
+            {
+                shapeType: 'circlePoints',
+                shapeID: 1,
+                isActive: true,
+                show: true,
+                clearPrevious: true,
+                label: '2',
+            },
+        ],
+        description: 'Mark your machine here directly on the gantry at the current position using the edge of a piece of tape or pencil.'
+    },
+    {
+        id: 1,
+        checked: false,
+        hasBeenChanged: false,
+        hideCompleteButton: true,
+        label: ({ isCurrentAction, onChange, setRequestedDistance }) => {
+            const [val, setVal] = useState(30);
+            const [didClick, setDidClick] = useState(false);
+            const AXIS = 'Z';
+
+            const handleClick = () => {
+                setRequestedDistance(val);
+                setDidClick(true); //Disable button
+                jogMachine({ axis: AXIS, value: val });
+                onChange({ axis: AXIS.toLowerCase(), value: val });
+            };
+
+            return (
+                <div style={{ display: 'flex', gap: '0.5rem' }}>
+                    <Button
+                        primary
+                        disabled={!isCurrentAction || didClick}
+                        style={buttonStyle}
+                        onClick={handleClick}
+                    >
+                        Move Z-Axis
+                    </Button>{' '}
+                    <Input
+                        className={styles.actionInput}
+                        style={inputStyle}
+                        disabled={!isCurrentAction}
+                        units="mm"
+                        value={val}
+                        onChange={(e) => setVal(e.target.value)}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </div>
+            );
+        },
+        shapeActions: [
+            {
+                shapeType: 'arrows',
+                shapeID: 1,
+                isActive: true,
+                show: true,
+                clearPrevious: true,
+                label: 'Z-Axis Movement'
+            },
+        ],
+        description: 'Here you will jog your machine in the Z-axis by a customizable distance.' +
+            'Larger distances are better but make sure you don\'t hit your machine limits. Once you are ready, click the "Move Z-Axis" button.'
+    },
+    {
+        id: 2,
+        checked: false,
+        hasBeenChanged: false,
+        label: () => {
+            return <span>Mark second location</span>;
+        },
+        shapeActions: [
+            {
+                shapeType: 'circlePoints',
+                shapeID: 2,
+                isActive: true,
+                show: true,
+                clearPrevious: true,
+                label: '3',
+            },
+        ],
+        description: 'Here you will make one last mark on the gantry using the tape or pencil. Once you\'ve done so, you may check off this step off and proceed to the next page'
+    },
+    {
+        id: 3,
+        checked: false,
+        hasBeenChanged: false,
+        hideCompleteButton: true,
+        label: ({ isCurrentAction, onChange, options, setActualDistance }) => {
+            const [val, setVal] = useState(100);
+            const AXIS = 'Z';
+
+            const handleClick = () => {
+                setActualDistance(val);
+                onChange({ axis: AXIS.toLowerCase(), value: val });
+            };
+
+            return (
+                <div style={{ display: 'flex', gap: '0.5rem' }}>
+                    <Button
+                        primary
+                        disabled={!isCurrentAction}
+                        style={buttonStyle}
+                        onClick={handleClick}
+                    >
+                        Set travel
+                    </Button>
+                    <Input
+                        className={styles.actionInput}
+                        style={inputStyle}
+                        disabled={!isCurrentAction}
+                        units="mm"
+                        value={val}
+                        onChange={(e) => {
+                            setVal(e.target.value);
+                        }}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </div>
+
+            );
+        },
+        shapeActions: [
+            {
+                shapeType: 'circlePoints',
+                shapeID: 2,
+                isActive: true,
+                show: true,
+                clearPrevious: true,
+                label: '3',
+            },
+        ],
+        description: 'Measure the distance travelled between point A and point B and enter it in the provided input.  The more accurate the measurement, the more accurate the motor calibration will be.'
+    },
+];
+
 export const axisSteps = {
     x: xAxisStep,
     y: yAxisStep,
+    z: zAxisStep
 };
