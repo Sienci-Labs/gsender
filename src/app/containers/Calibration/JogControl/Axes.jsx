b3b80dd5a499798fb251499f0691348a33553d1e
Walid Kayhan <walid.kayhan@hotmail.com>
2021-06-30 14:46:56 -0400

    Removed styling for calibration keypad component

--- src/app/containers/Calibration/JogControl/Axes.jsx
+++ src/app/containers/Calibration/JogControl/Axes.jsx
@@ -24,24 +24,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Keypad from './Keypad';
-import styles from './index.styl';
 
 const Axes = (props) => {
     const { state, actions } = props;
     return (
-        <div className={styles.jogWidget}>
-            <Keypad
-                canClick={state.canClick}
-                units={state.units}
-                axes={state.axes}
-                jog={state.jog}
-                actions={actions}
-                isJogging={state.isJogging}
-                activeState={state.activeState}
-                selectedSpeed={state.selectedSpeed}
-                canJog={state.canJog}
-            />
-        </div>
+        <Keypad
+            canClick={state.canClick}
+            units={state.units}
+            axes={state.axes}
+            jog={state.jog}
+            actions={actions}
+            isJogging={state.isJogging}
+            activeState={state.activeState}
+            selectedSpeed={state.selectedSpeed}
+            canJog={state.canJog}
+        />
     );
 };
 
