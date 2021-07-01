3456f88f6d53e4bf7874947d5d6c12cf2666e009
KGN <kevin.glovernetherton@gmail.com>
2021-07-01 09:22:31 -0400

    Added key to image map

--- src/app/containers/Calibration/AxisTuning/ImageDiagram.jsx
+++ src/app/containers/Calibration/AxisTuning/ImageDiagram.jsx
@@ -4,11 +4,13 @@ import cx from 'classnames';
 import styles from './index.styl';
 
 const ImageDiagram = ({ actions, currentAction }) => {
+    console.log(currentAction);
     return (
         <div className={styles.diagramWrapper}>
             {
                 actions.map(action => {
                     return <img
+                        key={`diagram-${action.id}`}
                         src={action.image}
                         alt="Axis tuning diagram"
                         className={cx({ [styles.hidden]: action.id !== currentAction })}
