000a4742401f787d69c8304ec8e18d147f616ab6
KGN <kevin.glovernetherton@gmail.com>
2021-07-06 15:46:20 -0400

    Updates to how auto-update works to avoid always downloading latest version

--- src/app/containers/Header/Header.jsx
+++ src/app/containers/Header/Header.jsx
@@ -263,11 +263,14 @@ class Header extends PureComponent {
         window.ipcRenderer.on('message', (event, msg) => {
             console.log(msg);
         });
-        window.ipcRenderer.once('update_downloaded', () => {
+        window.ipcRenderer.on('updated_error', (event, err) => {
+            console.log(err);
+        });
+        window.ipcRenderer.once('update_available', (info) => {
             this.setState({
                 updateAvailable: true
             });
-            pubsub.publish('showUpdateToast');
+            pubsub.publish('showUpdateToast', info);
         });
     }
 
