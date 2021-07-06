22d4f5bcf944886bed4b14b622bd7d5b248e03f8
Walid Kayhan <walid.kayhan@hotmail.com>
2021-07-06 11:13:42 -0400

    Added reusable main table component instead

--- src/app/containers/Preferences/Keybindings/Profile.js
+++ src/app/containers/Preferences/Keybindings/Profile.js
@@ -1,37 +1,27 @@
 import React from 'react';
 
-// import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import Table from 'app/components/Table';
-
 import styles from './index.styl';
-
 import ProfileItem from './ProfileItem';
+import MainTable from './MainTable';
 
 const Profile = ({ currentProfile, active, onClick, onDelete, shortcut }) => {
-    const { title, icon, id, data } = currentProfile;
-
-    const columns = [
-        { dataIndex: 'id', title: 'ID', sortable: true, key: 'id', width: '10%' },
-        { dataIndex: 'name', title: 'Name', sortable: true, key: 'name', width: '40%', },
-        { dataIndex: 'action', title: 'Action', key: 'action', width: '40%', },
-        { dataIndex: 'active', title: 'Active', key: 'active', width: '10%', },
-    ];
+    const { profileName, icon, shortcuts } = currentProfile;
 
     return (
-        <div style={{ overflowY: 'auto' }}>
-            <h3 style={{ margin: '1rem 0 2rem' }}>
-                <i className={classnames(icon, styles.profileItemIcon)} /> {title}{' '}
-                <small>(ID: {id})</small>
-            </h3>
-            <Table
-                bordered
-                rowKey="id"
-                columns={columns}
-                data={data}
-                emptyText={() => 'No Actions for This Profile'}
-            />
+        <div style={{ overflowY: 'clip' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 11fr', alignItems: 'center', margin: '0 0 1rem' }}>
+                <i className={classnames(icon, styles.profileItemIcon)} /> <div style={{ fontSize: '1.5rem' }}>{profileName}</div>
+            </div>
+            <div style={{ overflowY: 'auto', height: '100%' }}>
+                <MainTable
+                    onEdit={() => console.log('Editing...')}
+                    onDelete={() => console.log('Deleting...')}
+                    onShortcutToggle={() => console.log('Shortcut Toggle...')}
+                    data={shortcuts}
+                />
+            </div>
         </div>
     );
 };
