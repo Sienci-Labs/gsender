538c7ba0c68beaa55b9ba8751b28bf452849a4dd
Walid Kayhan <walid.kayhan@hotmail.com>
2021-07-07 09:59:13 -0400

    Added table and shortcut modal

--- src/app/containers/Preferences/Keybindings/Profile.js
+++ src/app/containers/Preferences/Keybindings/Profile.js
@@ -1,28 +1,44 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import classnames from 'classnames';
 
 import styles from './index.styl';
 import ProfileItem from './ProfileItem';
 import MainTable from './MainTable';
+import ProfileShortcutModal from './ProfileShortcutModal';
 
-const Profile = ({ currentProfile, active, onClick, onDelete, shortcut }) => {
+const Profile = ({ currentProfile }) => {
     const { profileName, icon, shortcuts } = currentProfile;
 
+    const [currentShortcut, setCurrentShortcut] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
     return (
-        <div style={{ overflowY: 'clip' }}>
-            <div style={{ display: 'grid', gridTemplateColumns: '1fr 11fr', alignItems: 'center', margin: '0 0 1rem' }}>
-                <i className={classnames(icon, styles.profileItemIcon)} /> <div style={{ fontSize: '1.5rem' }}>{profileName}</div>
+        <>
+            <div style={{ overflowY: 'clip' }}>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr 11fr', alignItems: 'center', margin: '0 0 1rem' }}>
+                    <i className={classnames(icon, styles.profileItemIcon)} /> <div style={{ fontSize: '1.5rem' }}>{profileName}</div>
+                </div>
+                <div style={{ overflowY: 'auto', height: '90%', backgroundColor: 'white' }}>
+                    <MainTable
+                        onEdit={(shortcut) => {
+                            setShowModal(true);
+                            setCurrentShortcut(shortcut);
+                        }}
+                        onDelete={() => console.log('Deleting...')}
+                        onShortcutToggle={() => console.log('Shortcut Toggle...')}
+                        data={shortcuts}
+                    />
+                </div>
             </div>
-            <div style={{ overflowY: 'auto', height: '100%' }}>
-                <MainTable
-                    onEdit={() => console.log('Editing...')}
-                    onDelete={() => console.log('Deleting...')}
-                    onShortcutToggle={() => console.log('Shortcut Toggle...')}
-                    data={shortcuts}
+
+            {showModal && (
+                <ProfileShortcutModal
+                    profile={currentProfile}
+                    shortcut={currentShortcut}
+                    onClose={() => setShowModal(false)}
                 />
-            </div>
-        </div>
+            )}
+        </>
     );
 };
 
