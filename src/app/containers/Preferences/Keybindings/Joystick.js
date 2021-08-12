5440e2d5c67108b38b6da9f9292a44dcf8863e58
Walid Kayhan <walid.kayhan@hotmail.com>
2021-08-12 11:44:08 -0400

    Added logic to update profile

--- src/app/containers/Preferences/Keybindings/Joystick.js
+++ src/app/containers/Preferences/Keybindings/Joystick.js
@@ -40,18 +40,53 @@ const Joystick = () => {
         });
     };
 
+    const handleUpdateProfiles = (updatedProfiles) => {
+        setProfiles(updatedProfiles);
+        store.replace('workspace.gamepad.profiles', updatedProfiles);
+
+        const updatedCurrentProfile = updatedProfiles.find(profile => profile.id === currentProfile.id);
+
+        setCurrentProfile(updatedCurrentProfile);
+    };
+
+    const handleShortcutsToggle = (toggle) => {
+        const updatedShortcuts = currentProfile.shortcuts.map((shortcut) => ({ ...shortcut, isActive: toggle }));
+
+        const profiles = store.get('workspace.gamepad.profiles', []);
+
+        const updatedProfiles =
+            profiles.map(profile => (currentProfile.id === profile.id ? ({ ...profile, shortcuts: updatedShortcuts }) : currentProfile));
+
+        handleUpdateProfiles(updatedProfiles);
+    };
+
+    const allShortcutsEnabled = currentProfile?.shortcuts?.every(shortcut => shortcut.isActive);
+    const allShortcutsDisabled = currentProfile?.shortcuts?.every(shortcut => !shortcut.isActive);
+
     return (
         <div className={styles.container}>
             {
                 currentProfile
                     ? (
                         <>
-                            <Profile currentProfile={currentProfile} />
+                            <Profile currentProfile={currentProfile} onUpdateProfiles={handleUpdateProfiles} />
 
-                            <Button primary onClick={() => setCurrentProfile(null)}>
-                                <i className="fas fa-arrow-left" />
-                                <span>Back to Profiles List</span>
-                            </Button>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <div style={{ display: 'flex', gap: '1rem' }}>
+                                    <Button primary onClick={() => handleShortcutsToggle(true)} disabled={allShortcutsEnabled}>
+                                        <i className="fas fa-toggle-on" />
+                                        <span>Enable All Shortcuts</span>
+                                    </Button>
+                                    <Button primary onClick={() => handleShortcutsToggle(false)} disabled={allShortcutsDisabled}>
+                                        <i className="fas fa-toggle-on" />
+                                        <span>Disable All Shortcuts</span>
+                                    </Button>
+                                </div>
+                                <Button primary onClick={() => setCurrentProfile(null)}>
+                                    <i className="fas fa-arrow-left" />
+                                    <span>Back to Profiles List</span>
+                                </Button>
+                            </div>
                         </>
                     )
                     : (
