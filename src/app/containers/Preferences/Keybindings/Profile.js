ca7fc6a687d40dac9b7d695ccbcc37cb5fd4baff
Walid Kayhan <walid.kayhan@hotmail.com>
2021-08-12 11:45:30 -0400

    Added function for toggling and deleting shortcuts

--- src/app/containers/Preferences/Keybindings/Profile.js
+++ src/app/containers/Preferences/Keybindings/Profile.js
@@ -1,16 +1,49 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
+import store from 'app/store';
+import PropTypes from 'prop-types';
+import { Toaster, TOASTER_SUCCESS } from 'app/lib/toaster/ToasterLib';
 
 import styles from './index.styl';
 import ProfileItem from './ProfileItem';
 import MainTable from './MainTable';
 import ProfileShortcutModal from './ProfileShortcutModal';
 
-const Profile = ({ currentProfile }) => {
+const Profile = ({ currentProfile, onUpdateProfiles }) => {
+    Profile.propTypes = { currentProfile: PropTypes.object, onUpdateProfiles: PropTypes.func };
+
     const { profileName, icon, shortcuts } = currentProfile;
 
     const [currentShortcut, setCurrentShortcut] = useState(null);
     const [showModal, setShowModal] = useState(false);
+
+    const handleDelete = (currShortcut) => {
+        const updatedShortcuts = shortcuts.map((shortcut) => (shortcut.id === currShortcut.id ? { ...shortcut, keys: '', keysName: '', isActive: false } : shortcut));
+
+        const profiles = store.get('workspace.gamepad.profiles', []);
+
+        const updatedProfiles =
+            profiles.map(profile => (currentProfile.id === profile.id ? ({ ...profile, shortcuts: updatedShortcuts }) : currentProfile));
+
+        onUpdateProfiles(updatedProfiles);
+
+        Toaster.pop({
+            msg: 'Removed Joystick Action Shortcut',
+            type: TOASTER_SUCCESS,
+            duration: 2000
+        });
+    };
+
+    const handleShortcutToggle = (currShortcut) => {
+        const updatedShortcuts = shortcuts.map((shortcut) => (shortcut.id === currShortcut.id ? currShortcut : shortcut));
+
+        const profiles = store.get('workspace.gamepad.profiles', []);
+
+        const updatedProfiles =
+            profiles.map(profile => (currentProfile.id === profile.id ? ({ ...profile, shortcuts: updatedShortcuts }) : currentProfile));
+
+        onUpdateProfiles(updatedProfiles);
+    };
 
     return (
         <>
@@ -24,8 +57,8 @@ const Profile = ({ currentProfile }) => {
                             setShowModal(true);
                             setCurrentShortcut(shortcut);
                         }}
-                        onDelete={() => console.log('Deleting...')}
-                        onShortcutToggle={() => console.log('Shortcut Toggle...')}
+                        onDelete={handleDelete}
+                        onShortcutToggle={handleShortcutToggle}
                         data={shortcuts}
                     />
                 </div>
@@ -36,6 +69,7 @@ const Profile = ({ currentProfile }) => {
                     profile={currentProfile}
                     shortcut={currentShortcut}
                     onClose={() => setShowModal(false)}
+                    onUpdateProfiles={onUpdateProfiles}
                 />
             )}
         </>
