b9c47fc522dbec7c039a5d4023592cdf1d3dd915
Walid Kayhan <walid.kayhan@hotmail.com>
2021-09-09 10:04:04 -0400

    Moved styling for file name to job status index

--- src/app/widgets/JobStatus/JobStatus.jsx
+++ src/app/widgets/JobStatus/JobStatus.jsx
@@ -24,6 +24,9 @@
 /* eslint-disable no-restricted-globals */
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
+import get from 'lodash/get';
+import { connect } from 'react-redux';
+
 import IdleInfo from './components/IdleInfo';
 import Overrides from './components/Overrides';
 import styles from './index.styl';
@@ -37,20 +40,45 @@ class JobStatus extends PureComponent {
         state: PropTypes.object,
     };
 
-    render() {
-        const { state } = this.props;
+    /**
+     * Determine the file size format between bytes, kilobytes (KB) and megabytes (MB)
+     */
+    fileSizeFormat = (size) => {
+        const ONE_KB = 1000;
+        const ONE_MB = 1000000;
 
+        if (size >= ONE_KB && size < ONE_MB) {
+            return `${(size / ONE_KB).toFixed(0)} KB`;
+        } else if (size >= ONE_MB) {
+            return `${(size / ONE_MB).toFixed(1)} MB`;
+        }
+
+        return `${size} bytes`;
+    };
+
+    render() {
+        const { state, name, size, total, fileLoaded } = this.props;
         const { isRunningJob } = state;
 
         return (
             <div className={styles['job-status-wrapper']}>
-                {!isRunningJob
-                    ? <IdleInfo state={state} />
-                    : <Overrides state={state} />
-                }
+                <div style={{ width: '100%' }}>
+                    {
+                        fileLoaded
+                            ? <><span className={styles['file-name']}>{name}</span> ({this.fileSizeFormat(size)}, {total} lines)</>
+                            : <span className={styles['file-name']}>No File Loaded</span>}
+                    {!isRunningJob
+                        ? <IdleInfo state={state} />
+                        : <Overrides state={state} />
+                    }
+                </div>
             </div>
         );
     }
 }
 
-export default JobStatus;
+export default connect((store) => {
+    const file = get(store, 'file', {});
+
+    return { ...file };
+})(JobStatus);
