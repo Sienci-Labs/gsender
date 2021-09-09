8e5c0d6be6b802e2197c8dee3f647433f4d75149
Walid Kayhan <walid.kayhan@hotmail.com>
2021-09-09 14:33:32 -0400

    Added styling prop for tooltips

--- src/app/components/TooltipCustom/ToolTip.jsx
+++ src/app/components/TooltipCustom/ToolTip.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip as MainToolTip } from 'app/components/Tooltip';
 
-const Tooltip = ({ location, content, disabled, children }) => {
+const Tooltip = ({ location, content, disabled, children, style }) => {
     return (
         <MainToolTip
             content={content}
             placement={location === 'default' ? 'bottom' : location}
             enterDelay={1000}
             disabled={disabled}
+            tooltipStyle={style}
         >
             <div>
                 {children}
