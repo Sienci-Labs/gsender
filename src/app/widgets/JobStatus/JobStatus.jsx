53b14621acd55f4fcbc471fec780a5e77e1c33d9
Walid Kayhan <walid.kayhan@hotmail.com>
2021-09-10 12:02:44 -0400

    Adjusted styling for file information

--- src/app/widgets/JobStatus/JobStatus.jsx
+++ src/app/widgets/JobStatus/JobStatus.jsx
@@ -67,18 +67,18 @@ class JobStatus extends PureComponent {
                     {
                         fileLoaded
                             ? (
-                                <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1rem' }}>
-                                    <div className={styles['file-item']}>
+                                <div style={{ display: 'flex', justifyContent: 'space-between', gap: '3rem' }}>
+                                    <div style={{ whiteSpace: 'nowrap' }}>
                                         <TooltipCustom content={`${name} (${this.fileSizeFormat(size)}, ${total} lines)`} style={{ wordWrap: 'break-word' }}>
                                             <span className={styles['file-text']}>{name}</span> ({this.fileSizeFormat(size)}, {total} lines)
                                         </TooltipCustom>
                                     </div>
                                     {path && (
-                                        <TooltipCustom content={`File Path: ${path}`} style={{ wordWrap: 'break-word' }}>
-                                            <div className={styles['file-item']}>
-                                                Path: <span className={styles['file-text']}>{path}</span>
-                                            </div>
-                                        </TooltipCustom>
+                                        <div className={styles['file-path']}>
+                                            <TooltipCustom content={`File Path: ${path}`} style={{ wordWrap: 'break-word' }}>
+                                                    Path: <span className={styles['file-text']}>{path}</span>
+                                            </TooltipCustom>
+                                        </div>
                                     )}
                                 </div>
                             )
