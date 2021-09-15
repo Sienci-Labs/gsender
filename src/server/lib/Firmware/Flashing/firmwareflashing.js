d5a89f2c268a16773b2835b85981078355c03367
KGN <kevin.glovernetherton@gmail.com>
2021-09-15 09:33:05 -0400

    Updated flashing wizard to use newly compiled hex with updated compile date header

--- src/server/lib/Firmware/Flashing/firmwareflashing.js
+++ src/server/lib/Firmware/Flashing/firmwareflashing.js
@@ -22,7 +22,7 @@
  */
 
 import AvrgirlArduino from 'avrgirl-arduino';
-import hex from '!file-loader!./grblsept9.hex';
+import hex from '!file-loader!./grblsept15.hex';
 import logger from '../../logger';
 import store from '../../../store';
 
