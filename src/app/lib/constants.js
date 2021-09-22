aaae171243cdefd68ff53b54784df66ef3def0aa
Walid Kayhan <walid.kayhan@hotmail.com>
2021-09-22 10:59:00 -0400

    Added constants for probing

--- src/app/lib/constants.js
+++ src/app/lib/constants.js
@@ -29,3 +29,9 @@ export const modifierKeys = [
 ];
 
 export const MAX_TERMINAL_INPUT_ARRAY_SIZE = 100;
+
+export const TOUCHPLATE_TYPE_STANDARD = 'Standard Block';
+export const TOUCHPLATE_TYPE_AUTOZERO = 'Auto Zero Touchplate';
+
+export const PROBE_TYPE_AUTO = 'Auto';
+export const PROBE_TYPE_ZERO = 'Zero';
