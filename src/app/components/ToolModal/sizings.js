e0d92fbbb7d4e6e6a45a8e71c796361edd88386e
Walid Kayhan <walid.kayhan@hotmail.com>
2021-09-24 09:18:11 -0400

    Added sizing constants for the tool modal

---
+++ src/app/components/ToolModal/sizings.js
@@ -0,0 +1,14 @@
+export const SMALL = {
+    width: '600px',
+    height: '400px'
+};
+
+export const MEDIUM = {
+    width: '800px',
+    height: '500px'
+};
+
+export const LARGE = {
+    width: '1000px',
+    height: '700px'
+};
