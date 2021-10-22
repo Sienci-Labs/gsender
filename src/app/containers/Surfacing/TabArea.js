7a797c376b62b0b93091954679a1b4cd048aabe1
Walid Kayhan <walid.kayhan@hotmail.com>
2021-10-22 11:46:27 -0400

    Added sepearate tab area component

---
+++ src/app/containers/Surfacing/TabArea.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TabbedWidget from 'app/components/TabbedWidget';
+import { SURFACING_VISUALIZER_CONTAINER_ID } from 'app/constants';
+
+
+const TabArea = ({ tabs, currentTab, onTabChange }) => {
+    return (
+        <TabbedWidget>
+            <TabbedWidget.Tabs
+                tabs={tabs}
+                activeTabIndex={currentTab}
+                onClick={onTabChange}
+                style={{ backgroundColor: '#e5e7eb' }}
+            >
+            </TabbedWidget.Tabs>
+            <TabbedWidget.Content>
+                <div style={{ width: '100%', height: '100%', position: 'relative' }} id={SURFACING_VISUALIZER_CONTAINER_ID}>
+                    {
+                        tabs.map((tab, index) => {
+                            const active = index === currentTab;
+                            return (
+                                <TabbedWidget.ChildComponent key={tab.id} active={active}>
+                                    {tab.component}
+                                </TabbedWidget.ChildComponent>
+                            );
+                        })
+                    }
+                </div>
+            </TabbedWidget.Content>
+        </TabbedWidget>
+    );
+};
+
+TabArea.propTypes = {
+    tabs: PropTypes.array,
+    currentTab: PropTypes.number,
+    onTabChange: PropTypes.func,
+};
+
+export default TabArea;
