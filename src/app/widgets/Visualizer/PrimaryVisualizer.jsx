4afd747c4807ba8676f8c20dde6bdbdc8c5d3d7f
Walid Kayhan <walid.kayhan@hotmail.com>
2021-10-22 11:48:09 -0400

    Created wrapper for primary/secondary visualizers

---
+++ src/app/widgets/Visualizer/PrimaryVisualizer.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+import * as WebGL from 'app/lib/three/WebGL';
+import Widget from 'app/components/Widget';
+import ToggleSwitch from 'app/components/ToggleSwitch';
+
+import WorkflowControl from './WorkflowControl';
+import MachineStatusArea from './MachineStatusArea';
+import ValidationModal from './ValidationModal';
+import WarningModal from './WarningModal';
+import Visualizer from './Visualizer';
+import Loading from './Loading';
+import Rendering from './Rendering';
+import WatchDirectory from './WatchDirectory';
+
+import { MODAL_WATCH_DIRECTORY } from './constants';
+
+import styles from './index.styl';
+
+
+const PrimaryVisualizer = ({ actions, state, capable, showLoading, showRendering, showVisualizer, visualizerRef, workflowRef, widgetContentRef }) => {
+    const { liteMode, modal, cameraPosition, invalidLine, invalidGcode } = state;
+
+    const { handleLiteModeToggle, handleRun, reset } = actions;
+
+    const containerID = 'visualizer_container';
+
+    return (
+        <Widget className={styles.vizWidgetOverride}>
+            <Widget.Header className={styles['visualizer-header']}>
+                <Widget.Title>
+                    Visualizer
+                </Widget.Title>
+                <Widget.Controls style={{ top: '-4px' }}>
+                    <ToggleSwitch
+                        label="Lightweight Mode"
+                        checked={liteMode}
+                        onChange={() => handleLiteModeToggle()}
+                        className={styles.litetoggle}
+                        size="md"
+                    />
+                </Widget.Controls>
+            </Widget.Header>
+            <Widget.Content
+                reference={widgetContentRef}
+                className={classNames(
+                    { [styles.view3D]: capable.view3D },
+                    styles['visualizer-component'],
+                )}
+                id={containerID}
+            >
+                {showLoading &&
+                <Loading />
+                }
+                {showRendering &&
+                <Rendering />
+                }
+                {modal.name === MODAL_WATCH_DIRECTORY && (
+                    <WatchDirectory
+                        state={state}
+                        actions={actions}
+                    />
+                )}
+
+                {WebGL.isWebGLAvailable() && (
+                    <div className={styles.visualizerWrapper}>
+                        <MachineStatusArea
+                            state={state}
+                            actions={actions}
+                        />
+                        <Visualizer
+                            show={showVisualizer}
+                            cameraPosition={cameraPosition}
+                            ref={visualizerRef}
+                            state={state}
+                            actions={actions}
+                            containerID={containerID}
+                        />
+
+                        <WorkflowControl
+                            ref={workflowRef}
+                            state={state}
+                            actions={actions}
+                            invalidGcode={invalidLine.line}
+                        />
+
+
+                        {
+                            invalidGcode.shouldShow && invalidGcode.showModal && (
+                                <ValidationModal
+                                    invalidGcode={invalidGcode}
+                                    onProceed={handleRun}
+                                    onCancel={reset}
+                                />
+                            )
+                        }
+                        {
+                            invalidLine.shouldShow && invalidLine.show && (
+                                <WarningModal
+                                    onContinue={actions.lineWarning.onContinue}
+                                    onIgnoreWarning={actions.lineWarning.onIgnoreWarning}
+                                    onCancel={actions.lineWarning.onCancel}
+                                    invalidLine={invalidLine.line}
+                                />
+                            )
+                        }
+                    </div>
+                )}
+            </Widget.Content>
+        </Widget>
+    );
+};
+
+PrimaryVisualizer.propTypes = {
+    actions: PropTypes.object,
+    state: PropTypes.object,
+    capable: PropTypes.object,
+    showLoading: PropTypes.bool,
+    showRendering: PropTypes.bool,
+    showVisualizer: PropTypes.bool,
+    visualizerRef: PropTypes.func,
+    workflowRef: PropTypes.func,
+    widgetContentRef: PropTypes.func,
+    containerID: PropTypes.string,
+};
+
+export default PrimaryVisualizer;
