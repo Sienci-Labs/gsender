2a6c0b1ff392820216f228c416699b2210600968
Walid Kayhan <walid.kayhan@hotmail.com>
2021-10-22 11:45:54 -0400

    Adjusted styling and added machine position area

--- src/app/containers/Surfacing/InputArea.js
+++ src/app/containers/Surfacing/InputArea.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Input from './components/Input';
-
+import MachinePosition from './components/MachinePosition';
 import styles from './index.styl';
+import inputStyles from './components/input.styl';
 
-const InputArea = ({ values, onChange, units }) => {
+const InputArea = ({ values, onChange, onSelect, units }) => {
     const {
         bitDiameter,
         stepover,
@@ -14,91 +15,107 @@ const InputArea = ({ values, onChange, units }) => {
         width,
         skimDepth,
         spindleRPM,
-        maxDepth
+        maxDepth,
+        type,
+        startPosition,
     } = values;
 
     return (
-        <div className={styles.grid}>
+        <div>
             <div>
-                <div className={styles.dimensions}>
-                    <span style={{ alignSelf: 'center', fontSize: '1.1rem', margin: '0 10px' }}>Surfacing Dimensions</span>
-                    <Input
-                        label="Length"
-                        additionalProps={{
-                            type: 'number',
-                            id: 'length',
-                            min: 1,
-                            max: 50000,
-                            style: { paddingLeft: 5, paddingRight: 5, borderRadius: 4 }
-                        }}
-                        value={length}
-                        onChange={onChange}
-                        className={styles['dimension-input']}
-                    />
-                    <span className={styles.x}>
-                        x
-                    </span>
-                    <Input
-                        label="Width"
-                        additionalProps={{
-                            type: 'number',
-                            id: 'width',
-                            min: 1,
-                            max: 50000,
-                            style: { paddingLeft: 5, paddingRight: 5 }
-                        }}
-                        value={width}
-                        onChange={onChange}
-                        units={units}
-                        className={styles['dimension-input']}
-                    />
+                <div className={inputStyles.input}>
+                    <span style={{ alignSelf: 'center', fontSize: '1.1rem', margin: '1rem 0.5rem 0 0' }}>Dimensions</span>
+
+                    <div className={styles.dimensions}>
+                        <Input
+                            label="Length"
+                            additionalProps={{
+                                type: 'number',
+                                id: 'length',
+                                min: 1,
+                                max: 50000,
+                                style: { paddingLeft: 5, paddingRight: 5, borderRadius: 4, ...inputStyles }
+                            }}
+                            value={length}
+                            onChange={onChange}
+                            className={styles['dimension-input']}
+                        />
+                        <span className={styles.x}>
+                            x
+                        </span>
+                        <Input
+                            label="Width"
+                            additionalProps={{
+                                type: 'number',
+                                id: 'width',
+                                min: 1,
+                                max: 50000,
+                                style: { paddingLeft: 5, paddingRight: 5, ...inputStyles }
+                            }}
+                            value={width}
+                            onChange={onChange}
+                            units={units}
+                            className={styles['dimension-input']}
+                        />
+                    </div>
                 </div>
                 <Input
                     label="Router Bit Diameter"
                     units={units}
-                    additionalProps={{ type: 'number', id: 'bitDiameter', min: 0.01, max: 1000 }}
+                    additionalProps={{ type: 'number', id: 'bitDiameter', min: 0.01, max: 1000, style: { ...inputStyles } }}
                     value={bitDiameter}
                     onChange={onChange}
                 />
                 <Input
                     label="Spindle RPM"
                     units={units}
-                    additionalProps={{ type: 'number', id: 'spindleRPM', min: 1, max: 200000 }}
+                    additionalProps={{ type: 'number', id: 'spindleRPM', min: 1, max: 200000, style: { ...inputStyles } }}
                     value={spindleRPM}
                     onChange={onChange}
                 />
             </div>
 
-            <div style={{ marginTop: '25px' }}>
+            <div>
                 <Input
                     label="Stepover"
                     units="%"
-                    additionalProps={{ type: 'number', id: 'stepover', min: 1, max: 100 }}
+                    additionalProps={{ type: 'number', id: 'stepover', min: 1, max: 100, style: { ...inputStyles } }}
                     value={stepover}
                     onChange={onChange}
                 />
                 <Input
                     label="Feedrate"
                     units={`${units}/min`}
-                    additionalProps={{ type: 'number', id: 'feedrate', min: 1, max: 1000000 }}
+                    additionalProps={{ type: 'number', id: 'feedrate', min: 1, max: 1000000, style: { ...inputStyles } }}
                     value={feedrate}
                     onChange={onChange}
                 />
                 <Input
                     label="Layer Depth"
                     units={units}
-                    additionalProps={{ type: 'number', id: 'skimDepth', min: 0.001, max: 500 }}
+                    additionalProps={{ type: 'number', id: 'skimDepth', min: 0.001, max: 500, style: { ...inputStyles } }}
                     value={skimDepth}
                     onChange={onChange}
                 />
                 <Input
                     label="Max Depth"
                     units={units}
-                    additionalProps={{ type: 'number', id: 'maxDepth', min: 0.001, max: 500 }}
+                    additionalProps={{ type: 'number', id: 'maxDepth', min: 0.001, max: 500, style: { ...inputStyles } }}
                     value={maxDepth}
                     onChange={onChange}
-                    className={styles.maxDepth}
                 />
+
+                <div style={{ marginTop: '2.5rem' }}>
+                    <div className={inputStyles.input}>
+                        <label htmlFor="">Start Position</label>
+
+                        <MachinePosition
+                            current={startPosition}
+                            type={type}
+                            onChange={(value, type) => onSelect({ value, type })}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
@@ -108,6 +125,7 @@ InputArea.propTypes = {
     values: PropTypes.object,
     onChange: PropTypes.func,
     units: PropTypes.string,
+    onSelect: PropTypes.func,
 };
 
 export default InputArea;
