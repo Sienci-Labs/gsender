70d7e0a3c490ac221bccceb12c769e1b85aa6770
KGN <kevin.glovernetherton@gmail.com>
2021-10-25 13:26:23 -0400

    Renamed Z-only touchplate to Z Probe

--- src/app/lib/constants.js
+++ src/app/lib/constants.js
@@ -32,7 +32,7 @@ export const MAX_TERMINAL_INPUT_ARRAY_SIZE = 100;
 
 export const TOUCHPLATE_TYPE_STANDARD = 'Standard Block';
 export const TOUCHPLATE_TYPE_AUTOZERO = 'Auto Zero Touchplate';
-export const TOUCHPLATE_TYPE_ZERO = 'Z-only Touchplate';
+export const TOUCHPLATE_TYPE_ZERO = 'Z Probe';
 
 export const PROBE_TYPE_AUTO = 'Auto';
 export const PROBE_TYPE_TIP = 'Tip';
