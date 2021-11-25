f5dd2a1ac6260e81c14b3d2f1a0dac5f8a4e903e
Walid Kayhan <walid.kayhan@hotmail.com>
2021-11-25 10:55:49 -0500

    Added image for xy probe

--- src/app/widgets/Probe/ProbeImage.jsx
+++ src/app/widgets/Probe/ProbeImage.jsx
@@ -27,6 +27,7 @@ import Image from 'app/components/Image';
 import styles from './index.styl';
 import XProbe from './assets/x_probe.svg';
 import YProbe from './assets/y_probe.svg';
+import XYProbe from './assets/xy_probe.svg';
 import XYZProbe from './assets/xyz_probe.svg';
 
 const ProbeImage = ({ probeCommand, visible = true }) => {
@@ -36,6 +37,8 @@ const ProbeImage = ({ probeCommand, visible = true }) => {
             return XProbe;
         } else if (id === 'Y Touch') {
             return YProbe;
+        } else if (id === 'XY Touch') {
+            return XYProbe;
         }
         return XYZProbe;
     };
