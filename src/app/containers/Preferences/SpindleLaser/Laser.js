21002466e7570219879a84a77568ca6195450854
walidkayhan <walid.kayhan@hotmail.com>
2022-02-15 13:03:53 -0500

    Added seperate components for laser and spindle

---
+++ src/app/containers/Preferences/SpindleLaser/Laser.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import TooltipCustom from 'app/components/TooltipCustom/ToolTip';
+
+import Fieldset from '../components/Fieldset';
+import Input from '../components/Input';
+
+const Laser = ({ state, actions }) => {
+    const { units, spindle: { laser } } = state;
+    const { xOffset, yOffset } = laser;
+    const laserActions = actions.laser;
+
+    return (
+        <>
+            <Fieldset legend="Axes Offset">
+                <Input
+                    label="X Axis Offset"
+                    units={units}
+                    value={xOffset}
+                    onChange={(e) => laserActions.handleOffsetChange(e, 'X')}
+                />
+                <Input
+                    label="Y Axis Offset"
+                    units={units}
+                    value={yOffset}
+                    onChange={(e) => laserActions.handleOffsetChange(e, 'Y')}
+                />
+            </Fieldset>
+
+            <Fieldset legend="Power">
+                <TooltipCustom content="Minimum laser amount" location="default">
+                    <Input
+                        label="Minimum Laser Power"
+                        units="PWM"
+                        value={laser.minPower}
+                        onChange={(e) => laserActions.setPower(e.target.value, 'minPower')}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </TooltipCustom>
+                <TooltipCustom content="Maximum laser amount" location="default">
+                    <Input
+                        label="Maximum Laser Power"
+                        units="PWM"
+                        value={laser.maxPower}
+                        onChange={(e) => laserActions.setPower(e.target.value, 'maxPower')}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </TooltipCustom>
+            </Fieldset>
+        </>
+    );
+};
+
+export default Laser;
---
+++ src/app/containers/Preferences/SpindleLaser/Spindle.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import TooltipCustom from 'app/components/TooltipCustom/ToolTip';
+
+import Fieldset from '../components/Fieldset';
+import Input from '../components/Input';
+
+const Spindle = ({ state, actions }) => {
+    const { spindle } = state;
+    const { spindleMin, spindleMax } = spindle;
+    const spindleActions = actions.spindle;
+
+    return (
+        <>
+            <Fieldset legend="Speed">
+                <TooltipCustom content="Minimum spindle amount" location="default">
+                    <Input
+                        label="Minimum Spindle Speed"
+                        units="RPM"
+                        value={spindleMin}
+                        onChange={(e) => spindleActions.setSpeed(e.target.value, 'spindleMin')}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </TooltipCustom>
+                <TooltipCustom content="Maximum spindle speed" location="default">
+                    <Input
+                        label="Maximum Spindle Speed"
+                        units="RPM"
+                        value={spindleMax}
+                        onChange={(e) => spindleActions.setSpeed(e.target.value, 'spindleMax')}
+                        additionalProps={{ type: 'number' }}
+                    />
+                </TooltipCustom>
+            </Fieldset>
+        </>
+    );
+};
+
+export default Spindle;
