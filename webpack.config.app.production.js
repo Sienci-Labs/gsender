5c44e84074b17fa7a0d6abb650337bc599dbc9ec
Lorin Francell <lawrence@twintails.com>
2022-06-13 07:44:42 -0500

    update deprectated "substr" use in webpack

--- webpack.config.app.production.js
+++ webpack.config.app.production.js
@@ -30,7 +30,7 @@ const publicPath = ((payload) => {
     const algorithm = 'sha1';
     const buf = String(payload);
     const hash = crypto.createHash(algorithm).update(buf).digest('hex');
-    return '/' + hash.substr(0, 8) + '/'; // 8 digits
+    return '/' + hash.substring(0, 8) + '/'; // 8 digits
 })(pkg.version);
 const buildVersion = pkg.version;
 const timestamp = new Date().getTime();
@@ -244,6 +244,25 @@ module.exports = {
             path.resolve(__dirname, 'src'),
             'node_modules'
         ],
-        extensions: ['.js', '.jsx']
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        alias: {
+            // Alias paths so that e.g. "../../components/File" becomes "Components/File"
+            Actions: path.resolve(__dirname, './src/app/actions'),
+            Atoms: path.resolve(__dirname, './src/app/atoms'),
+            APIs: path.resolve(__dirname, './src/app/apis'),
+            Components: path.resolve(__dirname, './src/app/components'),
+            Containers: path.resolve(__dirname, './src/app/containers'),
+            Constants: path.resolve(__dirname, './src/app/constants'),
+            Contexts: path.resolve(__dirname, './src/app/contexts'),
+            Hooks: path.resolve(__dirname, './src/app/hooks'),
+            Images: path.resolve(__dirname, './src/app/images'),
+            Models: path.resolve(__dirname, './src/app/models'),
+            Reducers: path.resolve(__dirname, './src/app/reducers'),
+            Routes$: path.resolve(__dirname, './src/app/routes'),
+            Styles: path.resolve(__dirname, './src/app/styles'),
+            Types: path.resolve(__dirname, './src/app/types'),
+            Utils: path.resolve(__dirname, './src/app/utils'),
+            Views: path.resolve(__dirname, './src/app/views')
+        }
     }
 };
