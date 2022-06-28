19fd3745addedb12213de61b8a59ebd4c202f125
Sophia Beluli <sophiabeluli@gmail.com>
2022-06-28 10:31:45 -0400

    fixing quotes to single

--- src/app/containers/Preferences/About/tools.js
+++ src/app/containers/Preferences/About/tools.js
@@ -1,33 +1,33 @@
 export const team = [
   {
     id: 3,
-    name: "Chris T.",
-    title: "Project Lead",
+    name: 'Chris T.',
+    title: 'Project Lead',
   },
   {
     id: 1,
-    name: "Kevin G.",
-    title: "Dev Lead",
+    name: 'Kevin G.',
+    title: 'Dev Lead',
   },
   {
     id: 0,
-    name: "Walid K.",
-    title: "Dev",
+    name: 'Walid K.',
+    title: 'Dev',
   },
   {
     id: 2,
-    name: "Justin W.",
-    title: "Dev",
+    name: 'Justin W.',
+    title: 'Dev',
   },
   {
     id: 4,
-    name: "Kelly Z.",
-    title: "Icon Design",
+    name: 'Kelly Z.',
+    title: 'Icon Design',
   },
   {
     id: 5,
-    name: "Aleks G.",
-    title: "Research and Design",
+    name: 'Aleks G.',
+    title: 'Research and Design',
     isLastInList: true,
   },
   {
@@ -37,31 +37,31 @@ export const team = [
   },
   {
     id: 7,
-    name: "Swagat S.",
-    title: "Dev",
+    name: 'Swagat S.',
+    title: 'Dev',
   },
 ];
 
 export const timeline = [
   {
     id: 0,
-    date: "January 29th, 2021",
-    text: "Closed Alpha Release",
+    date: 'January 29th, 2021',
+    text: 'Closed Alpha Release',
   },
   {
     id: 1,
-    date: "March 5th, 2021",
-    text: "Closed Beta Release",
+    date: 'March 5th, 2021',
+    text: 'Closed Beta Release',
   },
   {
     id: 2,
-    date: "May 14th, 2021",
-    text: "Open Beta Release",
+    date: 'May 14th, 2021',
+    text: 'Open Beta Release',
   },
   {
     id: 3,
-    date: "October 29th, 2021",
-    text: "Full Version Release",
+    date: 'October 29th, 2021',
+    text: 'Full Version Release',
     isLastInList: true,
   },
 ];
