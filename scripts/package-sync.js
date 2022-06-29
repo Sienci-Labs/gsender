9351c01eda4dd2fd4aec50f71a0c52aab31c2447
swagat samal <swagat_samal@sienci.com>
2022-06-29 15:03:48 -0400

    MI-669-Complete

--- scripts/package-sync.js
+++ scripts/package-sync.js
@@ -10,14 +10,13 @@ const findImports = require('find-imports');
 const pkg = require('../package.json');
 const pkgApp = require('../src/package.json');
 
-const files = [
-    'src/*.js',
-    'src/server/**/*.{js,jsx}'
-];
+const files = ['src/*.js', 'src/server/**/*.{js,jsx}'];
 const deps = [
     '@babel/runtime', // 'babel-runtime' is required for electron app
-    'debug' // 'debug' is required for electron app
-].concat(findImports(files, { flatten: true })).sort();
+    'debug', // 'debug' is required for electron app
+]
+    .concat(findImports(files, { flatten: true }))
+    .sort();
 
 //pkgApp.name = pkg.name; // Exclude the name field
 pkgApp.version = pkg.version;
@@ -32,3 +31,36 @@ pkgApp.dependencies = _.pick(pkg.dependencies, deps);
 const target = path.resolve(__dirname, '../src/package.json');
 const content = JSON.stringify(pkgApp, null, 2);
 fs.writeFileSync(target, content + '\n', 'utf8');
+
+//Save app versions to releases.json
+try {
+    const releaseTarget = path.resolve(
+        __dirname,
+        '../src/app/containers/Preferences/About/releases.json'
+    );
+
+    const removeLines = (data, deleteTill) => {
+        let tempArray = data.split('\n');
+        let finalRelease = '';
+        let count = 0;
+        const index = tempArray.indexOf(deleteTill);
+        tempArray.splice(0, index + 1);
+        tempArray.forEach((element, index) => {
+            if (element.includes('###')) {
+                count++;
+                if (count === 2) {
+                    finalRelease = tempArray;
+                    finalRelease.splice(index, tempArray.length);
+                    return true;
+                }
+            }
+        });
+        tempArray = finalRelease;
+        return tempArray;
+    };
+    let readme = fs.readFileSync(path.resolve('README.md'), 'utf8');
+    const releases = removeLines(readme, '## 🕣 Development History');
+    fs.writeFileSync(releaseTarget, JSON.stringify(releases));
+} catch (error) {
+    console.log(error.message);
+}
