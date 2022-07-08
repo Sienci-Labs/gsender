7a148765bcfddc2512d31a40002f608f680aee2d
Walid Kayhan <walid.kayhan@outlook.com>
2022-07-08 09:14:01 -0400

    Added component for confirmation modal popups

---
+++ src/app/containers/Firmware/components/Actions/Notifications.jsx
@@ -0,0 +1,66 @@
+import React, { useContext } from 'react';
+
+import ToolsNotificationModal from 'app/components/ToolsNotificationModal/Modal';
+
+import { FirmwareContext, getResetToDefaultMessage, restoreDefaultSettings, startFlash } from '../../utils';
+import defaultGRBLSettings from '../../eepromFiles/DefaultGrblSettings.json';
+
+const Notifications = () => {
+    const {
+        machineProfile,
+        port,
+        initiateFlashing,
+        shouldRestoreDefault,
+        setShouldRestoreDefault,
+        setInitiateFlashing,
+        setCurrentlyFlashing,
+        setSettings,
+    } = useContext(FirmwareContext);
+
+    const beginFlashing = () => {
+        startFlash(port);
+        setInitiateFlashing(false);
+        setCurrentlyFlashing(true);
+    };
+
+    const message = getResetToDefaultMessage(machineProfile);
+
+    const restoreDefaults = () => {
+        const machineProfileUpdated = { ...machineProfile, eepromSettings: machineProfile.eepromSettings ?? defaultGRBLSettings };
+        restoreDefaultSettings(machineProfileUpdated);
+        setSettings(prev => prev.map(item => ({ ...item, value: machineProfileUpdated.eepromSettings[item.setting] })));
+        setShouldRestoreDefault(false);
+    };
+
+    return (
+        <>
+            {initiateFlashing && (
+                <ToolsNotificationModal
+                    title="Grbl Flashing"
+                    onClose={() => setInitiateFlashing(false)}
+                    show={initiateFlashing}
+                    footer="This process will disconnect your machine, and may take a couple minutes to complete."
+                    footerTwo="Continue?"
+                    yesFunction={beginFlashing}
+                >
+                    This feature exists to flash the GRBL firmware onto compatible Arduino boards only!
+                    Improper flashing could damage your device on port: <strong>{port}</strong>.
+                </ToolsNotificationModal>
+            )}
+
+            {shouldRestoreDefault && (
+                <ToolsNotificationModal
+                    title="Restore Cnc Defaults"
+                    onClose={() => setShouldRestoreDefault(false)}
+                    show={shouldRestoreDefault}
+                    footer="Restore your Cnc machine?"
+                    yesFunction={restoreDefaults}
+                >
+                    {message}
+                </ToolsNotificationModal>
+            )}
+        </>
+    );
+};
+
+export default Notifications;
