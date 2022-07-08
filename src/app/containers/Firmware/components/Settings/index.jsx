8b75bf8cfc4aa291b2321ba631768340c51a502c
Walid Kayhan <walid.kayhan@outlook.com>
2022-07-08 09:17:56 -0400

    Added separate component for settings area

---
+++ src/app/containers/Firmware/components/Settings/index.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+
+import SettingsList from './List';
+import SearchBar from './SearchBar';
+import { connectToLastDevice, FirmwareContext } from '../../utils';
+import NotConnectedWarning from '../NotConnected/NotConnectedWarning';
+
+import styles from '../../index.styl';
+
+const SettingsArea = () => {
+    const { hasSettings, machineProfile } = useContext(FirmwareContext);
+
+    const machineName = (
+        <>
+            Machine Profile:{' '}
+            <strong>{`${machineProfile?.company} ${machineProfile?.name} ${machineProfile?.version}`}</strong>
+        </>
+    );
+
+    return hasSettings ? (
+        <div className={styles.settingsAreaContainer}>
+            {machineProfile && <p style={{ margin: 0 }}>{machineName}</p>}
+            <SearchBar />
+            <div style={{ width: '100%', height: '100%', overflow: 'auto' }}>
+                <SettingsList />
+            </div>
+        </div>
+    ) : <NotConnectedWarning onReconnectClick={() => connectToLastDevice()} />;
+};
+
+export default SettingsArea;
