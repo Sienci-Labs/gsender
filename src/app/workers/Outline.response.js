7c4ab01a4b3c3edba6259e3002e4e6a80e6f0c08
Sophia Beluli <sophiabeluli@Sophias-MacBook.local>
2022-07-15 16:22:24 -0400

    added toggle in settings for laser ON/OFF during outline; outline code checks for this setting and turns the laser on accordingly

--- src/app/workers/Outline.response.js
+++ src/app/workers/Outline.response.js
@@ -1,5 +1,13 @@
+import { ensurePositiveNumber } from 'ensure-type';
 import controller from '../lib/controller';
 
-export const outlineResponse = ({ data }) => {
-    controller.command('gcode', data.outlineGcode);
+export const outlineResponse = ({ data }, isLaserOn) => {
+    if (isLaserOn) {
+        const power = 1, maxS = 1000;
+        let turnOn = 'G1F1 M3 S' + ensurePositiveNumber(maxS * (power / 100));
+        let turnOff = 'M5 S0';
+        controller.command('gcode', [turnOn, ...data.outlineGcode, turnOff]);
+    } else {
+        controller.command('gcode', data.outlineGcode);
+    }
 };
