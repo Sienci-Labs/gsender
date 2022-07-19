7aa3a808a4e17a7dd62ca3ae93ac6d25dab70b28
swagat <sinu2290@gmail.com>
2022-07-19 14:23:11 -0400

    helper function refractored

--- src/server/controllers/helper.js
+++ src/server/controllers/helper.js
@@ -1,43 +1,35 @@
 export const generateGcode = (pinState, tooltipMoveFactor, sensorPositon) => {
-    switch (sensorPositon) {
     //Sensor Position: 0-is top right // 1-is top left // 2-bottom right // 3-bottom left
-    case 0:
-        // sensor corner - top right
-        if ('X' in pinState) {
-            return `G0 Y-${tooltipMoveFactor}`;
-        } else if ('Y' in pinState) {
-            return `G0 X-${tooltipMoveFactor}`;
-        } else {
-            return `G0 Z-${tooltipMoveFactor}`;
-        }
-    case 1:
-        // sensor corner - top left
-        if ('X' in pinState) {
-            return `G0 Y-${tooltipMoveFactor}`;
-        } else if ('Y' in pinState) {
-            return `G0 X${tooltipMoveFactor}`;
-        } else {
-            return `G0 Z-${tooltipMoveFactor}`;
-        }
-    case 2:
-        // sensor corner - bottom right
-        if ('X' in pinState) {
-            return `G0 Y${tooltipMoveFactor}`;
-        } else if ('Y' in pinState) {
-            return `G0 X-${tooltipMoveFactor}`;
-        } else {
-            return `G0 Z-${tooltipMoveFactor}`;
-        }
-    case 3:
-        // sensor corner - bottom left
-        if ('X' in pinState) {
-            return `G0 Y${tooltipMoveFactor}`;
-        } else if ('Y' in pinState) {
-            return `G0 X${tooltipMoveFactor}`;
-        } else {
-            return `G0 Z-${tooltipMoveFactor}`;
-        }
-    default:
-        return true;
+    if ('X' in pinState) {
+        return `G0 G21 G91 Y${getDirection(sensorPositon, 'X') * tooltipMoveFactor}`;
+    } else if ('Y' in pinState) {
+        return `G0 G21 G91 X${getDirection(sensorPositon, 'Y') * tooltipMoveFactor}`;
+    } else {
+        return `G0 G21 G91 Z-${tooltipMoveFactor}`;
     }
 };
+
+function getDirection(sensorPositon, axis) {
+    switch (sensorPositon) {
+    case 0:
+        return -1;
+    case 1:
+        if (axis === 'X') {
+            return -1;
+        } else {
+            return 1;
+        }
+    case 2:
+        if (axis === 'x') {
+            return 1;
+        } else {
+            return -1;
+        }
+
+    case 3:
+        return 1;
+
+    default:
+        return 0;
+    }
+}
