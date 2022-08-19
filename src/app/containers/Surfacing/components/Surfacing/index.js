bc0f52f07f560539ab10e4af43c17c1ecd02fb5a
Walid Kayhan <walid.kayhan@outlook.com>
2022-08-19 09:46:11 -0400

    Moved to seperate folder

--- src/app/containers/Surfacing/components/Surfacing/index.js
+++ src/app/containers/Surfacing/components/Surfacing/index.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import pubsub from 'pubsub-js';
 import { inRange, throttle } from 'lodash';
+import { Provider as ReduxProvider } from 'react-redux';
 
-import reduxStore from 'app/store/redux';
 import { SET_CURRENT_VISUALIZER } from 'app/actions/visualizerActions';
 import store from 'app/store';
+import reduxStore from 'app/store/redux';
 import controller from 'app/lib/controller';
 import { METRIC_UNITS, IMPERIAL_UNITS, VISUALIZER_PRIMARY, VISUALIZER_SECONDARY } from 'app/constants';
 import api from 'app/api';
 
-import Visualizer from './Visualizer';
-import InputArea from './InputArea';
-import ActionArea from './components/actions';
-import styles from './index.styl';
-import Generator from './helpers/Generator';
-import GcodeViewer from './components/GcodeViewer';
-import TabArea from './TabArea';
+import Visualizer from '../Visualizer';
+import InputArea from '../InputArea';
+import ActionArea from '../ActionArea';
+import styles from '../../index.styl';
+import Generator from '../../utils/Generator';
+import GcodeViewer from '../GcodeViewer';
+import TabArea from '../TabArea';
+import { SurfacingContext } from './Context';
 
 const convertTo = (type, val) => (type === METRIC_UNITS ? Math.round(val * 25.4) : Number((val / 25.4).toFixed(2)));
 
@@ -34,6 +36,8 @@ const Surfacing = ({ onClose, showTitle }) => {
     const [currentTab, setCurrentTab] = useState(0);
 
     const runGenerate = throttle(async () => {
+        setCurrentTab(0);
+
         const generator = new Generator({ surfacing, units, controller });
 
         const gcode = generator.handleGenerate();
@@ -43,7 +47,7 @@ const Surfacing = ({ onClose, showTitle }) => {
         await api.file.upload(serializedFile, controller.port, VISUALIZER_SECONDARY);
 
         setGcode(gcode);
-    }, 1000, { leading: true });
+    }, 5000);
 
     const handleChange = ({ target, shouldConvert = true }) => {
         const { id, value, min, max } = target;
@@ -77,24 +81,6 @@ const Surfacing = ({ onClose, showTitle }) => {
         pubsub.publish('gcode:surfacing', { gcode, name, size });
         onClose();
     };
-
-    const canLoad = !!gcode; //For accessing the gcode line viewer
-
-    const tabs = [
-        {
-            id: 0,
-            label: 'Visualizer Preview',
-            widgetId: 'viz-preview',
-            component: <Visualizer gcode={gcode} surfacing={surfacing} />,
-        },
-        {
-            id: 1,
-            label: `G-code Viewer ${gcode && `(${gcode.split('\n').length} lines)`}`,
-            widgetId: 'gcode-viewer',
-            component: <GcodeViewer gcode={gcode} />,
-            disabled: !gcode,
-        },
-    ];
 
     /**
      * Grab the set machine profile and workspace units on component mount
@@ -184,39 +170,63 @@ const Surfacing = ({ onClose, showTitle }) => {
         }
     }, [currentTab]);
 
+
+    const canLoad = !!gcode; //For accessing the gcode line viewer
+    const tabs = [
+        {
+            id: 0,
+            label: 'Visualizer Preview',
+            widgetId: 'viz-preview',
+            component: <Visualizer gcode={gcode} surfacing={surfacing} />,
+        },
+        {
+            id: 1,
+            label: `G-code Viewer ${gcode && `(${gcode.split('\n').length} lines)`}`,
+            widgetId: 'gcode-viewer',
+            component: <GcodeViewer gcode={gcode} />,
+            disabled: !gcode,
+        },
+    ];
+    const contextValue = {
+        surfacing,
+        setSurfacing,
+        onChange: handleChange,
+        onSelect: handleSelect,
+        units
+    };
+
     return (
-        <>
-            {showTitle && (
-                <div className={styles.header}>
-                    <h3 className={styles.headerText}>Surfacing Tool</h3>
-                </div>
-            )}
-
-            <div className={styles.container}>
-                <div className={styles.mainContainer}>
-                    <div>
-                        <p style={{ fontSize: '1.25rem', lineHeight: '1.25', fontWeight: 'bold', textAlign: 'center' }}>
-                            * Please disable your hard or soft limits on your machine before surfacing *
-                        </p>
-                        <InputArea
-                            values={surfacing}
-                            onChange={handleChange}
-                            onSelect={handleSelect}
-                            units={units}
-                        />
+        <ReduxProvider store={reduxStore}>
+            <SurfacingContext.Provider value={contextValue}>
+                {showTitle && (
+                    <div className={styles.header}>
+                        <h3 className={styles.headerText}>Surfacing Tool</h3>
                     </div>
-                    <TabArea tabs={tabs} currentTab={currentTab} onTabChange={(index) => setCurrentTab(index)} />
-                </div>
+                )}
 
-                <ActionArea
-                    handleCancel={onClose}
-                    handleGenerateGcode={runGenerate}
-                    handleLoadGcode={loadGcode}
-                    surfacing={surfacing}
-                    canLoad={canLoad}
-                />
-            </div>
-        </>
+                <div className={styles.container}>
+                    <div className={styles.mainContainer}>
+                        <div>
+                            <p style={{ fontSize: '1.25rem', lineHeight: '1.25' }}>
+                                To best surface your whole wasteboard, ensure you know the exact limits of your CNCs
+                                movement and account for limit switches or other add-ons. Use a wide diameter bit to
+                                clear the largest space and consider turning off hard and soft limits so you don&apos;t
+                                encounter alarms or errors.
+                            </p>
+                            <InputArea />
+                        </div>
+                        <TabArea tabs={tabs} currentTab={currentTab} onTabChange={(index) => setCurrentTab(index)} />
+                    </div>
+
+                    <ActionArea
+                        handleCancel={onClose}
+                        handleGenerateGcode={runGenerate}
+                        handleLoadGcode={loadGcode}
+                        canLoad={canLoad}
+                    />
+                </div>
+            </SurfacingContext.Provider>
+        </ReduxProvider>
     );
 };
 
