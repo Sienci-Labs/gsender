efc9f624a7cc1b86c436912fbb60009ef7a8f52e
Walid Kayhan <walid.kayhan@outlook.com>
2022-08-30 08:51:53 -0400

    Added copy gcode button and logic

--- src/app/containers/Surfacing/components/GcodeViewer/index.js
+++ src/app/containers/Surfacing/components/GcodeViewer/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import throttle from 'lodash/throttle';
+
+import Button from 'app/components/FunctionButton/FunctionButton';
+import { Toaster, TOASTER_INFO } from 'app/lib/toaster/ToasterLib';
 
 import Line from './Line';
-import { Container } from './styled';
+import { Container, GcodeContainer } from './styled';
 
 const GcodeViewer = ({ gcode }) => {
+    const handleCopy = throttle(async () => {
+        await navigator.clipboard.writeText(gcode);
+
+        Toaster.pop({
+            msg: 'Copied G-code to Clipboard',
+            type: TOASTER_INFO
+        });
+    }, 2000, { trailing: false });
+
     return (
         <Container>
-            {
-                gcode.split('\n').map((line, i) => <Line key={`${i}-${line}`} number={i + 1} text={line} />)
-            }
+            <GcodeContainer>
+                {
+                    gcode.split('\n').map((line, i) => <Line key={`${i}-${line}`} number={i + 1} text={line} />)
+                }
+            </GcodeContainer>
+            <Button style={{ margin: 0 }} onClick={handleCopy}>
+                <i className="fas fa-copy" /> Copy to Clipboard
+            </Button>
         </Container>
     );
 };
