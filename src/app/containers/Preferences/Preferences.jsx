e976c4d34e6db40bdf85f70ade1488929cf49e3c
Walid Kayhan <walid.kayhan@outlook.com>
2022-09-16 09:11:14 -0400

    Update surfacing values when units change

--- src/app/containers/Preferences/Preferences.jsx
+++ src/app/containers/Preferences/Preferences.jsx
@@ -42,6 +42,7 @@ import styles from './index.styl';
 import { METRIC_UNITS } from '../../constants';
 import { convertToImperial, convertToMetric } from './calculate';
 import { CUST_LIGHT_THEME, DARK_THEME, DARK_THEME_VALUES, LIGHT_THEME, LIGHT_THEME_VALUES } from '../../widgets/Visualizer/constants';
+import { convertValuesToMetric, convertValuesToImperial } from '../Surfacing/utils';
 
 
 class PreferencesPage extends PureComponent {
@@ -754,6 +755,19 @@ class PreferencesPage extends PureComponent {
         if (prevState.selectedMenu !== this.state.selectedMenu) {
             return;
         }
+
+        if (units !== prevState.units) {
+            const surfacingValues = store.get('widgets.surfacing');
+
+            if (units === 'mm') {
+                store.replace('widgets.surfacing', convertValuesToMetric(surfacingValues));
+            }
+
+            if (units === 'in') {
+                store.replace('widgets.surfacing', convertValuesToImperial(surfacingValues));
+            }
+        }
+
 
         this.showToast();
     }
