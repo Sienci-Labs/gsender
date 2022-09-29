f7bd030c9ca026e58b0c2580f2c0a619af43faa4
Sophia Beluli <sophiabeluli@Sophias-MacBook.local>
2022-09-29 15:36:46 -0400

    updated modal package: new prop disableOverlayClick stops the modal from being closed by clicking outside of it; added this prop to the AddMacro modal

--- src/app/components/ToolModal/ToolModal.jsx
+++ src/app/components/ToolModal/ToolModal.jsx
@@ -30,7 +30,7 @@ import styles from './index.styl';
 import { LARGE, MEDIUM, SMALL } from './sizings';
 
 
-const ToolModal = ({ onClose, title, style, className, size, children }) => {
+const ToolModal = ({ onClose, title, style, className, size, children, disableOverlayClick = false }) => {
     let sizingStyles;
 
     switch (size?.toLowerCase()) {
@@ -61,6 +61,7 @@ const ToolModal = ({ onClose, title, style, className, size, children }) => {
             style={{ ...style, ...sizingStyles }}
             className={className}
             size={size}
+            disableOverlayClick={disableOverlayClick}
         >
             <div className={styles.toolModal}>
                 <div className={styles.header}>
