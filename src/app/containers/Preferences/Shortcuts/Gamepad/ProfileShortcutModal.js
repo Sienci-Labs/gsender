c2e37bab16ea8496d6f855807a87f6b5c39a809b
Walid Kayhan <walid.kayhan@outlook.com>
2022-11-07 16:47:12 -0500

    REFACTORED: use one piece of state and dynamic JSX

--- src/app/containers/Preferences/Shortcuts/Gamepad/ProfileShortcutModal.js
+++ src/app/containers/Preferences/Shortcuts/Gamepad/ProfileShortcutModal.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import classnames from 'classnames';
 
 import { Toaster, TOASTER_SUCCESS } from 'app/lib/toaster/ToasterLib';
@@ -11,14 +11,16 @@ import Modal from 'app/components/ToolModal/ToolModal';
 import Input from '../../components/Input';
 
 import styles from '../index.styl';
+import { AVAILABILITY_TYPES } from '../utils';
+import Listener from './Listener';
 
-let queue = [];
+const { DEFAULT, AVAILABLE, UNAVAILABLE, IS_THE_SAME } = AVAILABILITY_TYPES;
 
 const ProfileShortcutModal = ({ profile, shortcut, onClose, onUpdateProfiles }) => {
     const [gamepadShortcut, setGamepadShortcut] = useState(null);
-    const [showIndicator, setShowIndicator] = useState(false);
+    const [availability, setAvailability] = useState(DEFAULT);
     const [shortcutName, setShortcutName] = useState('');
-    const [inUseError, setInUseError] = useState(false);
+    const listenerRef = useRef();
 
     const gamepadListener = ({ detail }) => {
         const { gamepad: currentGamepad } = detail;
@@ -43,22 +45,22 @@ const ProfileShortcutModal = ({ profile, shortcut, onClose, onUpdateProfiles }) 
         const comboKeys = shortcutComboBuilder(clickedButtons.map(shortcut => shortcut.buttonIndex));
 
         const comboInUse = currentProfile.shortcuts
-            .filter(currShortcut => currShortcut.id !== shortcut.id)
             .find(shortcut => shortcut.keys === comboKeys);
 
-        setInUseError(!!comboInUse);
-        setGamepadShortcut(clickedButtons);
-        setShowIndicator(true);
-
-        if (queue.length) {
-            queue.forEach(item => clearTimeout(item));
+        if (!comboInUse) {
+            setAvailability(AVAILABLE);
         }
 
-        const newTimeout = () => {
-            setShowIndicator(false);
-        };
+        console.log({ comboInUse, shortcut });
 
-        queue.push(setTimeout(newTimeout, 3000));
+        if (comboInUse?.id === profile?.id) {
+            setAvailability(IS_THE_SAME);
+        } else {
+            setAvailability(UNAVAILABLE);
+        }
+
+        setGamepadShortcut(clickedButtons);
+        listenerRef.current.handleButtonPress();
     };
 
     useEffect(() => {
@@ -68,7 +70,6 @@ const ProfileShortcutModal = ({ profile, shortcut, onClose, onUpdateProfiles }) 
 
         return () => {
             gamepad.removeEventListener('gamepad:button', gamepadListener);
-            queue.forEach(item => clearTimeout(item));
         };
     }, []);
 
@@ -101,53 +102,77 @@ const ProfileShortcutModal = ({ profile, shortcut, onClose, onUpdateProfiles }) 
         });
     };
 
-    const RenderButtonsPressed = () => {
-        return gamepadShortcut
-            ? (
-                <>
-                    {
-                        gamepadShortcut.map((shortcut, i) => (
-                            i === 0
-                                ? <strong key={shortcut.buttonIndex}>{shortcut.buttonIndex}</strong>
-                                : <React.Fragment key={shortcut.buttonIndex}> and <strong>{shortcut.buttonIndex}</strong></React.Fragment>
-                        ))
-                    }
-                </>
-            )
-            : <>-</>;
+    const ButtonsPressed = () => {
+        if (!gamepadShortcut) {
+            return null;
+        }
+        return (
+            <div>
+                {
+                    gamepadShortcut.map((shortcut, i) => (
+                        i === 0
+                            ? <strong key={shortcut.buttonIndex}>{shortcut.buttonIndex}</strong>
+                            : <React.Fragment key={shortcut.buttonIndex}> and <strong>{shortcut.buttonIndex}</strong></React.Fragment>
+                    ))
+                }
+            </div>
+        );
+    };
+
+    const Availability = ({ type }) => {
+        const output = {
+            [DEFAULT]: (
+                <div className={styles.availability}>
+                    <i className="fas fa-info-circle" />
+                    <p style={{ textAlign: 'center' }}>Press any button or button combination on your gamepad</p>
+                </div>
+            ),
+            [AVAILABLE]: (
+                <div className={styles.available}>
+                    <i className={classnames('fas fa-check-circle')} />
+                    <p style={{ margin: 0 }}>Shortcut is Availabile</p>
+                    <ButtonsPressed />
+                </div>
+            ),
+            [UNAVAILABLE]: (
+                <div className={styles.unavailable}>
+                    <i className={classnames('fas fa-times-circle')} />
+                    <p style={{ margin: 0 }}>Shortcut Already Exists on an Action</p>
+                </div>
+            ),
+            [IS_THE_SAME]: (
+                <div className={styles.availability}>
+                    <i className={classnames('fas fa-info-circle')} />
+                    <p style={{ margin: 0 }}>This is the Current Shortcut for This Action</p>
+                </div>
+            ),
+        }[type];
+
+        return (
+            <div style={{ position: 'relative', height: '100%' }}>
+                <span style={{ position: 'absolute', top: 10, left: 10, backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: 10 }}>
+                    {shortcut.title}
+                </span>
+                <Listener ref={listenerRef} />
+                {output}
+            </div>
+        );
     };
 
     return (
-        <Modal onClose={onClose} size="small" title="Gamepad Profile Action">
+        <Modal onClose={onClose} size="small" title="Set Gamepad Profile Action">
             <div className={styles.profileActionWrapper}>
-                <h5 style={{ marginTop: 0, textAlign: 'center' }}>Press Any Button on Your Gamepad</h5>
+                <Availability type={availability} />
 
-                <div>
-                    <div style={{ display: 'grid', justifyItems: 'center', gap: '1rem', alignItems: 'center', gridTemplateRows: '1fr 1fr' }}>
-                        <div className={classnames(styles.activeIndicator, { [styles.activeIndicatorOn]: showIndicator })}>
-                            { showIndicator && <i className={classnames('fas fa-gamepad', styles.activePulse)} /> }
-                        </div>
-                        {showIndicator ? <strong>Button press detected</strong> : <span>Waiting for button press...</span>}
-                    </div>
+                <Input
+                    value={shortcutName}
+                    onChange={(e) => setShortcutName(e.target.value)}
+                    additionalProps={{ placeholder: 'Shortcut Name...', disabled: !gamepadShortcut }}
+                    isNumber={false}
+                    style={{ margin: 0 }}
+                />
 
-                    <div style={{ margin: '1rem 0', textAlign: 'center' }}>
-                        Last Button(s) Pressed: <RenderButtonsPressed />
-                    </div>
-
-                    <div style={{ margin: '1.5rem 0', textAlign: 'center', fontSize: '1.3rem' }}>
-                        { (!gamepadShortcut && !inUseError) && <span>-</span> }
-                        { inUseError && <span style={{ color: '#dc2626' }}>This Shortcut is Already in Use, Please Use a Different One</span> }
-                        { (gamepadShortcut && !inUseError) && <span style={{ color: '#3e85c7' }}>Shortcut is Available</span>}
-                    </div>
-
-                    <Input
-                        value={shortcutName}
-                        onChange={(e) => setShortcutName(e.target.value)}
-                        additionalProps={{ placeholder: 'Shortcut Name...', disabled: !gamepadShortcut }}
-                    />
-                </div>
-
-                <Button primary onClick={handleAddShortcut} disabled={!gamepadShortcut || inUseError}>Set shortcut for {`"${shortcut.title}"`}</Button>
+                <Button primary onClick={handleAddShortcut} disabled={!gamepadShortcut || availability !== AVAILABLE}>Set Shortcut</Button>
             </div>
         </Modal>
     );
