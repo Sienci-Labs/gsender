944556909fe3f2d9366b976bc3f80a777a9d93eb
Walid Kayhan <walid.kayhan@outlook.com>
2022-11-07 16:45:37 -0500

    Cleanup

--- src/app/containers/Preferences/Shortcuts/Gamepad/ProfileList.js
+++ src/app/containers/Preferences/Shortcuts/Gamepad/ProfileList.js
@@ -10,16 +10,18 @@ const ProfileList = ({ profiles, onClick, onDelete, onAdd }) => {
         <>
             <div className={profiles.length ? styles.profileList : styles.profileListEmpty}>
                 {
-                    !profiles.length ? <p style={{ fontSize: '1.5rem' }}>No Profiles, Click the Button Below to Add One</p> : profiles.map(({ id, profileName, icon = 'fas fa-gamepad' }) => (
-                        <ProfileItem
-                            key={id}
-                            id={id}
-                            title={profileName}
-                            icon={icon}
-                            onClick={onClick}
-                            onDelete={onDelete}
-                        />
-                    ))
+                    !profiles.length
+                        ? <p style={{ fontSize: '1.5rem' }}>No Profiles, Click the Button Below to Add One</p>
+                        : profiles.map(({ id, profileName, icon = 'fas fa-gamepad' }) => (
+                            <ProfileItem
+                                key={id}
+                                id={id}
+                                title={profileName}
+                                icon={icon}
+                                onClick={onClick}
+                                onDelete={onDelete}
+                            />
+                        ))
                 }
             </div>
 
