8494e688a2f2cafea79c69ca4ded2b59160f353a
swagat <sinu2290@gmail.com>
2022-11-24 12:10:58 -0500

    Validation errors displayed on modal

--- src/app/components/HeadlessIndicator/index.jsx
+++ src/app/components/HeadlessIndicator/index.jsx
@@ -25,35 +25,51 @@ import React, { useState, useEffect } from 'react';
 import Switch from '@mui/material/Switch';
 import Button from '@mui/material/Button';
 import Select from 'react-select';
-import { Toaster, TOASTER_SUCCESS, TOASTER_WARNING } from 'app/lib/toaster/ToasterLib';
+import { Toaster, TOASTER_SUCCESS } from 'app/lib/toaster/ToasterLib';
 import Tooltip from '../TooltipCustom/ToolTip';
 import HeadlessConfig from './HeadlessConfig';
-import store from '../../store';
 import styles from './index.styl';
 
 
 const HeadlessIndicator = ({ address, port }) => {
+    const defaultHeadlessSettings = { ip: { value: '', label: '' }, port: 8000, headlessStatus: false };
+    const defaultErrorMessage = { ipError: '', portError: '' };
     const [showConfig, setShowConfig] = useState(false);
-    const [headlessMode, setHeadlessMode] = useState(false);
-    const [currentPort, setCurrentPort] = useState(0); //TODO - set this on app load
-    const [ipList, setIpList] = useState([{}]);
-    const [currentIp, setCurrentIp] = useState(''); //TODO - set this on app load
+    const [headlessSettings, setHeadlessSettings] = useState(defaultHeadlessSettings); //TODO - set IP and PORT on app load
+    const [settingErrors, setSettingErrors] = useState(defaultErrorMessage);
+    const [ipList, setIpList] = useState([{ value: '', label: '' }]);
 
     const handleShowConfig = () => {
         setShowConfig(true);
-    };
-    const updateHeadlessStatus = () => {
-        setHeadlessMode(!headlessMode);
-    };
-    const updatePort = (event) => {
-        setCurrentPort(event.target.value);
+        setSettingErrors(defaultErrorMessage);
     };
     //handles enter key press in IP address input field
     const handleEnterKey = (event) => {
         if (event.keyCode === 13) {
-            setCurrentIp({ value: event.target.value, label: event.target.value });
+            setHeadlessSettings({ ...headlessSettings, ip: { value: event.target.value, label: event.target.value } });
+            setSettingErrors({ ...settingErrors, ipError: '' });
             event.target.blur();
         }
+    };
+    //handles any change on IP, PORT and Headless toggle
+    const handleInputChanges = (event) => {
+        //handle select
+        if (!event.target) {
+            setHeadlessSettings({ ...headlessSettings, ip: event });
+            return;
+        }
+        const { name, value } = event.target;
+        //Reset errors when user starts typing
+        name === 'ip' ? setSettingErrors({ ...settingErrors, ipError: '' }) : setSettingErrors({ ...settingErrors, portError: '' });
+        //handle toggle change
+        if (name === 'headlessStatus') {
+            setHeadlessSettings({ ...headlessSettings, headlessStatus: !headlessSettings.headlessStatus });
+            setSettingErrors(defaultErrorMessage);
+            return;
+        }
+
+        //Save new setting
+        setHeadlessSettings({ ...headlessSettings, [name]: value });
     };
     const copyToClipboard = () => {
         navigator.clipboard.writeText(`${address}:${port}`);
@@ -62,57 +78,59 @@ const HeadlessIndicator = ({ address, port }) => {
             type: TOASTER_SUCCESS,
         });
     };
-    const updateRemotePreferences = () => {
-    //Validations
+    const validateInputs = () => {
+        if (!headlessSettings.headlessStatus) {
+            //TODO - if user changed from toggle ON to OFF, send a restart command with new settings
+
+
+            setShowConfig(false);
+            //Else don't do anything when user hits OK button
+            return true;
+        }
+        const errors = defaultErrorMessage;
+        let hasError = false;
         //Port
         //Only values between 1025 and 65535
-        if (currentPort.value < 1025 || currentPort.value > 65535) {
-            Toaster.pop({
-                msg: 'Invalid port',
-                type: TOASTER_WARNING,
-            });
-            return;
+        if (headlessSettings.port < 1025 || headlessSettings.port > 65535) {
+            errors.portError = 'Please enter a valid port number';
+            hasError = true;
         }
         //IP
         // 210.110 – must have 4 octets
         // y.y.y.y – format allowed
         // 255.0.0.y – format allowed
         // 666.10.10.20 – digits must be between [0-255]
-        if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(currentIp.value)) {
-            Toaster.pop({
-                msg: 'Invalid IP Address',
-                type: TOASTER_WARNING,
-            });
+        if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(headlessSettings.ip.value)) {
+            errors.ipError = 'Invalid IP Address';
+            hasError = true;
+        }
+        setSettingErrors(errors);
+        return hasError;
+    };
+    const updateRemotePreferences = () => {
+        //Validations
+        if (validateInputs()) {
             return;
         }
-
         //Update settings logic
         //If remote mode toggle is off, clear current IP and port
-        //else restart application
-        if (!headlessMode) {
-            setCurrentIp('');
-            setCurrentPort(0);
+        if (!headlessSettings.headlessStatus) {
+            setHeadlessSettings(defaultHeadlessSettings);
         }
 
-        //Save port and IP in .sender_rc
-        store.set('remotePort', currentPort);
-        store.set('remoteIp', currentIp);
-
-        //TODO - Backend logic
+        //TODO - Save port and IP in .sender_rc (even if empty)
 
 
-        Toaster.pop({
-            msg: 'Changes saved, app will restart.',
-            type: TOASTER_SUCCESS,
-        });
         setShowConfig(false);
+        //TODO - pop a confirmation box only if settings changed
 
-        //TODO- Restart app here with new changes
+
+        //TODO- Restart app here with new changes or don't do anything if no changes made
     };
 
     //refresh IP list on reload
     useEffect(() => {
-        setIpList([{ value: 'todo IP', label: 'todo IP' }]);//TODO - fetch real data
+        setIpList([{ value: '', label: '' }]);//TODO - fetch real data
     }, []);
 
     return (
@@ -131,72 +149,76 @@ const HeadlessIndicator = ({ address, port }) => {
             </Tooltip>
             {port
                 ? (
-                    <div>Remote:
-                        <span className={styles.ip}>{address}:{port}</span>
+                    <div>
+                        <span
+                            className={styles.ip}
+                            onClick={copyToClipboard}
+                            tabIndex={-2}
+                            onKeyDown={() => {
+                                return;
+                            }}
+                            role="button"
+                        >Remote: {address}:{port}
+                        </span>
                     </div>
                 )
                 : ''
             }
             <HeadlessConfig
                 show={showConfig}
-                title="Connection Configuration"
+                title="Remote Mode Configuration"
                 onClose={() => {
                     setShowConfig(false);
                 }}
             >
                 <div className={styles.configContentWrapper}>
                     <div className={styles.toggleWrapper}>
-                        <span>Remote Mode:</span>
+                        <span>Enable Remote Mode:</span>
                         <Switch
-                            checked={headlessMode}
-                            onChange={updateHeadlessStatus}
+                            name="headlessStatus"
+                            checked={headlessSettings.headlessStatus}
+                            onChange={handleInputChanges}
                         />
-                        {
-                            port
-                                ? (
-                                    <span
-                                        className={styles.copyAddress} onClick={copyToClipboard}
-                                        tabIndex={-2}
-                                        onKeyDown={() => {
-                                            return;
-                                        }}
-                                        role="button"
-                                    >
-                                        <i className="fa fa-copy" /> Copy remote address
-                                    </span>
-                                )
-                                : ''
-                        }
                     </div>
                     <div className={styles.connectionWrapper}>
                         Please choose your remote IP and Port Bellow: <br />
-                        {/* <div className={styles.inputWrapper}>
-
-                            <Select />
-                        </div> */}
                         <div className={styles.inputWrapper}>
                             <div className={styles.titles}>
                                 <div className={styles.titleIp}>IP: &#32;</div>
                                 <div className={styles.titlePort}>Port: &#32;</div>
                             </div>
                             <div className={styles.inputs}>
-                                <Select
-                                    placeholder="select or type IP"
-                                    value={currentIp}
-                                    options={ipList}
-                                    onChange={(event) => {
-                                        setCurrentIp(event);
-                                    }}
-                                    onKeyDown={(event) => handleEnterKey(event)}
-                                />
+                                <div className={styles.ipInput}>
+                                    <Select
+                                        name="ip"
+                                        placeholder="select or type IP"
+                                        value={headlessSettings.ip}
+                                        options={ipList} //{value: , label: }
+                                        onChange={handleInputChanges}
+                                        onKeyDown={(event) => handleEnterKey(event)}
+                                        isDisabled={!headlessSettings.headlessStatus}
+                                        className={settingErrors.ipError ? styles.invalidInput : ''}
+                                        styles={{
+                                            container: base => ({
+                                                ...base,
+                                                width: '15rem',
+                                            })
+                                        }}
+                                    />
+                                    {settingErrors.ipError ? <span className={styles.warningText}> <i className="fa fa-exclamation-circle" /> {settingErrors.ipError} </span> : ''}
+                                </div>
                                 <div className={styles.portInput}>
                                     <input
+                                        name="port"
                                         type="number"
-                                        value={currentPort}
+                                        value={headlessSettings.port}
                                         min={0}
-                                        onChange={updatePort}
+                                        onChange={handleInputChanges}
                                         placeholder="port number"
+                                        disabled={!headlessSettings.headlessStatus}
+                                        className={settingErrors.portError ? styles.invalidInput : ''}
                                     />
+                                    {settingErrors.portError ? <span className={styles.warningText}> <i className="fa fa-exclamation-circle" /> {settingErrors.portError} </span> : ''}
                                 </div>
                             </div>
                         </div>
