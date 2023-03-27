84cd875b36ef3a9e745086213e8546286d55f3d6
Walid Kayhan <walid.kayhan@outlook.com>
2023-03-27 13:09:47 -0400

    Cleanup: Rotary object extends from threejs group

--- src/app/widgets/Visualizer/RotaryStock.jsx
+++ src/app/widgets/Visualizer/RotaryStock.jsx
@@ -1,15 +1,14 @@
 import * as THREE from 'three';
 
-class RotaryStock {
-    obj = null;
-
+class RotaryStock extends THREE.Group {
     constructor(params = {}) {
+        super();
         return this.createObject(params);
     }
 
     createObject = ({
-        radiusTop = 5,
-        radiusBottom = 5,
+        radiusTop = 2,
+        radiusBottom = 2,
         height = 30,
         radialSegments = 15,
         heightSegments = 5,
@@ -17,20 +16,16 @@ class RotaryStock {
         name,
         visible = true,
     } = {}) => {
-        const group = new THREE.Group();
-
         const geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded);
         geometry.translate(0, height / 2, 0);
 
         const material = new THREE.MeshStandardMaterial({ color: 'grey', wireframe: true });
         const cylinder = new THREE.Mesh(geometry, material);
 
-        group.name = name;
-        group.visible = visible;
-        group.rotateZ(-Math.PI / 2);
-        group.add(cylinder);
-
-        this.obj = group;
+        this.name = name;
+        this.visible = visible;
+        this.rotateZ(-Math.PI / 2);
+        this.add(cylinder);
     }
 
     updateSize = (height = 100) => {
