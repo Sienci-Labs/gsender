1aab6ab97105aca63d49d66d1aab894579512350
Sophia Beluli <sophiabeluli@Sophias-MacBook.local>
2023-04-20 11:43:07 -0400

    changed gcode to use G53 Z-{number} instead of upwards Z movements; set wizard 'running gcode' state to false on error

--- src/app/sagas/controllerSagas.js
+++ src/app/sagas/controllerSagas.js
@@ -512,6 +512,7 @@ export function* initialize() {
         if (isElectron() && (error.type === 'GRBL_ALARM' || error.type === 'GRBL_ERROR')) {
             window.ipcRenderer.send('logError:electron', error);
         }
+        pubsub.publish('error', error);
     });
 
     controller.addListener('wizard:next', (stepIndex, substepIndex) => {
