590949b15162fd48e2d09a457835ea812992ca8b
kgn <kevin.glovernetherton@gmail.com>
2023-06-08 16:30:10 -0400

    Angry red introduction on toolchanging

--- src/app/components/Wizard/components/Introduction.jsx
+++ src/app/components/Wizard/components/Introduction.jsx
@@ -4,6 +4,7 @@ import styles from '../index.styl';
 const Introduction = ({ description, title }) => {
     return (
         <div className={styles.introduction}>
+            <i className="fas fa-exclamation-circle" />
             <h2>{title}</h2>
             <p>{description}</p>
         </div>
