1775418e9bed48ad50915b1be9b19b9db3a8d2af
swagat <sinu2290@gmail.com>
2023-06-09 13:22:45 -0400

    revert guard statement

--- src/server/services/cncengine/CNCEngine.js
+++ src/server/services/cncengine/CNCEngine.js
@@ -323,12 +323,8 @@ class CNCEngine {
 
                 controller.addConnection(socket);
 
-                // Check if a job is already loaded and running
-                if (this.hasFileLoaded() && this.isJobRunning()) {
-                    // A job is already running, don't load the file
-                    log.debug('A job is already loaded and running on the controller');
-                    socket.emit('file:load', this.gcode, this.meta.size, this.meta.name);
-                } else if (this.hasFileLoaded()) { // Load file to controller if it exists
+                // Load file to controller if it exists
+                if (this.hasFileLoaded()) {
                     if (numClients === 0) {
                         controller.loadFile(this.gcode, this.meta);
                         socket.emit('file:load', this.gcode, this.meta.size, this.meta.name);
