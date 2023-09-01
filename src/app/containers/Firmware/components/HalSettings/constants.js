2e4478544a0f4140868395d6f6422abed2c26558
KGN <kevin.glovernetherton@gmail.com>
2023-09-01 15:32:58 -0400

    Added constants for field types

---
+++ src/app/containers/Firmware/components/HalSettings/constants.js
@@ -0,0 +1,11 @@
+// Data Types for inputs
+export const BOOLEAN = 0;
+export const BITFIELD = 1;
+export const EXCLUSIVE_BITFIELD = 2;
+export const RADIO_BUTTON = 3;
+export const AXIS_MASK = 4;
+export const INTEGER = 5;
+export const DECIMAL = 6;
+export const STRING = 7;
+export const PASSWORD = 8;
+export const IPV4 = 9;
