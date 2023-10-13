c3d078d1ddb0ff5fb4fde224d0f597c39cdf734b
KGN <kevin.glovernetherton@gmail.com>
2023-10-12 22:15:48 -0400

    Pins, pos, substate all parsed correctly on complete report

--- src/server/controllers/Grblhal/GrblHalLineParserResultCompleteStatus.js
+++ src/server/controllers/Grblhal/GrblHalLineParserResultCompleteStatus.js
@@ -2,6 +2,8 @@
 // More specific Full report sent on startup
 //import _ from 'lodash';
 
+import _ from 'lodash';
+
 class GrblHalLineParserResultCompleteStatus {
     static parse(line) {
         const r = line.match(/^<(Idle|Run|Hold|Jog|Alarm|Door|Check|Home|Sleep|Tool)(:\d*)?\|(.*\|FW:grblHAL)>$/);
@@ -16,22 +18,68 @@ class GrblHalLineParserResultCompleteStatus {
         const substate = r[2];
 
         const payload = {};
-        const results = {};
+        const result = {};
 
         // Separate parameters and store them in results, further split by comma for sub values
         {
             r[3].split('|').forEach((param) => {
                 let parts = param.split(':');
-                results[parts[0]] = parts[1].split(',') || null;
+                result[parts[0]] = parts[1].split(',') || null;
             });
         }
 
-        console.log(results);
+        console.log(result);
 
         {
             // Active state (Idle, Jog, etc) and substate (mostly alarm/door)
             payload.activeState = state;
             payload.substate = substate || '';
+        }
+
+        // Machine Position (v0.9, v1.1)
+        if (_.has(result, 'MPos')) {
+            const axes = ['x', 'y', 'z', 'a', 'b', 'c'];
+            const mPos = _.get(result, 'MPos', ['0.000', '0.000', '0.000']); // Defaults to [x, y, z]
+            payload.mpos = {};
+            for (let i = 0; i < mPos.length; ++i) {
+                payload.mpos[axes[i]] = mPos[i];
+            }
+        }
+
+        // Work Position (v0.9, v1.1)
+        if (_.has(result, 'WPos')) {
+            const axes = ['x', 'y', 'z', 'a', 'b', 'c'];
+            const wPos = _.get(result, 'WPos', ['0.000', '0.000', '0.000']); // Defaults to [x, y, z]
+            payload.wpos = {};
+            for (let i = 0; i < wPos.length; ++i) {
+                payload.wpos[axes[i]] = wPos[i];
+            }
+        }
+
+        // Work Coordinate Offset (v1.1)
+        if (_.has(result, 'WCO')) {
+            const axes = ['x', 'y', 'z', 'a', 'b', 'c'];
+            const wco = _.get(result, 'WCO', ['0.000', '0.000', '0.000']); // Defaults to [x, y, z]
+            payload.wco = {};
+            for (let i = 0; i < wco.length; ++i) {
+                payload.wco[axes[i]] = wco[i];
+            }
+        }
+
+        // Input Pin State (v1.1)
+        // * Pn:XYZPDHRS indicates which input pins Grbl has detected as 'triggered'.
+        // * Each letter of XYZPDHRS denotes a particular 'triggered' input pin.
+        //   - X Y Z XYZ limit pins, respectively
+        //   - P the probe pin.
+        //   - D H R S the door, hold, soft-reset, and cycle-start pins, respectively.
+        //   - Example: Pn:PZ indicates the probe and z-limit pins are 'triggered'.
+        //   - Note: A may be added in later versions for an A-axis limit pin.
+        if (_.has(result, 'Pn')) {
+            const pins = _.get(result, 'Pn[0]', '');
+            payload.pinState = {};
+            pins.split('').forEach(pin => {
+                payload.pinState[pin] = true;
+            });
         }
 
         return {
