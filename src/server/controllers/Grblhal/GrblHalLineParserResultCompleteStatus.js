02c240c4d4723873a3aab444bca78beca7d8a79e
KGN <kevin.glovernetherton@gmail.com>
2023-10-12 21:30:42 -0400

    Now just the logic :)

--- src/server/controllers/Grblhal/GrblHalLineParserResultCompleteStatus.js
+++ src/server/controllers/Grblhal/GrblHalLineParserResultCompleteStatus.js
@@ -1,5 +1,7 @@
 
 // More specific Full report sent on startup
+//import _ from 'lodash';
+
 class GrblHalLineParserResultCompleteStatus {
     static parse(line) {
         const r = line.match(/^<(Idle|Run|Hold|Jog|Alarm|Door|Check|Home|Sleep|Tool)(:\d*)?\|(.*\|FW:grblHAL)>$/);
@@ -10,8 +12,27 @@ class GrblHalLineParserResultCompleteStatus {
         console.log(line);
         console.log(r);
 
+        const state = r[1];
+        const substate = r[2];
+
         const payload = {};
-        //const result = {};
+        const results = {};
+
+        // Separate parameters and store them in results, further split by comma for sub values
+        {
+            r[3].split('|').forEach((param) => {
+                let parts = param.split(':');
+                results[parts[0]] = parts[1].split(',') || null;
+            });
+        }
+
+        console.log(results);
+
+        {
+            // Active state (Idle, Jog, etc) and substate (mostly alarm/door)
+            payload.activeState = state;
+            payload.substate = substate || '';
+        }
 
         return {
             type: GrblHalLineParserResultCompleteStatus,
