235fa6cba270d950ef3cf7b4ff6a6ecf9d83f68d
kgn <kevin.glovernetherton@gmail.com>
2023-10-19 14:50:41 -0400

    HAL Firmware tool now uses correct axes mask values

--- src/app/containers/Firmware/components/HalSettings/inputs/AxisMaskInput.jsx
+++ src/app/containers/Firmware/components/HalSettings/inputs/AxisMaskInput.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import get from 'lodash/get';
+import { connect } from 'react-redux';
 import BitfieldInput from 'Containers/Firmware/components/HalSettings/inputs/BitfieldInput';
 
-const AxisMaskInput = ({ info, setting, onChange }) => {
+const AxisMaskInput = ({ info, setting, onChange, axes }) => {
     return (
         <>
-            <BitfieldInput info={info} setting={setting} onChange={onChange} externalFormat={['X', 'Y', 'Z', 'A']} />
+            <BitfieldInput info={info} setting={setting} onChange={onChange} externalFormat={axes} />
         </>
     );
 };
 
-export default AxisMaskInput;
+export default connect((store) => {
+    const axes = get(store, 'controller.state.axes.axes',k ['X', 'Y', 'Z']);
+    return {
+        axes
+    };
+})(AxisMaskInput);
