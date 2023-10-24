2140d436d9a4b5be2b4790a5940c3cba6a4160b0
KGN <kevin.glovernetherton@gmail.com>
2023-10-24 13:02:52 -0400

    Direction button shape changes

---
+++ src/app/widgets/Probe/ProbeDirectionSelection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.styl';
+
+const ProbeDirectionSelection = ({ direction }) => {
+    return (
+        <button type="button" className={styles.directionButton}>
+            hi
+        </button>
+    );
+};
+
+export default ProbeDirectionSelection;
