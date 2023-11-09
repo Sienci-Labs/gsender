a79b596a00cc4a375489fb05612a45ff8cc4d969
Sophia Beluli <sophiabeluli@Sophias-MacBook.local>
2023-11-09 10:40:31 -0500

    removed unnecessary config

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,5 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:8000',
-  },
-  env: {
-    grbl_machine: '/dev/tty.usbmodem21101',
-    grblhal_machine: '/dev/tty.usbmodem3069383D33311',
   }
 });
