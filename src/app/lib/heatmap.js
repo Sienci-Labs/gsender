8c71c810e693a6f6adc3672270c8154fd0003569
Walid Kayhan <walid.kayhan@outlook.com>
2023-12-01 11:16:37 -0500

    Dont collect usage data if user did not consent

--- src/app/lib/heatmap.js
+++ src/app/lib/heatmap.js
@@ -1,6 +1,15 @@
 import api from 'app/api';
+import { USER_DATA_COLLECTION } from 'app/constants';
 
 export const collectUserUsageData = async (toolName) => {
+    const res = await api.metrics.getCollectDataStatus();
+
+    const collectUserDataStatus = res.body.collectUserDataStatus;
+
+    if (collectUserDataStatus !== USER_DATA_COLLECTION.ACCEPTED) {
+        return;
+    }
+
     try {
         await api.metrics.sendUsageData({ data: toolName });
     } catch (error) {
