361dd47f2726124eea057994b8eb9b0a46c9d7e6
KGN <kevin.glovernetherton@gmail.com>
2024-02-20 09:10:07 -0500

    Signing changes

--- WindowsSigner.js
+++ WindowsSigner.js
@@ -12,7 +12,7 @@ const { execSync } = require('child_process');
 
 // eslint-disable-next-line require-await
 exports.default = async (config) => {
-    const keypairAlias = process.env.KEYPAIR_ALIAS;
+    const keypairAlias = 'key_612024263';
     const path = config.path ? String(config.path) : '';
 
     if (process.platform !== 'win32' || !keypairAlias || !path) {
@@ -20,7 +20,7 @@ exports.default = async (config) => {
     }
 
     const output = execSync(
-        `smctl sign --keypair-alias=${keypairAlias} --input="${path}" --verbose`,
+        `smctl sign --keypair-alias=${keypairAlias} --certificate="C:/Certs/sienci.p12" --input="${path}" --verbose`,
     )
         .toString()
         .trim();
