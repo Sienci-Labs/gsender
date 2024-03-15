7f2ce421a7c9eb3fa1ce0a66c32431a7628a3922
Walid Kayhan <walid.kayhan@outlook.com>
2024-03-15 10:33:29 -0400

    Update label text for firmware selector in navbar connection

--- src/app/widgets/NavbarConnection/FirmwareSelector.jsx
+++ src/app/widgets/NavbarConnection/FirmwareSelector.jsx
@@ -4,7 +4,7 @@ import styles from './Index.styl';
 const FirmwareSelector = ({ list, onClick }) => {
     return (
         <button type="button" className={styles.otherDevices} onClick={onClick}>
-            <span>Select Firmware ({list.length})</span>
+            <span>Firmware ({list.length})</span>
             <i className="fas fa-fixed fa-arrow-circle-right" />
         </button>
     );
