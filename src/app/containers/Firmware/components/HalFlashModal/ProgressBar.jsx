59350846391886d19d00fff8c5d7fc63c50c93a0
Walid Kayhan <walid.kayhan@outlook.com>
2024-03-28 12:10:24 -0400

    Add better job completion display; Add listener for flashing errors; Small UI updates

--- src/app/containers/Firmware/components/HalFlashModal/ProgressBar.jsx
+++ src/app/containers/Firmware/components/HalFlashModal/ProgressBar.jsx
@@ -3,10 +3,17 @@ import { ProgressBar } from 'react-bootstrap';
 import styles from './index.styl';
 
 const ProgressBarWrapper = ({ sent, total }) => {
-    const now = (sent / total).toFixed(1) * 100;
+    const now = sent === 0 && total === 0 ? 0 : (sent / total).toFixed(1) * 100;
+
     return (
         <div className={styles.progressArea}>
-            <ProgressBar striped variant="info" animated now={now} label={`${now}%`}/>
+            <ProgressBar
+                striped
+                variant="info"
+                animated
+                now={now}
+                label={`${now}%`}
+            />
         </div>
     );
 };
