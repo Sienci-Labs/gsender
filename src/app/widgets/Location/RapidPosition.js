8d46375e36dac9623f7844536b55139ae79c0253
KGN <kevin.glovernetherton@gmail.com>
2024-04-11 08:57:46 -0400

    Bitwise logging removal

--- src/app/widgets/Location/RapidPosition.js
+++ src/app/widgets/Location/RapidPosition.js
@@ -40,7 +40,6 @@ export const getHomingLocation = (setting) => {
     setting = Number(setting);
     // eslint-disable-next-line no-bitwise
     setting &= 7;
-    console.log(setting);
 
     if (setting === 0) {
         return BACK_RIGHT;
