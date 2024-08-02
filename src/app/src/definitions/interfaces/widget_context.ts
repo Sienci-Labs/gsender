e65927969c2977c7abe16eae14a72a87fbc9a8d8
Sophia Beluli <sophiabeluli@gmail.com>
2024-08-02 16:23:36 -0400

    typo

--- src/app/src/definitions/interfaces/widget_context.ts
+++ src/app/src/definitions/interfaces/widget_context.ts
@@ -2,8 +2,8 @@ export interface WidgetContext {
     widgetId: string,
     actions: {
         get: (key: string, defaultValue: any) => void,
-        set: (key: string, value: any) => boolean ,
-        unset: (key: string) => object
-        replace: (key: string, value: any) => object 
+        set: (key: string, value: any) => boolean,
+        unset: (key: string) => object,
+        replace: (key: string, value: any) => object,
     }
 }
