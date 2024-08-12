fc52800fdac2eca4a19d9605478a380958c98048
Walid Kayhan <walid.kayhan@outlook.com>
2024-08-12 10:47:22 -0400

    Moved vite config to app directory; Add tanstack router plugin to vite

---
+++ src/app/vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+export default defineConfig({
+    root: path.resolve(__dirname, "./"), // Set root to the directory containing index.html
+    base: "./",
+    build: {
+        outDir: path.resolve(__dirname, "../../dist/gsender/app"), // Output directly to /output/app
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, "./src/entry-client.tsx"), // Main entry point
+            },
+            output: {
+                assetFileNames: "assets/[name].[ext]",
+            },
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
+    plugins: [
+        TanStackRouterVite({
+            routesDirectory: path.resolve(__dirname, "./src/routes"),
+            generatedRouteTree: path.resolve(
+                __dirname,
+                "./src/routeTree.gen.ts",
+            ),
+        }),
+        react(),
+    ],
+});
