a0b2537d8a8788b0992cc53bf80b1bf522ba219f
Walid Kayhan <walid.kayhan@outlook.com>
2024-08-12 10:45:03 -0400

    Add initial test script

--- src/app/src/entry-client.tsx
+++ src/app/src/entry-client.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import "./index.css";
+import App from "./App";
 
 ReactDOM.hydrateRoot(
-    document.getElementById('app') as HTMLElement,
+    document.getElementById("app") as HTMLElement,
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
 );
