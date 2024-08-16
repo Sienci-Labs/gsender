2ac479c634ea7d460b0533801bfd5ca3ef6fe911
Sophia Beluli <sophiabeluli@gmail.com>
2024-08-16 15:11:30 -0400

    added widget wrapper and example

---
+++ src/app/src/components/Widget/Button.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 Sienci Labs Inc.
+ *
+ * This file is part of gSender.
+ *
+ * gSender is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, under version 3 of the License.
+ *
+ * gSender is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gSender.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact for information regarding this program and its license
+ * can be sent through gSender@sienci.com or mailed to the main office
+ * of Sienci Labs Inc. in Waterloo, Ontario, Canada.
+ *
+ */
+
+import classNames from 'classnames';
+import Anchor from '../Anchor';
+import { WidgetProps } from '../../definitions/interfaces/widget_wrapper';
+
+const Button: React.FC<WidgetProps> = ({ className, inverted, disabled, ...props }) => {
+    return (
+        <Anchor
+            {...props}
+            className={classNames(
+                className,
+                "min-w-8 h-8 float-left relative text-sm font-[$font-family-arial] m-0 text-center first:pl-0",
+                {
+                    "disabled:text-yellow-300 disabled:cursor-not-allowed disabled:hover:bg-inherit": disabled,
+                    "text-white bg-gray-800 hover:bg-gray-900": inverted,
+                    "opacity-40": disabled && inverted,
+                }
+            )}
+        />
+    );
+}
+
+export default Button;
---
+++ src/app/src/components/Widget/Content.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021 Sienci Labs Inc.
+ *
+ * This file is part of gSender.
+ *
+ * gSender is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, under version 3 of the License.
+ *
+ * gSender is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gSender.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact for information regarding this program and its license
+ * can be sent through gSender@sienci.com or mailed to the main office
+ * of Sienci Labs Inc. in Waterloo, Ontario, Canada.
+ *
+ */
+
+import classNames from 'classnames';
+// import { forwardRef } from 'react';
+import { WidgetProps } from '../../definitions/interfaces/widget_wrapper';
+
+const Content: React.FC<WidgetProps> = ({ className, active, ...props }) => (
+    <div
+        {...props}
+        className={classNames(
+            className,
+            "border-solid border-[1px] border-gray-400 relative " +
+            "h-[calc(100% - 2rem)] p-4 min-w-80 bg-gray-100 " +
+            "[@media(max-width: 639px),(max-device-width: 639px)]: p-3"
+        )}
+    />
+);
+
+export default Content;
+// export default forwardRef((props, ref) => <Content {...props} reference={ref} />);
