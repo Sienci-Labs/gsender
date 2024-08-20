6507f8bf23c45baa46b5d7ae0ab109f23d4a3daa
Sophia Beluli <sophiabeluli@gmail.com>
2024-08-20 11:23:13 -0400

    finish testing and remove unnecessary code

--- src/app/src/workspace/Column/index.tsx
+++ src/app/src/workspace/Column/index.tsx
@@ -1,89 +1,17 @@
-// import { Widget } from "../../components/Widget";
-// import { Location } from "../../features/Location";
-
-import { Button } from "../../components/shadcn/Button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../../components/shadcn/Card"
-import { Input } from "../../components/shadcn/Input"
-import { Label } from "../../components/shadcn/Label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "../../components/shadcn/Tabs"
+import { Widget } from "../../components/Widget";
+import { Location } from "../../features/Location";
 
 export const Column = () => {
     return (
         <div className="p-1 flex-1 box-border">
-            {/* <Widget>
+            <Widget>
                 <Widget.Content
                     className=""
                     style={{ width: '100%' }}
                 >
                     <Location />
                 </Widget.Content>
-            </Widget> */}
-
-
-            <Tabs defaultValue="account" className="w-[400px]">
-                <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="account">Account</TabsTrigger>
-                <TabsTrigger value="password">Password</TabsTrigger>
-                </TabsList>
-                <TabsContent value="account">
-                <Card>
-                    <CardHeader>
-                    <CardTitle>Account</CardTitle>
-                    <CardDescription>
-                        Make changes to your account here. Click save when you're done.
-                    </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                        <Label htmlFor="name">Name</Label>
-                        <Input id="name" defaultValue="Pedro Duarte" />
-                    </div>
-                    <div className="space-y-1">
-                        <Label htmlFor="username">Username</Label>
-                        <Input id="username" defaultValue="@peduarte" />
-                    </div>
-                    </CardContent>
-                    <CardFooter>
-                    <Button>Save changes</Button>
-                    </CardFooter>
-                </Card>
-                </TabsContent>
-                <TabsContent value="password">
-                <Card>
-                    <CardHeader>
-                    <CardTitle>Password</CardTitle>
-                    <CardDescription>
-                        Change your password here. After saving, you'll be logged out.
-                    </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                        <Label htmlFor="current">Current password</Label>
-                        <Input id="current" type="password" />
-                    </div>
-                    <div className="space-y-1">
-                        <Label htmlFor="new">New password</Label>
-                        <Input id="new" type="password" />
-                    </div>
-                    </CardContent>
-                    <CardFooter>
-                    <Button>Save password</Button>
-                    </CardFooter>
-                </Card>
-                </TabsContent>
-            </Tabs>
+            </Widget>
         </div>
     );
 };
