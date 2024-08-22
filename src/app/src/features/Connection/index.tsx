a075fd5d63faff091b9bdd56cc43b1968172189c
KGN <kevin.glovernetherton@gmail.com>
2024-08-22 10:20:43 -0400

    Connection area styling changes

--- src/app/src/features/Connection/index.tsx
+++ src/app/src/features/Connection/index.tsx
@@ -1,5 +1,8 @@
 import {useState} from "react";
 import {ConnectionStateIndicator} from "./components/ConnectionStateIndicator.tsx";
+import {ConnectionInfo} from "./components/ConnectionInfo.tsx";
+import styles from './assets/animations.module.css';
+import cn from 'classnames';
 
 export enum ConnectionState {
     DISCONNECTED,
@@ -15,17 +18,38 @@ export enum ConnectionType {
     REMOTE
 }
 
+function getBarStyling(state: ConnectionState): string {
+    switch (state) {
+        case ConnectionState.ERROR:
+            return ''
+        case ConnectionState.CONNECTED:
+            return ''
+        case ConnectionState.DISCONNECTED:
+            return ''
+        case ConnectionState.CONNECTING:
+            return ''
+        default:
+            return ''
+    }
+}
+
 export function Connection() {
     const [connectionState, setConnectionState] = useState(ConnectionState.DISCONNECTED)
     const [connectionType, setConnectionType] = useState(ConnectionType.DISCONNECTED)
 
     return (
-        <div className="relative group cursor-pointer animate-rotato">
+        <div className="relative group cursor-pointer">
+            {
+                connectionState !== ConnectionState.CONNECTED &&
+                <div className={
+                    cn(styles.steam,
+                        "absolute -inset-0.5 bg-gradient-to-r from-blue-300 to-blue-800 rounded-lg blur opacity-70 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"
+                    )
+                }
+                />
+            }
             <div
-                className="absolute -inset-0.5 bg-gradient-to-r from-yellow-600 to-orange-600 rounded-lg blur opacity-35 group-hover:opacity-100 transition duration-1000 group-hover:duration-200">
-            </div>
-            <div
-                className="relative border border-gray-400 bg-gray-100 font-bold  px-4 py-2 ring-1 ring-gray-900/5 gap-4 justify-between items-center rounded-lg leading-none flex flex-row items-top min-w-[250px] animate-rotato">
+                className="relative border border-gray-400 bg-gray-100 font-bold  px-4 py-2 ring-1 ring-gray-900/5 gap-4 justify-between items-center rounded-lg leading-none flex flex-row items-top min-w-[250px]">
                     <ConnectionStateIndicator state={connectionState} type={connectionType}/>
                 {
                     connectionState === ConnectionState.DISCONNECTED && <span>Connect to CNC</span>
@@ -35,6 +59,9 @@ export function Connection() {
                 }
                 {
                     connectionState === ConnectionState.ERROR && <span>Unable to connect.</span>
+                }
+                {
+                    connectionState == ConnectionState.CONNECTED && <ConnectionInfo port="COM7" firmwareType="grblHAL" />
                 }
             </div>
         </div>
