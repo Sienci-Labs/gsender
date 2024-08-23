c2995a2f452692e24d152e28fecc8e19184df24c
Walid Kayhan <walid.kayhan@outlook.com>
2024-08-23 00:01:16 -0400

    Improve build process (work in progress); Update vite

--- src/app/vite.config.js
+++ src/app/vite.config.js
@@ -1,24 +1,13 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    root: path.resolve(__dirname, "./"), // Set root to the directory containing index.html
-    base: "./",
-    build: {
-        outDir: path.resolve(__dirname, "../../dist/gsender/app"), // Output directly to /output/app
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, "./src/entry-client.tsx"), // Main entry point
-            },
-            output: {
-                assetFileNames: "assets/[name].[ext]",
-            },
-        },
-    },
+    root: path.resolve(__dirname, './'), // Set root to the directory containing index.html
+    base: './',
     css: {
         postcss: {
             plugins: [tailwindcss()],
@@ -26,10 +15,10 @@ export default defineConfig({
     },
     plugins: [
         TanStackRouterVite({
-            routesDirectory: path.resolve(__dirname, "./src/routes"),
+            routesDirectory: path.resolve(__dirname, './src/routes'),
             generatedRouteTree: path.resolve(
                 __dirname,
-                "./src/routeTree.gen.ts",
+                './src/routeTree.gen.ts',
             ),
         }),
         tsconfigPaths(),
