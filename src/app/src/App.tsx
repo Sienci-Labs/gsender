426552a742becda4c35f159ead1c748455523627
Walid Kayhan <walid.kayhan@outlook.com>
2024-08-26 16:04:18 -0400

    Add logic to authenticate and connect to backend controller

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -1,10 +1,16 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { Provider as ReduxProvider } from 'react-redux';
-import { store } from './store/redux';
+import { store as reduxStore } from './store/redux';
+import rootSaga from './store/redux/sagas';
+import { sagaMiddleware } from './store/redux/sagas';
+import store from 'app/store';
+import * as user from 'app/lib/user';
 
 import { routeTree } from './routeTree.gen';
 
 import './index.css';
+import { useEffect } from 'react';
+import controller from './lib/controller';
 
 const router = createRouter({ routeTree });
 
@@ -15,9 +21,32 @@ declare module '@tanstack/react-router' {
 }
 
 function App() {
+    useEffect(() => {
+        const token = store.get('session.token');
+        user.signin({ token }).then((result) => {
+            const { authenticated, token } = result as {
+                authenticated: boolean;
+                token: string;
+            };
+
+            if (authenticated) {
+                const host = '';
+                const options = {
+                    query: 'token=' + token,
+                };
+                controller.connect(host, options, () => {
+                    // @see "src/web/containers/Login/Login.jsx"
+                });
+                return;
+            }
+        });
+
+        sagaMiddleware.run(rootSaga);
+    }, []);
+
     return (
         <>
-            <ReduxProvider store={store}>
+            <ReduxProvider store={reduxStore}>
                 <RouterProvider router={router} />
             </ReduxProvider>
         </>
