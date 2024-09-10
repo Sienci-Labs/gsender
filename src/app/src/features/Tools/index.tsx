d60da45d98dc141c2f1ad0d8f956b37237dd0e0a
Walid Kayhan <walid.kayhan@outlook.com>
2024-09-10 15:51:39 -0400

    Add tab component to tools area

--- src/app/src/features/Tools/index.tsx
+++ src/app/src/features/Tools/index.tsx
@@ -1,14 +1,44 @@
-import {Widget} from "components/Widget";
+import { Widget } from '../../components/Widget';
+import { Tabs } from '../../components/Tabs';
+import { Console } from '../Console';
+
+const Probe = () => {
+    return <div>Probe</div>;
+};
+
+const Coolant = () => {
+    return <div>Coolant</div>;
+};
+
+const Rotary = () => {
+    return <div>Rotary</div>;
+};
+
+const tabs = [
+    {
+        label: 'Console',
+        content: <Console />,
+    },
+    {
+        label: 'Probe',
+        content: <Probe />,
+    },
+    {
+        label: 'Coolant',
+        content: <Coolant />,
+    },
+    {
+        label: 'Rotary',
+        content: <Rotary />,
+    },
+];
 
 export function Tools() {
     return (
         <Widget>
-            <Widget.Content
-                className=""
-                style={{ width: '100%' }}
-            >
-                <div>Tools Area Feature</div>
+            <Widget.Content>
+                <Tabs items={tabs} />
             </Widget.Content>
         </Widget>
-    )
+    );
 }
