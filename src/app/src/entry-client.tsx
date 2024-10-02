213184dab2dbdf66e657b6ee419b027b468c2876
Walid Kayhan <walid.kayhan@outlook.com>
2024-10-02 02:44:06 -0400

    Disable strict mode for now, causes issues with visualizer

--- src/app/src/entry-client.tsx
+++ src/app/src/entry-client.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 
 ReactDOM.hydrateRoot(
-    document.getElementById("app") as HTMLElement,
-    <React.StrictMode>
+    document.getElementById('app') as HTMLElement,
+    <>
         <App />
-    </React.StrictMode>,
+    </>,
 );
--- src/app/src/entry-server.tsx
+++ src/app/src/entry-server.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import App from './App';
-import './index.css'
 
+import App from './App';
+import './index.css';
 
 export function render() {
     const html = ReactDOMServer.renderToString(
-        <React.StrictMode>
+        <>
             <App />
-        </React.StrictMode>
+        </>,
     );
     return { html };
 }
