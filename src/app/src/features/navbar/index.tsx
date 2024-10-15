5ee38734a0168660af487c71c5c2d4c6a5525fb6
KGN <kevin.glovernetherton@gmail.com>
2024-10-15 08:08:42 -0400

    Changed menu to remove missing features

--- src/app/src/features/navbar/index.tsx
+++ src/app/src/features/navbar/index.tsx
@@ -1,6 +1,4 @@
 import { NavbarLink } from './components/NavbarLink.tsx';
-import { FaTasks } from 'react-icons/fa';
-import { HiOutlineChatBubbleOvalLeftEllipsis } from 'react-icons/hi2';
 import { FaCog } from 'react-icons/fa';
 import { MdKeyboardCommandKey } from 'react-icons/md';
 import { MinimizeHandle } from './components/MinimizeHandle.tsx';
@@ -9,13 +7,11 @@ export function NavBar() {
     return (
         <div className="w-20 flex flex-col no-padding no-margin gap-5 justify-end flex-grow self-stretch">
             <NavbarLink href="/" icon={MdKeyboardCommandKey} label="Control" />
-            <NavbarLink href="/configuration" icon={FaTasks} label="Config" />
             <NavbarLink
-                href="/stats"
-                icon={HiOutlineChatBubbleOvalLeftEllipsis}
-                label="Info"
+                href="/configuration"
+                icon={FaCog}
+                label="Preferences"
             />
-            <NavbarLink href="/tools" icon={FaCog} label="Preferences" />
             <MinimizeHandle />
         </div>
     );
