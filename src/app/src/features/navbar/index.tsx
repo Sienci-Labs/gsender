69fc7f2d1bddf7296364b773bdc65cf824729d1e
KGN <kevin.glovernetherton@gmail.com>
2024-10-18 08:22:33 -0400

    Added link to firmware to navbar

--- src/app/src/features/navbar/index.tsx
+++ src/app/src/features/navbar/index.tsx
@@ -1,4 +1,5 @@
 import { NavbarLink } from './components/NavbarLink.tsx';
+import { FaMicrochip } from 'react-icons/fa';
 import { FaCog } from 'react-icons/fa';
 import { MdKeyboardCommandKey } from 'react-icons/md';
 import { MinimizeHandle } from './components/MinimizeHandle.tsx';
@@ -7,6 +8,7 @@ export function NavBar() {
     return (
         <div className="w-20 flex flex-col no-padding no-margin gap-5 justify-end flex-grow self-stretch">
             <NavbarLink href="/" icon={MdKeyboardCommandKey} label="Control" />
+            <NavbarLink href="/tools" icon={FaMicrochip} label="Firmware" />
             <NavbarLink
                 href="/configuration"
                 icon={FaCog}
