fe3b5f55197e5506ffa8e7694833a7bd54de4a05
KGN <kevin.glovernetherton@gmail.com>
2024-10-17 23:31:13 -0400

    Added rapid position functions

--- src/app/src/features/DRO/component/RapidPositionButtons.tsx
+++ src/app/src/features/DRO/component/RapidPositionButtons.tsx
@@ -2,8 +2,33 @@ import blVector from '../assets/bl.svg';
 import brVector from '../assets/br.svg';
 import frVector from '../assets/fr.svg';
 import flVector from '../assets/fl.svg';
+import controller from 'app/lib/controller.ts';
+import { useSelector } from 'react-redux';
+import { RootState } from 'app/store/redux';
+import { getMovementGCode } from 'app/features/DRO/utils/RapidPosition';
+import get from 'lodash/get';
 
 export function RapidPositionButtons() {
+    const homingFlag = useSelector(
+        (state: RootState) => state.controller.homingFlag,
+    );
+    const homingDirection = useSelector((state: RootState) => {
+        return get(state, 'controller.settings.settings.$23', '0');
+    });
+    const pullOff = useSelector((state: RootState) => {
+        return get(state, 'controller.settings.settings.$27', 1);
+    });
+
+    function jogToCorner(corner: string) {
+        const gcode = getMovementGCode(
+            corner,
+            homingDirection,
+            homingFlag,
+            Number(pullOff),
+        );
+        controller.command('gcode', gcode);
+    }
+
     return (
         <div className=" absolute justify-center items-center -top-1 left-1/2 -translate-x-1/2 text-blue-500">
             <div className="grid grid-cols-2 text-3xl gap-2 font-bold">
