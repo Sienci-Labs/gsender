d5ae65765d6a9f4ba10c9954be2ed9b20fc51d3f
kgn <kevin.glovernetherton@gmail.com>
2024-10-18 14:31:04 -0400

    Disabled state to jog

--- src/app/src/features/Jogging/components/TabJog.tsx
+++ src/app/src/features/Jogging/components/TabJog.tsx
@@ -1,9 +1,16 @@
+import cn from "classnames";
+
 export interface TabJogProps {
     topHandlers: object;
     bottomHandlers: object;
+    canClick?: boolean;
 }
 
-const TabJog = (props: TabJogProps) => (
+const TabJog = (props: TabJogProps) => {
+    const standardColourClass = 'hover:fill-blue-600';
+    const disabledColorClass = 'fill-gray-400 hover:fill-gray-400'
+
+    return(
     <svg
         width={45}
         height={168}
@@ -15,18 +22,16 @@ const TabJog = (props: TabJogProps) => (
         <path
             d="M0.5 10C0.5 4.75329 4.75329 0.5 10 0.5H40C45.2467 0.5 49.5 4.7533 49.5 10V88.5H0.5V10Z"
             fill="#3F85C7"
-            stroke="#346DCA"
-            className="hover:fill-blue-600 transition duration-200"
+            className={cn(props.canClick ? standardColourClass : disabledColorClass)}
             {...props.topHandlers}
         />
         <path
             d="M0.5 98.5H49.5V177C49.5 182.247 45.2467 186.5 40 186.5H10C4.75329 186.5 0.5 182.247 0.5 177V98.5Z"
             fill="#3F85C7"
-            stroke="#346DCA"
-            className="hover:fill-blue-600 transition duration-200"
+            className={cn(props.canClick ? standardColourClass : disabledColorClass)}
             {...props.bottomHandlers}
         />
         <line y1={93.5} x2={49} y2={93.5} stroke="#CECECE" />
     </svg>
-);
+)};
 export default TabJog;
