69e9deb0a1ad4871bde6d961b19f8dae80317ffc
walidkayhan <walid.kayhan@hotmail.com>
2024-10-18 14:38:10 -0400

    Make styling consistent between no file loaded and not connected to a device

--- src/app/src/features/Console/index.tsx
+++ src/app/src/features/Console/index.tsx
@@ -18,7 +18,9 @@ const Console = ({ isActive }: Props) => {
                 className={`absolute top-0 left-0 rounded-lg w-full h-full bg-gray-50 z-10 transition-opacity duration-300 ${isConnected ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
             >
                 <div className="flex justify-center items-center h-full">
-                    Not connected to a device
+                    <h2 className="text-lg font-bold">
+                        Not connected to a device
+                    </h2>
                 </div>
             </div>
             <div className="grid grid-rows-[5fr_1fr] absolute gap-2 top-0 left-0 w-full h-full p-1">
