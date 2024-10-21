4c3350230bf460b8290d55f7ccf6666b11fbd80a
KGN <kevin.glovernetherton@gmail.com>
2024-10-21 16:46:56 -0400

    Working with longer compile time smile :)

--- webpack.config.app.development.js
+++ webpack.config.app.development.js
@@ -65,7 +65,6 @@ module.exports = {
                         }
                     }
                 },
-                exclude: /node_modules/
             },
             {
                 test: /\.styl$/,
