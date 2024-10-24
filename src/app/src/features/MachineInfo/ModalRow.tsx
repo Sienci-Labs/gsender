d3efe481028d5106d8a5e7754d501c9a14dc7cc6
Sophia Beluli <sophiabeluli@gmail.com>
2024-10-24 10:56:23 -0400

    machine info modal values aligned on the left

--- src/app/src/features/MachineInfo/ModalRow.tsx
+++ src/app/src/features/MachineInfo/ModalRow.tsx
@@ -10,8 +10,8 @@ const ModalRow: React.FC<Props> = ({ label, value }) => {
     return (
         <div className="relative flex flex-row justify-between w-full items-center leading-7 border-b-gray-300 border-b-2 overflow-visible h-[3px] mt-3 mb-3">
             <div className="text-gray-500 bg-gray-50 pr-2">{label}</div>
-            <div className="bg-gray-50 pl-2">
-                <div>{value}</div>
+            <div className="bg-gray-50 pl-2 min-w-12">
+                <div className="flex float-left">{value}</div>
             </div>
         </div>
     );
