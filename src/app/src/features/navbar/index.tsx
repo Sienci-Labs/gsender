52986d5508b87433ac00996d06943b56cfc8052c
Sophia Beluli <sophiabeluli@gmail.com>
2024-10-30 16:32:12 -0400

    [MI-1584]: implemented hider toggle

--- src/app/src/features/navbar/index.tsx
+++ src/app/src/features/navbar/index.tsx
@@ -2,19 +2,32 @@ import { NavbarLink } from './components/NavbarLink.tsx';
 import { FaMicrochip } from 'react-icons/fa';
 import { FaCog } from 'react-icons/fa';
 import { MdKeyboardCommandKey } from 'react-icons/md';
-import { MinimizeHandle } from './components/MinimizeHandle.tsx';
+import cx from 'classnames';
 
-export function NavBar() {
-    return (
-        <div className="w-20 flex flex-col no-padding no-margin gap-5 justify-end flex-grow self-stretch">
-            <NavbarLink href="/" icon={MdKeyboardCommandKey} label="Control" />
-            <NavbarLink href="/tools" icon={FaMicrochip} label="Firmware" />
-            <NavbarLink
-                href="/configuration"
-                icon={FaCog}
-                label="Preferences"
-            />
-            <MinimizeHandle />
-        </div>
-    );
+interface Props {
+    show: boolean;
 }
+export const NavBar: React.FC<Props> = ({ show }) => {
+    return (
+        <>
+            <div
+                className={cx(
+                    'w-20 flex flex-col no-padding no-margin gap-5 justify-end flex-grow self-stretch',
+                    { hidden: !show },
+                )}
+            >
+                <NavbarLink
+                    href="/"
+                    icon={MdKeyboardCommandKey}
+                    label="Control"
+                />
+                <NavbarLink href="/tools" icon={FaMicrochip} label="Firmware" />
+                <NavbarLink
+                    href="/configuration"
+                    icon={FaCog}
+                    label="Preferences"
+                />
+            </div>
+        </>
+    );
+};
--- src/app/src/workspace/Sidebar/index.tsx
+++ src/app/src/workspace/Sidebar/index.tsx
@@ -1,9 +1,20 @@
 import { NavBar } from 'app/features/navbar';
+import { MinimizeHandle } from 'app/features/navbar/components/MinimizeHandle';
+import { useState } from 'react';
+import cx from 'classnames';
 
 export const Sidebar = () => {
+    const [show, setShow] = useState(true);
     return (
         <div className="border flex flex-col justify-end box-border">
-            <NavBar />
+            <NavBar show={show} />
+            <MinimizeHandle
+                className={cx('z-20', {
+                    'absolute left-0 bottom-5 bg-white border-2 border-l-0 border-blue-500':
+                        !show,
+                })}
+                onClick={() => setShow(!show)}
+            />
         </div>
     );
 };
