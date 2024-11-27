ee68358e537504ffa61630a34506523a6dbc84fb
Walid Kayhan <walid.kayhan@outlook.com>
2024-11-27 11:30:54 -0500

    Use window event for gamepad to listen for connection; UI cleanup for status icons

--- src/app/src/features/StatusIcons/index.jsx
+++ src/app/src/features/StatusIcons/index.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import actions from '../RemoteMode/apiActions';
-import gamepad from 'app/lib/gamepad';
 import cx from 'classnames';
 import { Link } from '@tanstack/react-router';
 import { MdPhonelinkRing } from 'react-icons/md';
 import { LuGamepad2 } from 'react-icons/lu';
 import { FaRegKeyboard } from 'react-icons/fa6';
+
+import gamepad from 'app/lib/gamepad';
+
+import actions from '../RemoteMode/apiActions';
 
 const StatusIcons = () => {
     const [gamepadConnected, setGamePadConnected] = useState(false);
@@ -13,31 +15,60 @@ const StatusIcons = () => {
 
     useEffect(() => {
         actions.fetchSettings(setHeadlessSettings);
-        gamepad.on('gamepad:connected', setGamePadConnected(true));
-        gamepad.on('gamepad:disconnected', setGamePadConnected(false));
+
+        const gameConnectHandler = () => {
+            const gamepads = navigator.getGamepads();
+            const hasGamepad = gamepads.some((gamepad) => gamepad !== null);
+
+            setGamePadConnected(hasGamepad);
+        };
+
+        const gameDisconnectHandler = () => {
+            const gamepads = navigator.getGamepads();
+            const hasGamepad = gamepads.some((gamepad) => gamepad !== null);
+
+            setGamePadConnected(hasGamepad);
+        };
+
+        window.addEventListener('gamepadconnected', gameConnectHandler);
+        window.addEventListener('gamepaddisconnected', gameDisconnectHandler);
 
         return () => {
-            gamepad.removeEventListener(
-                'gamepad:connected',
-                setGamePadConnected(true),
-            );
-            gamepad.removeEventListener(
-                'gamepad:disconnected',
-                setGamePadConnected(false),
+            window.removeEventListener('gamepadconnected', gameConnectHandler);
+            window.removeEventListener(
+                'gamepaddisconnected',
+                gameDisconnectHandler,
             );
         };
+
+        // gamepad.on('gamepad:connected', () => {
+        //     console.log('hi');
+        //     setGamePadConnected(true);
+        // });
+        // gamepad.on('gamepad:disconnected', setGamePadConnected(false));
+
+        // return () => {
+        //     gamepad.removeEventListener(
+        //         'gamepad:connected',
+        //         setGamePadConnected(true),
+        //     );
+        //     gamepad.removeEventListener(
+        //         'gamepad:disconnected',
+        //         setGamePadConnected(false),
+        //     );
+        // };
     }, []);
 
     return (
-        <div className="flex flex-row float: right">
+        <div className="flex flex-row gap-4 absolute top-4 right-4">
             <Link
                 className="flex flex-col gap-0.5  self-center content-center items-center justify-center text-sm text-gray-500"
                 to={'/configuration'}
             >
                 <MdPhonelinkRing
-                    className={cx({
-                        'bg-black': !headlessSettings.headlessStatus,
-                        'bg-green-500': headlessSettings.headlessStatus,
+                    className={cx('w-7 h-7', {
+                        'text-black': !headlessSettings.headlessStatus,
+                        'text-green-500': headlessSettings.headlessStatus,
                     })}
                 />
             </Link>
@@ -45,21 +76,16 @@ const StatusIcons = () => {
                 className="flex flex-col gap-0.5  self-center content-center items-center justify-center text-sm text-gray-500"
                 to={'/configuration'}
             >
-                <FaRegKeyboard
-                    className={cx({
-                        // 'bg-black': !,
-                        'bg-green-500': true,
-                    })}
-                />
+                <FaRegKeyboard className="text-green-500 w-7 h-7" />
             </Link>
             <Link
                 className="flex flex-col gap-0.5  self-center content-center items-center justify-center text-sm text-gray-500"
                 to={'/configuration'}
             >
                 <LuGamepad2
-                    className={cx({
-                        'bg-black': !gamepadConnected,
-                        'bg-green-500': gamepadConnected,
+                    className={cx('w-7 h-7', {
+                        'text-black': !gamepadConnected,
+                        'text-green-500': gamepadConnected,
                     })}
                 />
             </Link>
