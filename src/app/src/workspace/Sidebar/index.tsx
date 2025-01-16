2553dbbbb0b501edce3e09a36dedef1963a3e72f
KGN <kevin.glovernetherton@gmail.com>
2025-01-16 11:28:26 -0500

    Catchup to 1.4.11

--- src/app/src/workspace/Sidebar/index.tsx
+++ src/app/src/workspace/Sidebar/index.tsx
@@ -2,16 +2,22 @@ import { NavBar } from 'app/features/navbar';
 import { MinimizeHandle } from 'app/features/navbar/components/MinimizeHandle';
 import { useState } from 'react';
 import cx from 'classnames';
+import { NavbarLink } from 'app/features/navbar/components/NavbarLink.tsx';
+import { RiSpeakLine } from 'react-icons/ri';
+``;
 
 export const Sidebar = () => {
     const [show, setShow] = useState(true);
     return (
         <div
             className={cx(
-                'border flex flex-col justify-end box-border transition-width ease-out duration-1000 w-10',
+                'border flex flex-col justify-around box-border transition-width ease-out duration-1000 w-10',
                 { 'w-20': show },
             )}
         >
+            <div>
+                <NavbarLink href={''} icon={RiSpeakLine} label="Helper" />
+            </div>
             <NavBar show={show} />
             <MinimizeHandle
                 className={cx('z-20')}
