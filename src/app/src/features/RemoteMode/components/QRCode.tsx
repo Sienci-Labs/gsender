cc5d2d6ac334916aa37a73209582261f3d58da23
KGN <kevin.glovernetherton@gmail.com>
2025-02-12 12:26:08 -0500

    Remote mode layout

---
+++ src/app/src/features/RemoteMode/components/QRCode.tsx
@@ -0,0 +1,17 @@
+import QRCode from 'react-qr-code';
+import React from 'react';
+
+export function QRCodeDisplay({ address = '192.168.0.10:8000' }) {
+    const safeAddress = `http://${address}/remote`;
+    return (
+        <div className="flex flex-col items-center text-sm text-gray-600 gap-4 px-4 justify-center">
+            <h1 className="text-blue-500 text-2xl">Scan QR Code</h1>
+            <p>Scan this with your phone camera to control your CNC:</p>
+            <QRCode size={200} value={safeAddress} viewBox="0 0 200 200" />
+            <p>Or type the text below into a web browser on any device:</p>
+            <div className="px-2 py-1.5 text-xs font-semibold text-blue-500 bg-gray-100 border border-gray-200 rounded-lg">
+                {safeAddress}
+            </div>
+        </div>
+    );
+}
---
+++ src/app/src/features/RemoteMode/index.tsx
@@ -0,0 +1,89 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from 'app/components/shadcn/Dialog.tsx';
+import { QRCodeDisplay } from 'app/features/RemoteMode/components/QRCode.tsx';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from 'app/components/shadcn/Select.tsx';
+import Button from 'app/components/Button';
+import Toggle from 'app/components/Switch/Toggle.tsx';
+import { useState } from 'react';
+
+export function RemoteModeDialog({ showRemote, onClose }) {
+    const [port, setPort] = useState(8000);
+    const [ips, setIps] = useState(['192.168.0.10']);
+    const [ip, setIp] = useState('192.168.0.10');
+
+    return (
+        <Dialog open={showRemote} onOpenChange={onClose}>
+            <DialogContent className="bg-white w-[750px] text-sm">
+                <form>
+                    <DialogHeader>
+                        <DialogTitle></DialogTitle>
+                    </DialogHeader>
+                    <div className="grid grid-cols-2 text-gray-600 grid-">
+                        <div className="flex flex-col gap-8 px-4">
+                            <h1 className="text-2xl text-blue-500">
+                                Enable Wireless CNC Control
+                            </h1>
+                            <div className="flex flex-row gap-4 items-center">
+                                <span className="font-bold">
+                                    Enable Wireless Control
+                                </span>
+                                <Toggle onChange={() => {}} />
+                            </div>
+                            <p>
+                                Choose your settings below. In most cases the
+                                default values should work:
+                            </p>
+                            <div className="flex flex-row w-full justify-between items-center gap-4">
+                                <span>Addr:</span>
+                                <Select>
+                                    <SelectTrigger className="w-2/3 bg-white bg-opacity-100">
+                                        <SelectValue placeholder={ip} />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {ips.map((o) => (
+                                            <SelectItem
+                                                key={`${o}`}
+                                                value={`${o}`}
+                                            >
+                                                {o}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="flex flex-row w-full justify-start items-center gap-4">
+                                <span className="">Port:</span>
+                                <input
+                                    className="border border-gray-200 rounded p-2 focus:outline-none w-full"
+                                    type="number"
+                                    value={port}
+                                />
+                            </div>
+                            <p className={'text-gray-600 text-sm'}>
+                                <b>Note:</b> Clicking "Save" will ask you to
+                                restart gSender so that the settings can be
+                                updated.
+                            </p>
+                            <hr />
+                            <Button color="primary">Save</Button>
+                        </div>
+
+                        <QRCodeDisplay />
+                    </div>
+                    <DialogFooter></DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
