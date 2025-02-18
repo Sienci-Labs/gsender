57bb9e2c9cf760657eeacb8a4edf9cf4a9f1ad90
Walid Kayhan <walid.kayhan@outlook.com>
2025-02-18 09:42:20 -0500

    Add reusable Page component with configurable layout options

---
+++ src/app/src/components/Page/index.tsx
@@ -0,0 +1,50 @@
+import cn from 'classnames';
+import { useRouter } from '@tanstack/react-router';
+import { LuArrowLeft } from 'react-icons/lu';
+
+import { Button } from 'app/components/shadcn/Button';
+
+type PageProps = {
+    children: React.ReactNode;
+    title: string;
+    withBorder?: boolean;
+    withPadding?: boolean;
+    withGoBackButton?: boolean;
+};
+
+const Page = ({
+    children,
+    title,
+    withBorder,
+    withPadding = true,
+    withGoBackButton,
+}: PageProps) => {
+    const navigate = useRouter();
+    return (
+        <div
+            className={cn(
+                withBorder && 'border',
+                withPadding && 'p-4',
+                'w-full',
+            )}
+        >
+            <div className="flex items-center justify-between mb-4 min-h-12">
+                <h1 className="text-3xl font-bold">{title}</h1>
+                {withGoBackButton && (
+                    <Button
+                        variant="outline"
+                        onClick={() => navigate.history.back()}
+                        size="lg"
+                        className="flex items-center justify-center"
+                    >
+                        <LuArrowLeft className="w-6 h-6 mr-1" />
+                        Go Back
+                    </Button>
+                )}
+            </div>
+            {children}
+        </div>
+    );
+};
+
+export default Page;
