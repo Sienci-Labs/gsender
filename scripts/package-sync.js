878e8bccea663f01cba64c38959b401dac9f6243
walidkayhan <walid.kayhan@hotmail.com>
2025-02-19 15:56:58 -0500

    Remove commented-out release notes generation code in package-sync script

--- scripts/package-sync.js
+++ scripts/package-sync.js
@@ -55,35 +55,3 @@ const secondContent = JSON.stringify(pkgApp, null, 2);
 
 fs.writeFileSync(target, content + "\n", "utf8");
 fs.writeFileSync(secondTarget, secondContent + "\n", "utf8");
-
-//Save app versions to releases.json
-/*try {
-  const releaseTarget = path.resolve(
-    __dirname,
-    "../src/app/containers/Preferences/About/releases.json",
-  );
-
-  const getLatestPatchNotes = (data, notesStart) => {
-    const patchNotes = data.split(notesStart)[1];
-    let headerCount = 0;
-
-    return patchNotes
-      .split("\n")
-      .filter((line) => {
-        if (line.includes("###")) {
-          headerCount++;
-        }
-        if (line.length < 2) {
-          return false;
-        }
-        return headerCount < 4;
-      })
-      .map((line) => line.trim());
-  };
-  let readme = fs.readFileSync(path.resolve("README.md"), "utf8");
-  const releases = getLatestPatchNotes(readme, "## 🕣 Development History");
-  fs.writeFileSync(releaseTarget, JSON.stringify(releases));
-} catch (error) {
-  console.log(error.message);
-}
-*/
