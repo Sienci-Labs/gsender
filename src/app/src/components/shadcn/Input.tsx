2f0c009be53f969dfafd9490747104c2f71194e1
walidkayhan <walid.kayhan@hotmail.com>
2025-02-26 10:18:30 -0500

    Simplify Input component to match Shadcn design system

--- src/app/src/components/shadcn/Input.tsx
+++ src/app/src/components/shadcn/Input.tsx
@@ -1,38 +1,19 @@
-// https://ui.shadcn.com/docs/components/input
-
 import * as React from 'react';
 
-import cx from 'classnames';
-import InputGroup from 'react-bootstrap/InputGroup';
+import { cn } from 'app/lib/utils';
 
-export interface InputProps
-    extends React.InputHTMLAttributes<HTMLInputElement> {
-    units?: string;
-    label?: string;
-}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ className, type, label, units, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
+    ({ className, type, ...props }, ref) => {
         return (
-            <div
-                className={cx('grid gap-4', {
-                    'grid-cols-[1fr_2fr] w-full': label != null,
-                })}
-            >
-                {label && <div className="text-lg self-center">{label}</div>}
-                <InputGroup className="flex flex-row flex-nowrap">
-                    <input
-                        type={type}
-                        className={cx(
-                            'flex h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-                            className,
-                        )}
-                        ref={ref}
-                        {...props}
-                    />
-                    {units && <InputGroup.Text>{units}</InputGroup.Text>}
-                </InputGroup>
-            </div>
+            <input
+                type={type}
+                className={cn(
+                    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                    className,
+                )}
+                ref={ref}
+                {...props}
+            />
         );
     },
 );
