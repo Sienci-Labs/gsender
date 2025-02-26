5662086059afe93a30b5780d2f273b88621b99b4
walidkayhan <walid.kayhan@hotmail.com>
2025-02-26 10:18:06 -0500

    Refactor Button component with enhanced variants and Shadcn integration

--- src/app/src/components/Button/index.tsx
+++ src/app/src/components/Button/index.tsx
@@ -1,49 +1,76 @@
 import React from 'react';
 import { tv, type VariantProps } from 'tailwind-variants';
 
+import { Button as ShadcnButton } from 'app/components/shadcn/Button';
+
 export const buttonStyle = tv({
     base: 'border rounded hover:opacity-90 px-3 shadow',
     variants: {
-        color: {
+        variant: {
             primary: 'border-blue-500 text-white bg-blue-500',
             secondary:
                 'border-robin-500 hover:bg-gray-200 text-gray-600 bg-white',
             alt: 'bg-robin-500 text-white border-robin-500',
             disabled: 'border-gray-300 bg-gray-100 text-gray-400',
+            outline:
+                'border-robin-500 hover:bg-gray-200 text-gray-600 bg-white',
+        },
+        size: {
+            sm: 'h-8 text-sm',
+            md: 'h-10 text-base',
+            lg: 'h-12 text-lg',
         },
         disabled: {
             true: 'bg-gray-300 border-gray-400 text-gray-500 hover:bg-gray-300',
         },
     },
-
     defaultVariants: {
-        color: 'secondary',
+        variant: 'secondary',
+        size: 'md',
     },
 });
 
 export type ButtonVariants = VariantProps<typeof buttonStyle>;
 
-export interface ButtonProps extends ButtonVariants {
+export type ButtonProps = ButtonVariants & {
     children?: React.ReactNode;
+    icon?: JSX.Element;
     onClick?: () => void;
     disabled?: boolean;
     className?: string;
-}
+    text?: string;
+};
 
-export function Button(props: ButtonProps) {
+export const Button = (props: ButtonProps) => {
+    const {
+        variant,
+        size,
+        disabled,
+        className,
+        children,
+        onClick,
+        icon,
+        text,
+        ...rest
+    } = props;
+
     return (
-        <button
-            onClick={props.onClick}
-            className={buttonStyle(props)}
-            disabled={props.disabled}
+        <ShadcnButton
+            onClick={onClick}
+            className={buttonStyle({ variant, size, disabled, className })}
+            disabled={disabled}
+            {...rest}
         >
-            {props.children}
-        </button>
+            {children ? (
+                children
+            ) : (
+                <span className="flex items-center gap-1">
+                    {icon}
+                    {text}
+                </span>
+            )}
+        </ShadcnButton>
     );
-}
-
-export function ButtonGroup() {
-    return <div></div>;
-}
+};
 
 export default Button;
