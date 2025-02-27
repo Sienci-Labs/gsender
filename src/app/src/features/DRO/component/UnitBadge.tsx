48eadb4b7a6631569c5c0318f6e0500116a2d594
KGN <kevin.glovernetherton@gmail.com>
2025-02-27 10:53:31 -0500

    Added units indicator to DRO area

--- src/app/src/features/DRO/component/UnitBadge.tsx
+++ src/app/src/features/DRO/component/UnitBadge.tsx
@@ -3,7 +3,7 @@ import store from "app/store";
 export function UnitBadge() {
     const units = store.get('workspace.units', 'mm');
     return (
-        <div className="absolute -top-2 -left-2 px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-300 text-sm rounded-tl rounded-br-lg">
+        <div className="absolute -top-2 -left-2 px-2 py-1.5 text-xs font-semibold text-gray-600 bg-gray-300 text-sm rounded-tl rounded-br-lg">
             <span>Units: {units}</span>
         </div>
     )
