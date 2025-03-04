5472bd36a67d81b069615bc31b67711b0b1dc711
KGN <kevin.glovernetherton@gmail.com>
2025-03-04 10:49:08 -0500

    Home button always shows up, just toggle hidden when single axis homing disabled

--- src/app/src/features/DRO/component/HomingSwitch.tsx
+++ src/app/src/features/DRO/component/HomingSwitch.tsx
@@ -1,17 +1,20 @@
 import Switch from 'app/components/Switch';
 import Button from 'app/components/Button';
 import { homeMachine } from 'app/features/DRO/utils/DRO.ts';
+import cn from 'classnames';
 
 interface HomingSwitchProps {
     onChange: () => void;
     homingValue: boolean;
     disabled: boolean;
+    singleAxisHoming: boolean;
 }
 
 export function HomingSwitch({
     onChange,
     homingValue,
     disabled,
+    singleAxisHoming,
 }: HomingSwitchProps) {
     return (
         <>
@@ -19,6 +22,7 @@ export function HomingSwitch({
                 onChange={onChange}
                 checked={homingValue}
                 disabled={disabled}
+                className={cn({ hidden: !singleAxisHoming })}
             />
             <Button color="alt" onClick={homeMachine} disabled={disabled}>
                 Home
