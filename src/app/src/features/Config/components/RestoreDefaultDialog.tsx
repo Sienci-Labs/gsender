3a54bfbe2930610c14b4ee4b880e7b22fac6c691
KGN <kevin.glovernetherton@gmail.com>
2025-03-14 12:56:12 -0400

    Buttons swapped?

--- src/app/src/features/Config/components/RestoreDefaultDialog.tsx
+++ src/app/src/features/Config/components/RestoreDefaultDialog.tsx
@@ -9,39 +9,66 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
 } from 'app/components/shadcn/AlertDialog.tsx';
-import { FaRedo } from 'react-icons/fa';
-interface RestoreDefaultDialogProps {
-    show: boolean;
-    toggleShow: (b) => void;
+import { ActionButton } from 'app/features/Config/components/ActionButton.tsx';
+import { GrRevert } from 'react-icons/gr';
+import store from 'app/store';
+import { useSelector } from 'react-redux';
+import { RootState } from 'app/store/redux';
+import machineProfiles from 'app/features/Config/assets/MachineDefaults/defaultMachineProfiles.ts';
+
+function getMachineProfile(id) {
+    const profile = machineProfiles.find((profile) => profile.id === id);
+    if (!profile) {
+        return null;
+    }
+    return profile;
 }
 
-export function RestoreDefaultDialog({
-    show,
-    toggleShow,
-}: RestoreDefaultDialogProps) {
+function restoreEEPROMDefaults(type) {
+    let eepromSettings = [];
+
+    const machineProfile = store.get('workspace.machineProfile');
+    const profile = getMachineProfile(machineProfile.id);
+    console.log(profile);
+    if (type === 'grblHAL') {
+        eepromSettings =
+            machineProfile?.grblHALeepromSettings ?? defaultGRBLHALSettings;
+    } else {
+        eepromSettings = machineProfile?.eepromSettings ?? defaultGRBLSettings;
+    }
+
+    const value = [];
+}
+
+export function ReloadFileAlert({ fileLoaded, handleFileReload }) {
+    const isConnected = useSelector(
+        (state: RootState) => state.connection.isConnected,
+    );
+
+    const machineProfile = store.get('workspace.machineProfile', {});
+    console.log(machineProfile);
+    const machineName = `${machineProfile.name} ${machineProfile.type}`;
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <Button
-                    disabled={true}
-                    icon={
-                        <FaRedo className="w-4 h-4 group-hover:text-blue-600 transition-colors" />
-                    }
-                    variant="ghost"
-                    className="h-10 w-12"
+                <ActionButton
+                    icon={<GrRevert />}
+                    label="Defaults"
+                    disabled={!isConnected}
                 />
             </AlertDialogTrigger>
             <AlertDialogContent className="bg-white">
                 <AlertDialogHeader>
-                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogTitle>Restore Defaults</AlertDialogTitle>
                     <AlertDialogDescription>
-                        This will reload the current file from disk.
+                        Are you sure you want to restore your{' '}
+                        <b>{machineName}</b> back to its default state?
                     </AlertDialogDescription>
                 </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction onClick={}>
-                        Reload File
+                <AlertDialogFooter className={'flex flex-col'}>
+                    <AlertDialogCancel>No</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleFileReload}>
+                        Restore Defaults
                     </AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
--- src/app/src/features/FileControl/components/ReloadFileAlert.tsx
+++ src/app/src/features/FileControl/components/ReloadFileAlert.tsx
@@ -9,95 +9,33 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
 } from 'app/components/shadcn/AlertDialog.tsx';
-import { ActionButton } from 'app/features/Config/components/ActionButton.tsx';
-import { GrRevert } from 'react-icons/gr';
-import store from 'app/store';
-import { useSelector } from 'react-redux';
-import { RootState } from 'app/store/redux';
-import machineProfiles from 'app/features/Config/assets/MachineDefaults/defaultMachineProfiles.ts';
-import { toast } from 'app/lib/toaster';
-import controller from 'app/lib/controller.ts';
-
-function getMachineProfile(id) {
-    const profile = machineProfiles.find((profile) => profile.id === id);
-    if (!profile) {
-        return null;
-    }
-    return profile;
-}
-
-function restoreEEPROMDefaults(type = '') {
-    let eepromSettings = [];
-
-    const selectedMachineProfile = store.get('workspace.machineProfile');
-    const profile = getMachineProfile(selectedMachineProfile.id);
-    console.log(profile);
-
-    if (type === 'grblHAL') {
-        eepromSettings = profile?.grblHALeepromSettings;
-    } else {
-        eepromSettings = profile?.eepromSettings;
-    }
-    console.log(eepromSettings);
-
-    const hasOrderedSettings = !!profile.orderedSettings;
-
-    const values = [];
-
-    for (let [key, value] of Object.entries(eepromSettings)) {
-        if (hasOrderedSettings && orderedSettings.has(key)) {
-            continue;
-        }
-
-        values.push(`${key}=${value}`);
-    }
-
-    if (hasOrderedSettings) {
-        for (const [k, v] of profile.orderedSettings) {
-            values.push(`${k}=${v}`);
-        }
-    }
-    values.push('$$');
-
-    controller.command('gcode', values);
-
-    toast.success('Restored default settings for your machine.');
-}
+import { Button } from 'app/components/Button';
+import { FaRedo } from 'react-icons/fa';
 
 export function ReloadFileAlert({ fileLoaded, handleFileReload }) {
-    const isConnected = useSelector(
-        (state: RootState) => state.connection.isConnected,
-    );
-    const controllerType = useSelector(
-        (state: RootState) => state.controller.type,
-    );
-
-    const machineProfile = store.get('workspace.machineProfile', {});
-    console.log(machineProfile);
-    const machineName = `${machineProfile.name} ${machineProfile.type}`;
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <ActionButton
-                    icon={<GrRevert />}
-                    label="Defaults"
-                    disabled={!isConnected}
+                <Button
+                    disabled={!fileLoaded}
+                    icon={
+                        <FaRedo className="w-4 h-4 group-hover:text-blue-600 transition-colors" />
+                    }
+                    variant="ghost"
+                    className="h-10 w-12"
                 />
             </AlertDialogTrigger>
             <AlertDialogContent className="bg-white">
                 <AlertDialogHeader>
-                    <AlertDialogTitle>Restore Defaults</AlertDialogTitle>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
                     <AlertDialogDescription>
-                        Are you sure you want to restore your{' '}
-                        <b>{machineName}</b> back to its default state?
+                        This will reload the current file from disk.
                     </AlertDialogDescription>
                 </AlertDialogHeader>
-                <AlertDialogFooter className={'flex flex-col'}>
-                    <AlertDialogCancel>No</AlertDialogCancel>
-                    <AlertDialogAction
-                        onClick={() => restoreEEPROMDefaults(controllerType)}
-                    >
-                        Restore Defaults
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleFileReload}>
+                        Reload File
                     </AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
