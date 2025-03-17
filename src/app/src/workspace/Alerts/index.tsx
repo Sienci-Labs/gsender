7158de175148a699a0036d2984b8dbd232d2d69a
Sophia Beluli <sophiabeluli@gmail.com>
2025-03-17 16:44:05 -0400

    add job end modal

--- src/app/src/workspace/Alerts/index.tsx
+++ src/app/src/workspace/Alerts/index.tsx
@@ -1,41 +1,64 @@
 import api from 'app/api';
-import { MaintenanceTask } from 'app/features/Stats/utils/StatContext';
+import { Job, MaintenanceTask } from 'app/features/Stats/utils/StatContext';
 import { useEffect, useState } from 'react';
 import MaintenanceAlert from './MaintenanceAlert';
 import pubsub from 'pubsub-js';
+import JobEndModal from './JobEndModal';
 
 export const Alerts = () => {
     const [showMaintenanceAlert, setShowMaintenanceAlert] = useState(false);
+    const [showJobEndModal, setShowJobEndModal] = useState(false);
     const [tasks, setTasks] = useState<MaintenanceTask[]>([]);
+    const [job, setJob] = useState<Job>(null);
+    const [errors, setErrors] = useState([]);
     useEffect(() => {
-        const token = pubsub.subscribe('job:end', () => {
-            api.maintenance.fetch().then((res) => {
-                const taskList: MaintenanceTask[] = res.data;
-                console.log(taskList);
-                let shouldAlert = false;
-                for (const task of taskList) {
-                    const { rangeStart, currentTime } = task;
-                    console.log('start: ' + rangeStart);
-                    console.log('current time: ' + currentTime);
-                    if (currentTime >= rangeStart) {
-                        shouldAlert = true;
-                        break;
+        const tokens = [
+            pubsub.subscribe('job:end', (_, data) => {
+                const { errors } = data;
+                setErrors(errors);
+                api.maintenance.fetch().then((res) => {
+                    const taskList: MaintenanceTask[] = res.data;
+                    console.log(taskList);
+                    let shouldAlert = false;
+                    for (const task of taskList) {
+                        const { rangeStart, currentTime } = task;
+                        console.log('start: ' + rangeStart);
+                        console.log('current time: ' + currentTime);
+                        if (currentTime >= rangeStart) {
+                            shouldAlert = true;
+                            break;
+                        }
                     }
-                }
-                if (shouldAlert) {
-                    console.log('made it');
-                    setTasks(taskList);
-                    setShowMaintenanceAlert(true);
-                }
-            });
-        });
+                    if (shouldAlert) {
+                        console.log('made it');
+                        setTasks(taskList);
+                        setShowMaintenanceAlert(true);
+                    }
+                });
+            }),
+            pubsub.subscribe('lastJob', (_, job) => {
+                setJob(job);
+                setShowJobEndModal(true);
+            }),
+        ];
         return () => {
-            pubsub.unsubscribe(token);
+            tokens.forEach((token) => {
+                pubsub.unsubscribe(token);
+            });
         };
     }, []);
 
     return (
         <>
+            {showJobEndModal && (
+                <JobEndModal
+                    job={job}
+                    errors={errors}
+                    showModal={showJobEndModal}
+                    setShowModal={setShowJobEndModal}
+                    onClose={() => setShowJobEndModal(false)}
+                />
+            )}
             {showMaintenanceAlert && (
                 <MaintenanceAlert
                     tasks={tasks}
