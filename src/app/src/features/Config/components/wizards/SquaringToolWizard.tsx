be37a710ecbbfaa7e3171660ba7b30019a6055b0
KGN <kevin.glovernetherton@gmail.com>
2025-03-28 13:48:58 -0400

    Updates on select and tool links

--- src/app/src/features/Config/components/wizards/SquaringToolWizard.tsx
+++ src/app/src/features/Config/components/wizards/SquaringToolWizard.tsx
@@ -6,7 +6,7 @@ export function ToolLink({ link, label }) {
     return (
         <Link
             to={link}
-            className="text-lg border border-blue-500 shadow hover:bg-gray-100 text-gray-700 p-2 justify-center rounded flex flex-row gap-2 items-center"
+            className="text-lg border border-blue-500 shadow hover:bg-gray-100 text-gray-700 p-2 justify-center rounded flex flex-row gap-2 items-center dark:text-white dark:hover:bg-dark-lighter"
         >
             {label}
             <FaHatWizard />
