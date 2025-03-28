4d0f0fe16565b8a7d0978ea5034d56726f76e08c
Walid Kayhan <walid.kayhan@outlook.com>
2025-03-28 05:15:22 -0400

    Add shuttle control events and keybindings to SpeedSelector component for enhanced speed selection functionality

--- src/app/src/features/Jogging/components/SpeedSelector.tsx
+++ src/app/src/features/Jogging/components/SpeedSelector.tsx
@@ -1,10 +1,13 @@
 import cn from 'classnames';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { JoggingSpeedOptions } from 'app/features/Jogging/utils/Jogging.ts';
 import store from 'app/store';
 import get from 'lodash/get';
 import { JogValueObject } from 'app/features/Jogging';
 import pubsub from 'pubsub-js';
+import { JOGGING_CATEGORY } from 'app/constants';
+import useKeybinding from 'app/lib/useKeybinding';
+import useShuttleEvents from 'app/hooks/useShuttleEvents';
 
 export interface SpeedSelectButtonProps {
     active?: boolean;
@@ -36,14 +39,16 @@ interface SpeedSelectorProps {
 export function SpeedSelector({ handleClick }: SpeedSelectorProps) {
     const [selectedSpeed, setSelectedSpeed] =
         useState<JoggingSpeedOptions>('Normal');
+    const selectedSpeedRef = useRef<JoggingSpeedOptions>(selectedSpeed);
 
     const rapidActive = selectedSpeed === 'Rapid';
     const normalActive = selectedSpeed === 'Normal';
     const preciseActive = selectedSpeed === 'Precise';
 
-    function handleSpeedClick(speed: JoggingSpeedOptions) {
+    function handleSpeedChange(speed: JoggingSpeedOptions) {
         // save old values to config
         setSelectedSpeed(speed);
+        selectedSpeedRef.current = speed;
     }
 
     function updateCurrentJogValues() {
@@ -65,23 +70,79 @@ export function SpeedSelector({ handleClick }: SpeedSelectorProps) {
         };
     }, [selectedSpeed]);
 
+    const shuttleControlEvents = {
+        SET_R_JOG_PRESET: {
+            title: 'Select Rapid Jog Preset',
+            keys: ['shift', 'v'].join('+'),
+            cmd: 'SET_R_JOG_PRESET',
+            preventDefault: false,
+            isActive: true,
+            category: JOGGING_CATEGORY,
+            callback: () => handleSpeedChange('Rapid'),
+        },
+        SET_N_JOG_PRESET: {
+            title: 'Select Normal Jog Preset',
+            keys: ['shift', 'c'].join('+'),
+            cmd: 'SET_N_JOG_PRESET',
+            preventDefault: false,
+            isActive: true,
+            category: JOGGING_CATEGORY,
+            callback: () => handleSpeedChange('Normal'),
+        },
+        SET_P_JOG_PRESET: {
+            title: 'Select Precise Jog Preset',
+            keys: ['shift', 'x'].join('+'),
+            cmd: 'SET_P_JOG_PRESET',
+            preventDefault: false,
+            isActive: true,
+            category: JOGGING_CATEGORY,
+            callback: () => handleSpeedChange('Precise'),
+        },
+        CYCLE_JOG_PRESETS: {
+            title: 'Cycle Through Jog Presets',
+            keys: ['shift', 'z'].join('+'),
+            cmd: 'CYCLE_JOG_PRESETS',
+            preventDefault: false,
+            isActive: true,
+            category: JOGGING_CATEGORY,
+            callback: () => {
+                const jogSpeedOptionsList: JoggingSpeedOptions[] = [
+                    'Rapid',
+                    'Normal',
+                    'Precise',
+                ];
+
+                const currentIndex = jogSpeedOptionsList.findIndex(
+                    (option) => option === selectedSpeedRef.current,
+                );
+                const nextIndex = currentIndex + 1;
+                const nextSpeed = jogSpeedOptionsList[nextIndex];
+
+                handleSpeedChange(nextSpeed ?? jogSpeedOptionsList[0]);
+            },
+        },
+    };
+
+    useKeybinding(shuttleControlEvents);
+    useShuttleEvents(shuttleControlEvents);
+
     return (
         <div className="flex flex-col bg-white rounded-md border-solid border border-gray-300 p-0.5 w-32">
             <SpeedSelectButton
                 active={rapidActive}
-                onClick={() => handleSpeedClick('Rapid')}
+                onClick={() => handleSpeedChange('Rapid')}
                 label="Rapid"
             />
 
             <SpeedSelectButton
                 active={normalActive}
-                onClick={() => handleSpeedClick('Normal')}
+                onClick={() => handleSpeedChange('Normal')}
                 label="Normal"
             />
 
             <SpeedSelectButton
                 active={preciseActive}
-                onClick={() => handleSpeedClick('Precise')}
+                onClick={() => handleSpeedChange('Precise')}
                 label="Precise"
             />
         </div>
