99ab0f2e984660ab5277bbdf35edd04e30e748f1
KGN <kevin.glovernetherton@gmail.com>
2025-04-16 15:31:14 -0400

    Icon opacity

--- src/app/src/components/shadcn/AlertDialog.tsx
+++ src/app/src/components/shadcn/AlertDialog.tsx
@@ -39,7 +39,7 @@ const AlertDialogContent = React.forwardRef<
         <AlertDialogPrimitive.Content
             ref={ref}
             className={cx(
-                'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%]  border bg-white dark:bg-dark p-0 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:text-white dark:border-dark-lighter',
+                'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%]  border bg-white dark:bg-dark-darker p-0 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:text-white dark:border-dark-lighter',
                 className,
             )}
             {...props}
@@ -61,7 +61,7 @@ const AlertDialogHeader = ({
         {...props}
     >
             <div className="text-4xl text-blue-500 flex items-center justify-center border-blue-500 border bg-blue-500 bg-opacity-20 rounded-md p-1 mr-6">
-                <IoIosWarning/>
+                <span className="opacity-60"><IoIosWarning/></span>
             </div>
         <div className="flex flex-col gap-2 p-4">{children}</div>
     </div>
@@ -74,7 +74,7 @@ const AlertDialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
     <div
         className={cx(
-            'flex flex-col-reverse sm:flex-row sm:justify-center sm:space-x-2 bg-gray-200 dark:bg-dark-darker rounded-bl-lg rounded-br-lg border-t border-t-gray-400 p-2',
+            'flex flex-col-reverse sm:flex-row sm:justify-center sm:space-x-2 bg-gray-200 dark:bg-dark rounded-bl-lg rounded-br-lg border-t border-t-gray-400 p-2',
             className,
         )}
         {...props}
