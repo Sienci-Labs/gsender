778557ba4a4257169675d78f1ca61fae43de03e3
KGN <kevin.glovernetherton@gmail.com>
2025-04-24 12:16:13 -0400

    Auto spin now detects firmware

--- src/app/src/features/Config/components/wizards/AutoSpinSetup.tsx
+++ src/app/src/features/Config/components/wizards/AutoSpinSetup.tsx
@@ -5,16 +5,9 @@ import { Button } from 'app/components/Button';
 import autoSpinIcon from '../../assets/images/autospin.svg';
 import { delay } from 'lodash';
 import { Confirm } from 'app/components/ConfirmationDialog/ConfirmationDialogLib.ts';
+import { GRBLHAL } from 'app/constants';
 
 function longmillAutospinSetup() {
-    controller.command('gcode', [
-        'G4P0.1',
-        '$31=1',
-        'G4P0.1',
-        '$30=31250',
-        'G4P0.1',
-        '$$',
-    ]);
     delay(() => {
         console.log('ok');
         Confirm({
@@ -27,45 +20,61 @@ function longmillAutospinSetup() {
     }, 500);
 }
 
-function SLBAutospinSetup() {
-    controller.command('gcode', [
-        '$9 = 1',
-        'G4P0.1',
-        '$16 = 0',
-        'G4P0.1',
-        '$30 = 30000',
-        'G4P0.1',
-        '$31 = 10000',
-        'G4P0.1',
-        '$33 = 1000',
-        'G4P0.1',
-        '$34 = 0',
-        'G4P0.1',
-        '$35 = 30',
-        'G4P0.1',
-        '$36 = 90',
-        'G4P0.1',
-        '$395 = 0',
-        'G4P0.1',
-        ';Flash onboard LED to confirm',
-        'M356 P0 Q2',
-        'M356 P1 Q2',
-        'G4P0.1',
-        'M356 P0 Q1',
-        'M356 P1 Q1',
-        'M356 P0 Q2',
-        'M356 P1 Q2',
-        'G4P0.1',
-        'M356 P0 Q1',
-        'M356 P1 Q1',
-        'M356 P0 Q0',
-        'M356 P1 Q0',
-        '(End of Macro 1)',
-        '(Reset)',
-        'M2',
-        'G4P0.1',
-        '$$',
-    ]);
+function autospinSetup(firmwareType: string = null) {
+    if (!firmwareType) {
+        return console.assert('No firmware type detected, failing early');
+    }
+
+    if (firmwareType === GRBLHAL) {
+        controller.command('gcode', [
+            '$9 = 1',
+            'G4P0.1',
+            '$16 = 0',
+            'G4P0.1',
+            '$30 = 30000',
+            'G4P0.1',
+            '$31 = 10000',
+            'G4P0.1',
+            '$33 = 1000',
+            'G4P0.1',
+            '$34 = 0',
+            'G4P0.1',
+            '$35 = 30',
+            'G4P0.1',
+            '$36 = 90',
+            'G4P0.1',
+            '$395 = 0',
+            'G4P0.1',
+            ';Flash onboard LED to confirm',
+            'M356 P0 Q2',
+            'M356 P1 Q2',
+            'G4P0.1',
+            'M356 P0 Q1',
+            'M356 P1 Q1',
+            'M356 P0 Q2',
+            'M356 P1 Q2',
+            'G4P0.1',
+            'M356 P0 Q1',
+            'M356 P1 Q1',
+            'M356 P0 Q0',
+            'M356 P1 Q0',
+            '(End of Macro 1)',
+            '(Reset)',
+            'M2',
+            'G4P0.1',
+            '$$',
+        ]);
+    } else {
+        controller.command('gcode', [
+            'G4P0.1',
+            '$31=1',
+            'G4P0.1',
+            '$30=31250',
+            'G4P0.1',
+            '$$',
+        ]);
+    }
+
     delay(() => {
         Confirm({
             title: 'Restart your Controller',
@@ -91,20 +100,19 @@ export function AutoSpinSetup() {
     const connected = useSelector(
         (state: RootState) => state.connection.isConnected,
     );
+    const firmwareType = useSelector(
+        (state: RootState) => state.controller.type,
+    );
 
     return (
         <div className="flex flex-col gap-4">
             <Button
-                onClick={SLBAutospinSetup}
-                className={'flex flex-row gap-2 justify-start'}
+                onClick={() => autospinSetup(firmwareType)}
+                className={'flex flex-row justify-start'}
                 disabled={!connected}
             >
                 <AutoSpinIcon />
-                SLB Setup
-            </Button>
-            <Button disabled={!connected} onClick={longmillAutospinSetup}>
-                <AutoSpinIcon />
-                Longboard Setup
+                Setup AutoSpin
             </Button>
         </div>
     );
