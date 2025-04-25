65859ae82884a6f6b510434d3f558a11bf7fee2f
walidkayhan <walid.kayhan@hotmail.com>
2025-04-25 09:56:53 -0400

    Refactor ToolArea layout to enhance portrait orientation support with improved element ordering

--- src/app/src/workspace/ToolArea/index.tsx
+++ src/app/src/workspace/ToolArea/index.tsx
@@ -4,14 +4,14 @@ import Tools from 'app/features/Tools';
 
 export const ToolArea = () => {
     return (
-        <div className="grid grid-cols-3 gap-2 box-border w-full">
-            <div className="w-full p-1 box-border">
+        <div className="grid grid-cols-3 portrait:grid-cols-1 gap-2 box-border w-full h-full portrait:gap-6 portrait:grid-rows-3">
+            <div className="w-full p-1 box-border order-1 portrait:order-2">
                 <FileControl />
             </div>
-            <div className="w-full p-1 box-border">
+            <div className="w-full p-1 box-border order-2 portrait:order-1">
                 <JobControl />
             </div>
-            <div className="w-full p-1 box-border">
+            <div className="w-full p-1 box-border order-3 portrait:order-3">
                 <Tools />
             </div>
         </div>
