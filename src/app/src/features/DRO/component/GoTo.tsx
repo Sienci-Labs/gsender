b4aedc768652435cd9c0c62cb42c169642ae5691
walidkayhan <walid.kayhan@hotmail.com>
2025-04-30 11:08:16 -0400

    Refactor GoTo component to use default Switch import and update toggle logic

--- src/app/src/features/DRO/component/GoTo.tsx
+++ src/app/src/features/DRO/component/GoTo.tsx
@@ -7,7 +7,7 @@ import { FaPaperPlane } from 'react-icons/fa6';
 import { Button } from 'app/components/Button';
 import { UnitInput } from 'app/components/UnitInput';
 import { DROPosition } from 'app/features/DRO/utils/DRO.ts';
-import { Switch } from 'app/components/shadcn/Switch.tsx';
+import Switch from 'app/components/Switch';
 import { useEffect, useState } from 'react';
 import controller from 'app/lib/controller';
 
@@ -50,7 +50,7 @@ export function GoTo({ units, wpos, disabled }: GotoProps) {
     }, [relativeMovement]);
 
     const onToggleSwap = () => {
-        setRelativeMovement(!relativeMovement);
+        setRelativeMovement((prev) => !prev);
     };
 
     function goToLocation() {
@@ -111,7 +111,10 @@ export function GoTo({ units, wpos, disabled }: GotoProps) {
                     />
                     <div className="flex flex-row text-sm text-gray-400 justify-between">
                         <span>ABS</span>
-                        <Switch onClick={onToggleSwap} />
+                        <Switch
+                            checked={relativeMovement}
+                            onChange={onToggleSwap}
+                        />
                         <span>INC</span>
                     </div>
                     <Button variant="primary" onClick={goToLocation}>
