e3f7c73f828e61b8ed1c1fa588efd444c43f68bf
walidkayhan <walid.kayhan@hotmail.com>
2025-05-13 15:28:54 -0400

    Refactor TabsTrigger component to simplify usage in TabList by removing redundant className prop while ensuring active state styling remains consistent

--- src/app/src/components/shadcn/Tabs.tsx
+++ src/app/src/components/shadcn/Tabs.tsx
@@ -31,7 +31,7 @@ const TabsTrigger = React.forwardRef<
     <TabsPrimitive.Trigger
         ref={ref}
         className={cx(
-            'items-center justify-center whitespace-nowrap rounded px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-gray-300 data-[state=active]:text-foreground data-[state=active]:shadow-sm dark:data-[state=active]:bg-dark',
+            'items-center justify-center whitespace-nowrap rounded px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-foreground data-[state=active]:shadow-sm dark:data-[state=active]:bg-dark',
             className,
         )}
         {...props}
