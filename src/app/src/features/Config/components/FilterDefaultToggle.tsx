fa8fc6c780ad9edb5097fa96606dd43b371b95aa
kgn <kevin.glovernetherton@gmail.com>
2025-06-05 13:43:31 -0400

    View to show terminology

--- src/app/src/features/Config/components/FilterDefaultToggle.tsx
+++ src/app/src/features/Config/components/FilterDefaultToggle.tsx
@@ -6,7 +6,7 @@ export function FilterDefaultToggle() {
 
     return (
         <div className="ml-8 flex flex-row gap-4 items-center max-xl:text-xs flex-grow">
-            <p className="text-gray-500 max-xl:hidden">Show Modified</p>
+            <p className="text-gray-500 max-xl:hidden">View Modified</p>
             <p className="text-gray-500 hidden max-xl:block">Modified</p>
             <Switch
                 onChange={toggleFilterNonDefault}
