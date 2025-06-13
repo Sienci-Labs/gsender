7921776930dee75f9eaf31c3099cf63cc0f640ae
walidkayhan <walid.kayhan@hotmail.com>
2025-06-13 10:02:21 -0400

    Refactor RunProbe component to use useEffect for setting probe connectivity based on connectivityTest and probePinStatus

--- src/app/src/features/Probe/RunProbe.tsx
+++ src/app/src/features/Probe/RunProbe.tsx
@@ -60,11 +60,13 @@ const RunProbe = ({ actions, state }: RunProbeProps) => {
         probePinStatus: state.controller.state.status?.pinState.P ?? false,
     }));
 
-    if (!connectivityTest) {
-        actions.setProbeConnectivity(true);
-    } else if (probePinStatus) {
-        actions.setProbeConnectivity(true);
-    }
+    useEffect(() => {
+        if (!connectivityTest) {
+            actions.setProbeConnectivity(true);
+        } else if (probePinStatus) {
+            actions.setProbeConnectivity(true);
+        }
+    }, [connectivityTest, probePinStatus, actions]);
 
     const [testInterval, setTestInterval] = useState<NodeJS.Timeout>(null);
 
