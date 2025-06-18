e25f1d4d6a7d714db8ef0893e9fd85950c2ef8f5
KGN <kevin.glovernetherton@gmail.com>
2025-06-18 10:35:44 -0400

    Update indentation

--- src/server/api/api.releasenotes.js
+++ src/server/api/api.releasenotes.js
@@ -25,25 +25,25 @@
 // import path from 'path';
 
 const releases = [
-  {
-    'version': '1.5.0',
-    'date': 'June 18th, 2025',
-    'notes': [
-      'All new user experience - we\'ve streamlined and modernized the UI, with a focus on touch device and usability.  It should seem familiar to previous gSender users with a number of new improvements.',
-      'Firmware now detected on connection - no more firmware selector, gSender can just use the correct controller type.',
-      'All new Stats tool which collates your job run statistics, alarms and errors, maintenance tasks, and diagnostics.',
-      'Firmware settings and gSender settings have been combined into a new streamlined Config tool, allowing you to easily configure your machine setup and application behaviour.',
-      'All settings (both EEPROM and application) can be filtered by non-default and restored to default values at a single click.',
-      'Rotary now a first-class citizen - enabling rotary functionality adds all DRO and jogging controls you could need to the main UI.',
-      'Some new perspectives - gSender now comes with a configurable dark mode, selectable in config.  As well, portrait mode is available by rotating your device.',
-      'Updated remote mode - more functionality at your finger tips',
-      'What\'d I miss - all new notifications center to keep you informed about what\'s happened when running your job.',
-      'Helper - Alarm explanations and toolchanging are now helpers, which will pop up as required.  ',
-      'Visualizer - Lightweight mode has had some behaviour changes, and better supports touch movements like pinch and zoom.',
-      'Tweak to 30X30 machine profile for missing acceleration change for $111.',
-      'Tools - All tools and widgets are collated on the new Tools interface, allowing you to easily access tools and widgets'
-    ]
-  },
+    {
+        'version': '1.5.0',
+        'date': 'June 18th, 2025',
+        'notes': [
+            'All new user experience - we\'ve streamlined and modernized the UI, with a focus on touch device and usability.  It should seem familiar to previous gSender users with a number of new improvements.',
+            'Firmware now detected on connection - no more firmware selector, gSender can just use the correct controller type.',
+            'All new Stats tool which collates your job run statistics, alarms and errors, maintenance tasks, and diagnostics.',
+            'Firmware settings and gSender settings have been combined into a new streamlined Config tool, allowing you to easily configure your machine setup and application behaviour.',
+            'All settings (both EEPROM and application) can be filtered by non-default and restored to default values at a single click.',
+            'Rotary now a first-class citizen - enabling rotary functionality adds all DRO and jogging controls you could need to the main UI.',
+            'Some new perspectives - gSender now comes with a configurable dark mode, selectable in config.  As well, portrait mode is available by rotating your device.',
+            'Updated remote mode - more functionality at your finger tips',
+            'What\'d I miss - all new notifications center to keep you informed about what\'s happened when running your job.',
+            'Helper - Alarm explanations and toolchanging are now helpers, which will pop up as required.  ',
+            'Visualizer - Lightweight mode has had some behaviour changes, and better supports touch movements like pinch and zoom.',
+            'Tweak to 30X30 machine profile for missing acceleration change for $111.',
+            'Tools - All tools and widgets are collated on the new Tools interface, allowing you to easily access tools and widgets'
+        ]
+    },
     {
         'version': '1.4.11',
         'date': 'December 16th, 2024',
