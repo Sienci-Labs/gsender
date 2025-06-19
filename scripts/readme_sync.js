d0df408b8fe227d20b92afa5d3b28b9af844bd09
KGN <kevin.glovernetherton@gmail.com>
2025-06-19 12:50:35 -0400

    Fix for dropdown on remote mode

--- scripts/readme_sync.js
+++ scripts/readme_sync.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 export function parseLatestReadmeNotes() {
-  let readme = fs.readFileSync("README.md", "utf8");
+    let readme = fs.readFileSync('README.md', 'utf8');
 
-  let notes = readme.split("## 🕣 Development History")[1];
+    let notes = readme.split('## 🕣 Development History')[1];
 }
