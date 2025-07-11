d5b2ad8c77de3b9d7e2ce5b647a49f745e914ab2
KGN <kevin.glovernetherton@gmail.com>
2025-07-11 15:13:35 -0400

    Fix for polyfills issue

--- src/app/src/sentry-config.ts
+++ src/app/src/sentry-config.ts
@@ -1,12 +1,19 @@
 import * as Sentry from '@sentry/react';
 import process from 'process';
-
 import pkg from '../package.json';
 
-if (process.env.NODE_ENV === 'production') {
+const env = process.env
+if (env.NODE_ENV === 'production') {
     Sentry.init({
         dsn: 'https://eeb4899f0415aa6bc9de477a7faeb720@o558751.ingest.us.sentry.io/4509479105986560',
         sendDefaultPii: true,
         release: pkg.version,
     });
 }
+
+
+/*Sentry.init({
+    dsn: env.SENTRY_DSN,
+    sendDefaultPii: true,
+    release: pkg.version,
+});*/
