aa7a27addbb0fc2362e43b669b549893b7b58b87
Walid Kayhan <walid.kayhan@outlook.com>
2025-07-16 17:05:05 -0400

    Add SDCard component and utility functions for mounting and file retrieval

--- src/app/src/features/Tools/index.tsx
+++ src/app/src/features/Tools/index.tsx
@@ -6,6 +6,7 @@ import Spindle from '../Spindle';
 import Coolant from '../Coolant';
 import Rotary from '../Rotary';
 import Macros from '../Macros';
+import SDCard from '../SDCard';
 import { useWidgetState } from 'app/hooks/useWidgetState';
 import { useWorkspaceState } from 'app/hooks/useWorkspaceState';
 import { ATC } from 'app/features/ATC';
@@ -43,6 +44,10 @@ const tabs = [
     {
         label: 'Console',
         content: Console,
+    },
+    {
+        label: 'SDCard',
+        content: SDCard,
     },
 ];
 
