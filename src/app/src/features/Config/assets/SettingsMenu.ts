d6ecac950240160e1c470bd38f7f69a7b2398ab4
Sienci Labs <chris_thorogood@sienci.com>
2025-07-22 17:28:17 -0400

    Clarification on safe height setting description in Config

--- src/app/src/features/Config/assets/SettingsMenu.ts
+++ src/app/src/features/Config/assets/SettingsMenu.ts
@@ -176,7 +176,7 @@ export const SettingsMenu: SettingsMenuSection[] = [
                         type: 'number',
                         unit: 'variable',
                         description:
-                            "Amount Z-axis will move up before moving in X/Y/A using go tos, corner-movements, or parking. (Doesn't apply to files, if homing is enabled this value becomes an offset from the top of the Z-axis, Default 0)",
+                            "Amount Z-axis will move up before moving in X/Y/A using go tos. (Doesn't apply to files, corner-movements, or parking, if homing is enabled this value becomes an offset from the top of the Z-axis, Default 0)",
                     },
                     {
                         label: 'Send usage data',
