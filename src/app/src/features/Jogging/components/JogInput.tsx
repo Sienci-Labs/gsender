87da09cba3831d8f0a1b34682e4ee5014393798f
Sophia Beluli <sophiabeluli@gmail.com>
2025-07-25 14:16:48 -0400

    [MI-1955]: reworked step calculations to remove unnecessary number of if else statements

--- src/app/src/features/Jogging/components/JogInput.tsx
+++ src/app/src/features/Jogging/components/JogInput.tsx
@@ -13,46 +13,37 @@ interface JogInputProps {
 export const JogInput = ({ label, currentValue, onChange }: JogInputProps) => {
     const getStep = (increment = false) => {
         let step;
+        const digitCount = Math.floor(currentValue).toString().length; // number of whole digits
+        const split = currentValue.toString().split('.');
+        const digitCountDecimal = split[1] ? split[1].length : 0; // number of digits after the decimal point
+        const x = Number('1'.padEnd(digitCount, '0')); // ex. currentValue = 234, x = 100
+        const y = Number('1'.padEnd(digitCount - 1, '0')); // ex. currentValue = 234, y = 10
+        const xD = Number('0.' + '1'.padStart(digitCountDecimal, '0')); // ex. currentValue = 0.02, x = 0.01
+        const yD = Number('0.' + '1'.padStart(digitCountDecimal + 1, '0')); // ex. currentValue = 0.02, x = 0.001
 
         if (currentValue === 0) {
             if (!increment) {
-                return 0;
+                return 0; // don't decrease less than 0
             }
-            return 0.1;
-        }
-        if (currentValue < 0.01) {
-            step = 0.001;
-        } else if (currentValue < 0.1) {
-            step = 0.01;
-        } else if (currentValue < 1) {
-            step = 0.1;
-        } else if (currentValue < 10 || (!increment && currentValue - 10 < 1)) {
-            step = 1;
-        } else if (
-            currentValue < 100 ||
-            (!increment && currentValue - 100 < 100)
-        ) {
-            step = 10;
-        } else if (
-            currentValue < 1000 ||
-            (!increment && currentValue - 1000 < 1000)
-        ) {
-            step = 100;
-        } else if (
-            currentValue < 10000 ||
-            (!increment && currentValue - 10000 < 10000)
-        ) {
-            step = 1000;
+            return 0.1; // add 0.1
+        } else if (currentValue < 1 || (!increment && currentValue === 1)) {
+            if (!increment && currentValue - xD < xD) {
+                // ex. 0.01 - 0.01 < 0.01
+                step = yD; // ex. currentValue = 0.01, step = 0.001
+            } else {
+                step = xD; // ex. currentValue = 0.02, step = 0.01
+            }
         } else {
-            step = 10000;
+            if (!increment && currentValue - x < x) {
+                // ex. 110 - 100 < 100
+                step = y; // ex. currentValue = 110, step = 10
+            } else {
+                step = x; // ex. currentValue = 210, step = 100
+            }
         }
 
-        if (!increment && currentValue - step <= 0) {
-            if (step > 0.001) {
-                step /= 10;
-            } else {
-                return 0;
-            }
+        if (step < 0.001) {
+            return 0; // make sure it can't go lower than 0.001
         }
         return step;
     };
@@ -64,7 +55,6 @@ export const JogInput = ({ label, currentValue, onChange }: JogInputProps) => {
         // so we need to round to 4 decimal places first to get rid of any infinite decimals.
         // I chose 4 because it's 1 more than our max decimal places, so it shouldn't affect the number we want to display.
         newValue = Number(newValue.toFixed(4));
-        console.log(newValue);
         if (newValue < 1) {
             return toFixedIfNecessary(newValue, 3); // round to max 3 decimal places
         } else if (newValue < 10) {
@@ -76,7 +66,7 @@ export const JogInput = ({ label, currentValue, onChange }: JogInputProps) => {
             const higher = Number('2'.padEnd(digitCount, '0')); // 20, 200, 2000, etc
 
             if (newValue >= lower && newValue < higher) {
-                // ex. >= 10 && < 20
+                // ex. >= 100 && < 200
                 return increment
                     ? Math.floor(newValue / x) * x // ex. 115->120
                     : Math.ceil(newValue / x) * x; // ex. 115->110
