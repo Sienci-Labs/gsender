e5dd31d288af26df6acf4066457161a51b707b29
KGN <kevin.glovernetherton@gmail.com>
2025-07-28 21:42:10 -0400

    Added fallback to probeTool call

--- src/app/src/features/ATC/components/CurrentToolInfo.tsx
+++ src/app/src/features/ATC/components/CurrentToolInfo.tsx
@@ -4,6 +4,8 @@ import { RootState } from 'app/store/redux';
 import { useEffect, useState } from 'react';
 import { lookupSpecificTool } from 'app/features/ATC/utils/ATCFunctions.ts';
 import {ToolNameInput} from "app/features/ATC/components/ToolNameInput.tsx";
+import { Button } from 'app/components/Button';
+import controller from "app/lib/controller.ts";
 
 interface ToolInfoWidgetProps {
     toolNumber: number;
@@ -21,7 +23,7 @@ export function CurrentToolInfo({
     onLoadTool,
 }) {
     const [selectedTool, setSelectedTool] = useState<ToolInstance>({
-        id: -1,
+        id: 1,
         nickname: '-',
         toolOffsets: {
             x: 0,
@@ -33,10 +35,28 @@ export function CurrentToolInfo({
     const currentTool = useTypedSelector(
         (state: RootState) => state.controller.state.status?.currentTool,
     );
+
     const toolTable = useTypedSelector(
         (state: RootState) => state.controller.settings.toolTable,
     );
 
+    useEffect(() => {
+        if (currentTool) {
+            let populatedTool = lookupSpecificTool(currentTool, toolTable);
+            console.log(currentTool);
+            if (populatedTool) {
+                console.log(populatedTool);
+                setSelectedTool(populatedTool);
+            }
+        }
+    }, []);
+
+    const probeTool = (id) => {
+        if (id < 1) {
+            return;
+        }
+        controller.command('gcode', [`G65P301Q${id}`,'$#']);
+    }
 
     useEffect(() => {
         if (currentTool) {
@@ -84,21 +104,23 @@ export function CurrentToolInfo({
                     <span className="text-sm font-medium text-gray-700 w-20">
                         Tool Offset:
                     </span>
-                    <div className="flex-1 border-2 border-robin-300 rounded-lg px-3 py-2 bg-robin-100">
+                    <div className="flex-1 flex-row flex gap-7">
                         <div className="flex items-center gap-3">
-                            <span className="text-robin-900 font-medium">
-                                {zOffset.toFixed(3)} mm
+                            <span className="text-robin-900 font-medium border-2 rounded-lg px-3 py-2 font-mono border-robin-300 bg-robin-100">
+                                {zOffset.toFixed(3)}
                             </span>
-                            <button
-                                className={`ml-auto px-3 py-1 rounded text-sm font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 ${
-                                    status === 'Probed'
-                                        ? 'bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500'
-                                        : 'bg-orange-500 hover:bg-orange-600 text-white focus:ring-orange-500'
-                                }`}
-                            >
-                                Probe
-                            </button>
                         </div>
+                        <Button
+                            className={`ml-auto px-3 py-1 rounded text-sm font-medium transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+                                status === 'Probed'
+                                    ? 'bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500'
+                                    : 'bg-orange-500 hover:bg-orange-600 text-white focus:ring-orange-500'
+                            }`}
+                            disabled={currentTool < 1}
+                            onClick={() => probeTool(currentTool)}
+                        >
+                            Probe
+                        </Button>
                     </div>
                 </div>
             </div>
