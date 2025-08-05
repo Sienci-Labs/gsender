e245d4f8fdeb1cc4571acddc593a1bdadeb8cf31
KGN <kevin.glovernetherton@gmail.com>
2025-08-05 16:25:36 -0400

    Add context, now to hook it into all components smile

--- src/app/src/features/ATC/index.tsx
+++ src/app/src/features/ATC/index.tsx
@@ -12,6 +12,7 @@ import {
 } from 'app/features/ATC/utils/ATCFunctions.ts';
 import { ToolInstance } from 'app/features/ATC/components/ToolTable.tsx';
 import { AdvancedOptions } from 'app/features/ATC/components/AdvancedOptions.tsx';
+import { ToolchangeProvider } from 'app/features/ATC/utils/ToolChangeContext.tsx';
 
 export function ATC() {
     const [showToolTable, setShowToolTable] = useState(false);
@@ -46,27 +47,29 @@ export function ATC() {
     }
 
     return (
-        <div className="flex flex-col  w-full gap-2 relative">
-            <div className="flex flex-col gap-2 w-36 justify-end absolute top-0 right-16">
-                <ToolDisplayModal
-                    showToolTable={showToolTable}
-                    onOpenChange={toggleToolTable}
+        <ToolchangeProvider>
+            <div className="flex flex-col  w-full gap-2 relative">
+                <div className="flex flex-col gap-2 w-36 justify-end absolute top-0 right-16">
+                    <ToolDisplayModal
+                        showToolTable={showToolTable}
+                        onOpenChange={toggleToolTable}
+                        tools={tools}
+                        disabled={disabledButton}
+                    />
+                    <AdvancedOptions
+                        disabled={disabledButton}
+                        setLoadToolMode={setLoadToolMode}
+                    />
+                </div>
+
+                <ToolDisplay
                     tools={tools}
                     disabled={disabledButton}
-                />
-                <AdvancedOptions
-                    disabled={disabledButton}
-                    setLoadToolMode={setLoadToolMode}
+                    loadToolMode={loadToolMode}
+                    loadToolPopoverOpen={loadToolPopoverOpen}
+                    setLoadToolPopoverOpen={setLoadToolPopoverOpen}
                 />
             </div>
-
-            <ToolDisplay
-                tools={tools}
-                disabled={disabledButton}
-                loadToolMode={loadToolMode}
-                loadToolPopoverOpen={loadToolPopoverOpen}
-                setLoadToolPopoverOpen={setLoadToolPopoverOpen}
-            />
-        </div>
+        </ToolchangeProvider>
     );
 }
--- src/app/src/features/ATC/utils/ToolChangeContext.tsx
+++ src/app/src/features/ATC/utils/ToolChangeContext.tsx
@@ -1,9 +1,66 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import {
+    LoadToolMode,
+    mapToolNicknamesAndStatus,
+} from 'app/features/ATC/utils/ATCFunctions.ts';
+import { ToolInstance } from 'app/features/ATC/components/ToolTable.tsx';
+import { useTypedSelector } from 'app/hooks/useTypedSelector.ts';
+import { RootState } from 'app/store/redux';
 
-const ToolChangeContext = createContext({});
+const ToolChangeContext = createContext<IToolChangeContext>({
+    mode: 'load',
+    loadToolOpen: false,
+    setLoadToolOpen: () => {},
+    setLoadToolMode: () => {},
+    disabled: false,
+    connected: false,
+    tools: [],
+    showTable: false,
+    setShowTable: () => {},
+});
 
-const ToolchangeProvider = ({ children }) => {
-    const payload = {};
+export interface IToolChangeContext {
+    mode: LoadToolMode;
+    setLoadToolMode: (mode: LoadToolMode) => void;
+    loadToolOpen: boolean;
+    setLoadToolOpen: (open: boolean) => void;
+    disabled: boolean;
+    connected: boolean;
+    tools: ToolInstance[];
+    showTable: boolean;
+    setShowTable: (show: boolean) => void;
+}
+
+export const ToolchangeProvider = ({ children }) => {
+    const [loadToolMode, setLoadToolMode] = useState<LoadToolMode>('load');
+    const [toolPopoutOpen, setToolPopoutOpen] = useState<boolean>(false);
+    const [tools, setTools] = useState<ToolInstance[]>([]);
+    const [showTable, setShowTable] = useState<boolean>(false);
+
+    const toolTableData = useTypedSelector(
+        (state: RootState) => state.controller.settings.toolTable,
+    );
+
+    const isConnected = useTypedSelector(
+        (state: RootState) => state.connection.isConnected,
+    );
+
+    useEffect(() => {
+        setTools(mapToolNicknamesAndStatus(toolTableData));
+    }, [toolTableData]);
+
+    const payload = {
+        mode: loadToolMode,
+        loadToolOpen: toolPopoutOpen,
+        setLoadToolOpen: setToolPopoutOpen,
+        setLoadToolMode: setLoadToolMode,
+        disabled: !isConnected,
+        connected: isConnected,
+        tools: tools,
+        showTable: showTable,
+        setShowTable: setShowTable,
+    };
+
     return (
         <ToolChangeContext.Provider value={payload}>
             {children}
