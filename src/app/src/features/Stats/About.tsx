be8aae3fa27024773544b28b85917483872a2353
Sienci Labs <chris_thorogood@sienci.com>
2025-08-22 21:36:07 -0400

    Update About.tsx width compensation in About

--- src/app/src/features/Stats/About.tsx
+++ src/app/src/features/Stats/About.tsx
@@ -126,7 +126,7 @@ const About = () => {
     };
 
     return (
-        <div className="w-full flex flex-col gap-6 h-full">
+        <div className="w-full flex flex-col gap-6 h-full px-4">
             <div className="flex items-center justify-between gap-4">
                 <div className="flex items-center gap-2">
                     <img
