7d5fa6b595b80c38e2971ded26ce81c20b9dc359
Sienci Labs <chris_thorogood@sienci.com>
2025-08-22 21:17:01 -0400

    Update IP.tsx trying to get IP to fit Config better

--- src/app/src/features/Config/components/SettingInputs/IP.tsx
+++ src/app/src/features/Config/components/SettingInputs/IP.tsx
@@ -17,7 +17,7 @@ export function IPSettingInput({ ip = [], onChange }: IPSettingInputsProps) {
         <div className="flex flex-row gap-2 items-end">
             <ControlledInput
                 type="number"
-                className="w-[6ch] p-2 text-center"
+                className="w-[4ch] p-2 text-center"
                 value={ip[0]}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     updateIPPortion(0, e.target.value)
@@ -26,7 +26,7 @@ export function IPSettingInput({ ip = [], onChange }: IPSettingInputsProps) {
             .
             <ControlledInput
                 type="number"
-                className="w-[6ch] p-2 text-center"
+                className="w-[4ch] p-2 text-center"
                 value={ip[1]}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     updateIPPortion(1, e.target.value)
@@ -35,7 +35,7 @@ export function IPSettingInput({ ip = [], onChange }: IPSettingInputsProps) {
             .
             <ControlledInput
                 type="number"
-                className="w-[6ch] p-2 text-center"
+                className="w-[4ch] p-2 text-center"
                 value={ip[2]}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     updateIPPortion(2, e.target.value)
@@ -44,7 +44,7 @@ export function IPSettingInput({ ip = [], onChange }: IPSettingInputsProps) {
             .
             <ControlledInput
                 type="number"
-                className="w-[6ch] p-2 text-center"
+                className="w-[4ch] p-2 text-center"
                 value={ip[3]}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     updateIPPortion(3, e.target.value)
