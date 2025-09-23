6c792a1ef0f0cb90396933c63d7fb3b14a74eec5
KGN <kevin.glovernetherton@gmail.com>
2025-09-22 21:40:44 -0400

    Add variables and fake default values

--- src/app/src/features/ATC/assets/defaultATCIMacros.ts
+++ src/app/src/features/ATC/assets/defaultATCIMacros.ts
@@ -1,5 +1,21 @@
-const macro;
+export type Macro = {
+    name: string;
+    content: string;
+};
+
+const defaultMacros: Macro[] = [];
 
 export const defaultATCIMacros = {
     version: 20250909,
+    variables: {
+        _ort_offset_mode: {},
+        _irt_offset_mode: {},
+        _tc_rack_enable: {},
+        _tc_slots: {},
+        _passthrough_offset_setting: {},
+        _pres_sense: {},
+        _holder_sense: {},
+    },
+    variableFile: 'P100.macro',
+    macros: defaultMacros,
 };
