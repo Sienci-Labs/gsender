037a0ce58ad753a9e0aa3333dda5031e3bad2640
Sophia Beluli <sophiabeluli@gmail.com>
2025-10-20 10:50:10 -0400

    add navigation blocker to config to prompt to save changes

--- src/app/src/features/navbar/index.tsx
+++ src/app/src/features/navbar/index.tsx
@@ -5,8 +5,45 @@ import cx from 'classnames';
 
 import Carve from './assets/Carve.svg';
 import { NavbarLink } from './components/NavbarLink.tsx';
+import { useLocation, useNavigate } from 'react-router';
+import { useSettings } from '../Config/utils/SettingsContext.tsx';
+import Blocker from './components/Blocker.tsx';
+import { Confirm } from 'app/components/ConfirmationDialog/ConfirmationDialogLib.ts';
 
 export const NavBar = () => {
+    const { settingsAreDirty } = useSettings();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const blocker = new Blocker();
+    console.log(settingsAreDirty);
+
+    const proceed = () => {
+        blocker.proceed();
+    };
+
+    const reset = () => {
+        blocker.reset();
+    };
+
+    const checkIfNeedsBlock = (
+        e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+        href: string,
+    ) => {
+        e.preventDefault();
+        if (location.pathname.includes('configuration') && settingsAreDirty) {
+            blocker.block(() => navigate(href));
+            Confirm({
+                title: 'Unsaved Changes',
+                content: 'Are you sure you want to leave without saving?',
+                onClose: reset,
+                onConfirm: proceed,
+                confirmLabel: 'Yes',
+                cancelLabel: 'No',
+            });
+        } else {
+            navigate(href);
+        }
+    };
     return (
         <>
             <div
@@ -15,13 +52,24 @@ export const NavBar = () => {
                 )}
             >
                 <div className="py-5 border-gray-400 border-r-2 dark:border-gray-700"></div>
-                <NavbarLink href="/" svg={Carve} label="Carve" />
+                <NavbarLink
+                    href="/"
+                    svg={Carve}
+                    label="Carve"
+                    onClick={(e) => checkIfNeedsBlock(e, '/')}
+                />
                 <NavbarLink
                     href="stats"
                     icon={IoSpeedometerOutline}
                     label="Stats"
+                    onClick={(e) => checkIfNeedsBlock(e, 'stats')}
                 />
-                <NavbarLink href="tools" icon={RiToolsFill} label="Tools" />
+                <NavbarLink
+                    href="tools"
+                    icon={RiToolsFill}
+                    label="Tools"
+                    onClick={(e) => checkIfNeedsBlock(e, 'tools')}
+                />
 
                 <NavbarLink
                     href="configuration"
