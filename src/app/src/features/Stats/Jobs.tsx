8c8820f8e57cd866341683869b84e535fe7bacb6
walidkayhan <walid.kayhan@hotmail.com>
2025-11-25 13:46:22 -0500

    Fix class attribute to className in Jobs component

--- src/app/src/features/Stats/Jobs.tsx
+++ src/app/src/features/Stats/Jobs.tsx
@@ -93,12 +93,13 @@ const columnData: CustomColumnDef<Job, any>[] = [
 
 export function Jobs() {
     const { jobs } = useContext(StatContext);
+
     return (
         <div className="grid grid-cols-6 grid-rows-6 gap-2 w-full h-full overflow-y-auto">
             <div className="col-span-4 row-span-6 px-8 max-xl:px-0 mb-2">
                 <StatCard>
                     <CardHeader>Job History</CardHeader>
-                    <div class="w-full flex flex-col">
+                    <div className="w-full flex flex-col">
                         <SortableTable
                             defaultData={defaultData}
                             data={jobs}
