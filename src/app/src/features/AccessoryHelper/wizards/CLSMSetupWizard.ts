3ff49a9fbffa7ea6a9178310fd5f2803451a03c8
kgn <kevin.glovernetherton@gmail.com>
2025-12-02 15:40:58 -0500

    CLSM Wizard sample

---
+++ src/app/src/features/AccessoryHelper/wizards/CLSMSetupWizard.ts
@@ -0,0 +1,13 @@
+import { Wizard } from 'app/features/AccessoryHelper/types/wizard.ts';
+import CLSMLogo from './assets/clsm.jpg';
+
+export const CLSMSetupWizard: Wizard = {
+    category: 'Motor',
+    description: 'Upgrade your machine to Closed-Loop Stepper Motors',
+    estimatedTime: '30 minutes - 2 hours',
+    id: 'CLSM',
+    name: 'Closed-Loop Stepper Motors',
+    requiredItems: [],
+    steps: [],
+    thumbnail: CLSMLogo,
+};
--- src/app/src/features/AccessoryHelper/wizards/index.ts
+++ src/app/src/features/AccessoryHelper/wizards/index.ts
@@ -1,4 +1,5 @@
 import { Wizard } from 'app/features/AccessoryHelper/types/wizard.ts';
 import { ATCSetupWizard } from 'app/features/AccessoryHelper/wizards/ATCSetupWizard.ts';
+import { CLSMSetupWizard } from 'app/features/AccessoryHelper/wizards/CLSMSetupWizard.ts';
 
-export const MasterWizards: Wizard[] = [ATCSetupWizard];
+export const MasterWizards: Wizard[] = [ATCSetupWizard, CLSMSetupWizard];
