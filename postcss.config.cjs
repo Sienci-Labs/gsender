427fb84d9c17c20fa9c6f986f2bbdcace2834c9b
walidkayhan <walid.kayhan@hotmail.com>
2025-12-09 13:24:30 -0500

    Move postcss config to root folder

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: {
-        tailwindcss: { config: './tailwind.config.ts' },
+        tailwindcss: { config: './src/app/tailwind.config.ts' },
         autoprefixer: {},
     },
 };
