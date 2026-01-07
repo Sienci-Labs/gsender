290363f7280fa83f2c919655a7173ef36fcc090c
KGN <kevin.glovernetherton@gmail.com>
2026-01-07 15:13:57 -0500

    Thanks OB

--- notarize.js
+++ notarize.js
@@ -1,30 +1,27 @@
-// From https://github.com/simonw/til/blob/main/electron/sign-notarize-electron-macos.md
-// Based on https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
-
 const {
     notarize
-} = require("@electron/notarize");
+} = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
     const {
         electronPlatformName,
         appOutDir
     } = context;
-    if (electronPlatformName !== "darwin") return;
+    if (electronPlatformName !== 'darwin') return;
 
     const appName = context.packager.appInfo.productFilename;
 
     try {
         await notarize({
-            appBundleId: "org.sienci.gsender",
+            appBundleId: 'org.sienci.gsender',
             appPath: `${appOutDir}/${appName}.app`,
             appleId: process.env.APPLE_ID,
             appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
             teamId: process.env.APPLE_TEAM_ID,
         });
 
-        console.log("Notarization successful");
+        console.log('Notarization successful');
     } catch (error) {
-        console.error("Notarization failed:", error);
+        console.error('Notarization failed:', error);
     }
 };
