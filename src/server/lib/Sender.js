bb8b4861375483abef6a8d2915429cb1d3d5721a
Walid Kayhan <walid.kayhan@outlook.com>
2026-01-21 09:28:54 -0500

    Refactor remaining time estimation logic in Sender class for rotary files, they should now use received computed estimated time along with using countdown logic

--- src/server/lib/Sender.js
+++ src/server/lib/Sender.js
@@ -477,22 +477,13 @@ class Sender extends events.EventEmitter {
         // Elapsed Time
         this.updateElapsedTime();
 
-        if (this.state.isRotaryFile) {
-            // Make a 1 second delay before estimating the remaining time
-            if (this.state.elapsedTime >= 1000 && this.state.received > 0) {
-                const timePerCode = this.state.timeRunning / this.state.received;
-                this.state.remainingTime = (timePerCode * this.state.total - this.state.timeRunning);
-            }
-        } else {
-            // eslint-disable-next-line no-lonely-if
-            if (this.state.received > 0) {
-                if (this.state.estimatedTime > 0) { // in case smth goes wrong with the estimate, don't want to show negative time
-                    if (this.state.received < this.state.estimateData.length) {
-                        // add the lines to the queue from where we left off to the current number received
-                        for (let i = this.state.totalSentToQueue; i <= this.state.received; i++) {
-                            this.state.countdownQueue.push(Number(this.state.estimateData[i] || 0));
-                            this.state.totalSentToQueue++;
-                        }
+        if (this.state.received > 0) {
+            if (this.state.estimatedTime > 0) { // in case smth goes wrong with the estimate, don't want to show negative time
+                if (this.state.received < this.state.estimateData.length) {
+                    // add the lines to the queue from where we left off to the current number received
+                    for (let i = this.state.totalSentToQueue; i <= this.state.received; i++) {
+                        this.state.countdownQueue.push(Number(this.state.estimateData[i] || 0));
+                        this.state.totalSentToQueue++;
                     }
                 }
             }
