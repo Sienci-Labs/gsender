1a07021c485ce764b0a0d5193cf8fb0873e8809c
Sophia Beluli <sophiabeluli@gmail.com>
2026-03-17 11:30:59 -0400

    [MI-2288]: changes preferences api to hold object instead of save file

--- src/server/api/api.preferences.js
+++ src/server/api/api.preferences.js
@@ -1,19 +1,18 @@
-import config from '../services/configstore';
 import logger from '../lib/logger';
 
 const log = logger('api:preferences');
-const CONFIG_KEY = 'preferences';
+
+let preferencesObj = {};
 
 export const fetch = (req, res) => {
-    const prefs = config.get(CONFIG_KEY, {});
-    res.send(prefs);
+    res.send(preferencesObj);
 };
 
 export const replace = (req, res) => {
     const prefs = req.body;
 
     try {
-        config.set(CONFIG_KEY, prefs);
+        preferencesObj = prefs;
         res.send({ message: 'gSender preferences saved' });
     } catch (err) {
         res.status(ERR_INTERNAL_SERVER_ERROR).send({
