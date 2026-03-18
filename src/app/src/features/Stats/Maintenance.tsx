a3332de78bcac5de02e584e2e56e49c1354b71ce
Walid Kayhan <walid.kayhan@outlook.com>
2026-03-18 09:41:17 -0400

    Remove unnecessary margin from Maintenance component for cleaner layout

--- src/app/src/features/Stats/Maintenance.tsx
+++ src/app/src/features/Stats/Maintenance.tsx
@@ -6,7 +6,7 @@ import { MaintenanceList } from 'app/features/Stats/components/MaintenanceList.t
 export function Maintenance() {
     return (
         <div className="grid grid-cols-6 grid-rows-6 gap-4 max-xl:gap-2 w-full h-full overflow-y-auto">
-            <div className="col-span-4 row-span-6 pr-8 max-xl:pr-0 mb-2">
+            <div className="col-span-4 row-span-6 pr-8 max-xl:pr-0">
                 <StatCard>
                     <CardHeader>Maintenance</CardHeader>
                     <div className="w-full flex flex-col">
