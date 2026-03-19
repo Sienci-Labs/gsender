754209fdf3add7e9c863a7afc0d48280b3db22f8
kgn <kevin.glovernetherton@gmail.com>
2026-03-19 12:27:32 -0400

    Bug where it was always null - works on my computer, we'll see in 25 min

--- src/server/lib/YModemUSB.js
+++ src/server/lib/YModemUSB.js
@@ -66,7 +66,13 @@ export class YModem extends events.EventEmitter {
 
         // Empty file - add blank buffer
         if (!fileData.data) {
-            fileData.data = Buffer.alloc(0);
+            if (typeof fileData.content === 'string') {
+                debugLog(`sendFile: converting content string to Buffer for "${fileData.name}"`);
+                fileData.data = Buffer.from(fileData.content, 'utf8');
+                fileData.size = fileData.data.byteLength;
+            } else {
+                fileData.data = Buffer.alloc(0);
+            }
         } else if (!Buffer.isBuffer(fileData.data)) {
             if (typeof fileData.content === 'string') {
                 debugLog(`sendFile: converting content string to Buffer for "${fileData.name}"`);
@@ -179,7 +185,13 @@ export class YModem extends events.EventEmitter {
             const fileData = files[fileIndex];
             // Empty file - add blank buffer
             if (!fileData.data) {
-                fileData.data = Buffer.alloc(0);
+                if (typeof fileData.content === 'string') {
+                    debugLog(`sendFiles: converting content string to Buffer for "${fileData.name}"`);
+                    fileData.data = Buffer.from(fileData.content, 'utf8');
+                    fileData.size = fileData.data.byteLength;
+                } else {
+                    fileData.data = Buffer.alloc(0);
+                }
             } else if (!Buffer.isBuffer(fileData.data)) {
                 if (typeof fileData.content === 'string') {
                     debugLog(`sendFiles: converting content string to Buffer for "${fileData.name}"`);
