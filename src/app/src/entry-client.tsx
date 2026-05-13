6875146bf3e4c128310bc111bdfb55555c988495
Walid Kayhan <walid.kayhan@outlook.com>
2026-05-13 15:03:43 -0400

    Refactor PostHog integration: move configuration to PostHogConfig component and update entry-client to use it

--- src/app/src/entry-client.tsx
+++ src/app/src/entry-client.tsx
@@ -1,25 +1,15 @@
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-
 import App from './App';
+import PostHogConfig from './posthog-config';
 
 import './sentry-config';
-
-import posthog from 'posthog-js';
-import { PostHogErrorBoundary, PostHogProvider } from '@posthog/react';
-
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN, {
-    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-    defaults: '2026-01-30',
-});
+import './index.css';
 
 // Use createRoot instead of hydrateRoot since we don't need full SSR for Electron app
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 root.render(
-    <PostHogProvider client={posthog}>
-        <PostHogErrorBoundary>
-            <App />
-        </PostHogErrorBoundary>
-    </PostHogProvider>,
+    <PostHogConfig>
+        <App />
+    </PostHogConfig>,
 );
