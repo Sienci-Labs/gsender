b32c52afdf6b7ffd092d9a24dc7d3fb294f24cc7
Walid Kayhan <walid.kayhan@outlook.com>
2026-06-09 23:13:11 -0400

    Refactor PostHog initialization logic to streamline configuration based on development mode. Added conditional settings for autocapture, pageview capture, and session recording. Improved warning for uninitialized PostHog in development mode.

--- src/app/src/posthog-config.tsx
+++ src/app/src/posthog-config.tsx
@@ -6,40 +6,27 @@ const isInDevMode = import.meta.env.MODE === 'development';
 const posthogToken = import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN;
 const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST;
 
-posthog.init(posthogToken, {
-    api_host: posthogHost,
-    person_profiles: 'identified_only',
-});
+if (posthogToken && posthogHost) {
+    posthog.init(posthogToken, {
+        api_host: posthogHost,
+        person_profiles: 'identified_only',
+        debug: isInDevMode,
+        autocapture: !isInDevMode,
+        capture_pageview: !isInDevMode,
+        capture_heatmaps: !isInDevMode,
+        disable_session_recording: isInDevMode,
+        opt_out_capturing_by_default: isInDevMode,
+    });
 
-// Register properties that are added to all events
-posthog.register({
-    app_version: version,
-});
-
-posthog.on('eventCaptured', (payload) => {
-    // PostHog auto-captured events always start with "$"
-    if (payload.event && !payload.event.startsWith('$')) {
-        console.log('Manual PostHog Event:', payload.event, payload);
-    }
-});
-
-// if (!isInDevMode && posthogToken && posthogHost) {
-//     posthog.init(posthogToken, {
-//         api_host: posthogHost,
-//         person_profiles: 'identified_only',
-//     });
-
-//     // Register properties that are added to all events
-//     posthog.register({
-//         app_version: version,
-//     });
-
-//     posthog.debug(true);
-// } else {
-//     console.warn(
-//         'PostHog is not initialized because the app is running in development mode or the token/host are not set',
-//     );
-// }
+    // Register properties that are added to all events
+    posthog.register({
+        app_version: version,
+    });
+} else {
+    console.warn(
+        'PostHog is not initialized because the app is running in development mode or the token/host are not set',
+    );
+}
 
 const PostHogConfig = ({ children }: { children: React.ReactNode }) => {
     if (isInDevMode || !posthogToken || !posthogHost) {
