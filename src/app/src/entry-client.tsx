1efde74609adee954e77093b402fb90c75c9fd4e
Walid Kayhan <walid.kayhan@outlook.com>
2026-06-23 15:46:12 -0400

    Wrap App component with SentryConfig for improved error tracking integration

--- src/app/src/entry-client.tsx
+++ src/app/src/entry-client.tsx
@@ -2,14 +2,16 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import PostHogConfig from "./posthog-config";
+import SentryConfig from "./sentry-config";
 
-import "./sentry-config";
 import "./index.css";
 
 // Use createRoot instead of hydrateRoot since we don't need full SSR for Electron app
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 root.render(
 	<PostHogConfig>
-		<App />
+		<SentryConfig>
+			<App />
+		</SentryConfig>
 	</PostHogConfig>,
 );
