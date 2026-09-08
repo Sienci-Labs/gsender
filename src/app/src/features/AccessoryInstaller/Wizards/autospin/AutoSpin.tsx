cc59b17e280d3a1b7b02a26b4ce437a9773a666c
kgn <kevin.glovernetherton@gmail.com>
2026-09-08 14:15:15 -0400

    AutoSpin Configuration moved to wizard from config

---
+++ src/app/src/features/AccessoryInstaller/Wizards/autospin/AutoSpin.tsx
@@ -0,0 +1,65 @@
+import { useValidations } from "app/components/Wizard/hooks/UseValidations.tsx";
+import PlaceholderImage from "app/components/Wizard/assets/placeholder.png";
+import type { Wizard } from "app/components/Wizard/types";
+import AutoSpinIcon from "app/features/Config/assets/images/autospin.svg";
+import { AutoSpinCompletion } from "app/features/AccessoryInstaller/Wizards/autospin/components/Completion.tsx";
+import { AutoSpinGcodePreview } from "app/features/AccessoryInstaller/Wizards/autospin/components/AutoSpinGcodePreview.tsx";
+import { EepromConfig } from "app/features/AccessoryInstaller/Wizards/autospin/components/EepromConfig.tsx";
+import { TestAutoSpin } from "app/features/AccessoryInstaller/Wizards/autospin/components/TestAutoSpin.tsx";
+import { useMemo } from "react";
+
+export function useAutoSpinWizard() {
+	const { connectionValidation } = useValidations();
+
+	const validations = useMemo(
+		() => [connectionValidation],
+		[connectionValidation],
+	);
+
+	return useMemo<Wizard>(
+		() => ({
+			id: "autospin",
+			title: "AutoSpin",
+			image: AutoSpinIcon,
+			validations: [...validations],
+			subWizards: [
+				{
+					id: "autospin-config",
+					title: "AutoSpin Setup",
+					description: "Configure your AutoSpin for first time use",
+					estimatedTime: "5 - 15 minutes",
+					configVersion: "1.0",
+					completionPage: AutoSpinCompletion,
+					steps: [
+						{
+							id: "eeprom-config",
+							title: "AutoSpin EEPROM Configuration",
+							component: EepromConfig,
+							secondaryContent: [
+								{
+									type: "component",
+									content: AutoSpinGcodePreview,
+									title: "Commands to be sent",
+									fill: true,
+								},
+							],
+						},
+						{
+							id: "test",
+							title: "Test AutoSpin",
+							component: TestAutoSpin,
+							secondaryContent: [
+								{
+									type: "image",
+									content: PlaceholderImage,
+									title: "AutoSpin Dial",
+								},
+							],
+						},
+					],
+				},
+			],
+		}),
+		[validations],
+	);
+}
